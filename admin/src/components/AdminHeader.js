a4645446df5df89121b2cee058300c0b9b7ee765
navinesh <navinesh.chand@gmail.com>
2016-10-04 13:53:09 +1200

    format code to stateless functional component

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -1,44 +1,41 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
-const Header = ({ onLogoutClick }) => {
-  return (
-    <div className="Navs">
-      <nav className="navbar navbar-fixed-top">
-        <div className="container">
-          <div className="nav navbar-nav">
-            <Link className="nav-item nav-link" to="/">
-              LMS - dashboard
+const Header = ({ onLogoutClick }) =>
+  <div className="Navs">
+    <nav className="navbar navbar-fixed-top">
+      <div className="container">
+        <div className="nav navbar-nav">
+          <Link className="nav-item nav-link" to="/">
+            LMS - dashboard
+          </Link>
+          <div className="pull-xs-right">
+            <Link className="nav-item nav-link" to="/staffrecord">
+              Staff record
             </Link>
-            <div className="pull-xs-right">
-              <Link className="nav-item nav-link" to="/staffrecord">
-                Staff record
-              </Link>
-              <Link className="nav-item nav-link" to="/approvedleave">
-                Leave calendar
-              </Link>
-              <Link className="nav-item nav-link" to="/">
-                Leave report
-              </Link>
-              <Link className="nav-item nav-link" to="/">
-                Sick sheet record
-              </Link>
-              <Link className="nav-item nav-link" to="/newrecord">
-                New record
-              </Link>
-              <Link className="nav-item nav-link" to="/">
-                Archived staff
-              </Link>
-              <button onClick={() => onLogoutClick()} className="btn btn-primary m-l-1">
-                Sign out
-              </button>
-            </div>
+            <Link className="nav-item nav-link" to="/approvedleave">
+              Leave calendar
+            </Link>
+            <Link className="nav-item nav-link" to="/">
+              Leave report
+            </Link>
+            <Link className="nav-item nav-link" to="/">
+              Sick sheet record
+            </Link>
+            <Link className="nav-item nav-link" to="/newrecord">
+              New record
+            </Link>
+            <Link className="nav-item nav-link" to="/">
+              Archived staff
+            </Link>
+            <button onClick={() => onLogoutClick()} className="btn btn-primary m-l-1">
+              Sign out
+            </button>
           </div>
         </div>
-      </nav>
-    </div>
-  )
-}
+      </div>
+    </nav>
+  </div>
 
 Header.propTypes = {
   onLogoutClick: PropTypes.func.isRequired
