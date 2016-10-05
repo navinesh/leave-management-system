10c1ad59f5ab447f8f1741c97ccd5570b231c277
navinesh <navinesh.chand@gmail.com>
2016-10-06 07:51:29 +1200

    add clear search staff record state

--- admin/src/actions/StaffRecord.js
+++ admin/src/actions/StaffRecord.js
@@ -2,6 +2,7 @@ export const REQUEST_STAFF_RECORD = 'REQUEST_STAFF_RECORD'
 export const RECEIVE_STAFF_RECORD = 'RECEIVE_STAFF_RECORD'
 export const ERROR_STAFF_RECORD = 'ERROR_STAFF_RECORD'
 export const STAFF_RECORD_SEARCH = 'STAFF_RECORD_SEARCH'
+export const CLEAR_STAFF_RECORD_SEARCH = 'CLEAR_STAFF_RECORD_SEARCH'
 
 export const requestStaffRecord = () => ({
   type: REQUEST_STAFF_RECORD
@@ -21,6 +22,12 @@ export const searchStaffRecord = (searchTerm) => ({
     type: STAFF_RECORD_SEARCH,
     searchTerm
 })
+
+export const clearSearchStaffRecord = () => {
+  return {
+    type: CLEAR_STAFF_RECORD_SEARCH
+  }
+}
 
 export const fetchStaffRecord = () => {
   return dispatch => {
