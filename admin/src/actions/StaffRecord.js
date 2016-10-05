cf31643fa3fd3490d595c8a0b91c69216777debf
navinesh <navinesh.chand@gmail.com>
2016-10-06 07:45:54 +1200

    first commit

---
+++ admin/src/actions/StaffRecord.js
@@ -0,0 +1,35 @@
+export const REQUEST_STAFF_RECORD = 'REQUEST_STAFF_RECORD'
+export const RECEIVE_STAFF_RECORD = 'RECEIVE_STAFF_RECORD'
+export const ERROR_STAFF_RECORD = 'ERROR_STAFF_RECORD'
+export const STAFF_RECORD_SEARCH = 'STAFF_RECORD_SEARCH'
+
+export const requestStaffRecord = () => ({
+  type: REQUEST_STAFF_RECORD
+})
+
+export const receiveStaffRecord = (json) => ({
+  type: RECEIVE_STAFF_RECORD,
+  staff_record: json.staff_record,
+  receivedAt: Date.now()
+})
+
+export const errorStaffRecord = () => ({
+  type: ERROR_STAFF_RECORD
+})
+
+export const searchStaffRecord = (searchTerm) => ({
+    type: STAFF_RECORD_SEARCH,
+    searchTerm
+})
+
+export const fetchStaffRecord = () => {
+  return dispatch => {
+    dispatch(requestStaffRecord())
+    return fetch(`http://localhost:8080/staff-record.api`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveStaffRecord(json)))
+      .catch((error) => {
+        dispatch(errorStaffRecord())
+      })
+  }
+}
