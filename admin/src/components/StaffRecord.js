02cfa1e46b4f834b7737570dd5967270a2fc26bb
navinesh <navinesh.chand@gmail.com>
2016-10-07 13:33:27 +1200

    fic formatting

--- admin/src/components/StaffRecord.js
+++ admin/src/components/StaffRecord.js
@@ -16,41 +16,42 @@ class StaffRecordList extends Component {
         let dob = new Date(record.date_of_birth)
         let dateOfBirth = moment(dob).format('DD/MM/YYYY')
         return (
-        <div className="col-sm-3" key={record.id}>
-          <div className="card">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">{record.othernames} {record.surname}</li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{record.annual}</span>
-                Annual
-              </li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{record.sick}</span>
-                Sick
-              </li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{record.bereavement}</span>
-                Bereavement
-              </li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{record.christmas}</span>
-                Christmas
-              </li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{dateOfBirth}</span>
-                DOB
-              </li>
-              <li className="list-group-item">
-                <span className="tag tag-primary tag-pill pull-xs-right">{record.maternity}</span>
-                Maternity
-              </li>
-              <li className="list-group-item">
-                <Link to="#" className="card-link">Edit</Link>
-                <Link to="#" className="card-link">Archive</Link>
-              </li>
-            </ul>
-          </div>
-        </div>)})
+          <div className="col-sm-3" key={record.id}>
+            <div className="card">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{record.othernames} {record.surname}</li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{record.annual}</span>
+                  Annual
+                </li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{record.sick}</span>
+                  Sick
+                </li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{record.bereavement}</span>
+                  Bereavement
+                </li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{record.christmas}</span>
+                  Christmas
+                </li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{dateOfBirth}</span>
+                  DOB
+                </li>
+                <li className="list-group-item">
+                  <span className="tag tag-primary tag-pill pull-xs-right">{record.maternity}</span>
+                  Maternity
+                </li>
+                <li className="list-group-item">
+                  <Link to="#" className="card-link">Edit</Link>
+                  <Link to="#" className="card-link">Archive</Link>
+                </li>
+              </ul>
+            </div>
+          </div>)
+        })
 
     return (
       <div className="container">
