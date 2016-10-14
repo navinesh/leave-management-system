559afb99ae5e867ad43685446ada126fbc1a1edc
navinesh <navinesh.chand@gmail.com>
2016-10-14 13:46:21 +1200

    first commit

---
+++ admin/src/actions/ArchivedStaffRecord.js
@@ -0,0 +1,29 @@
+export const REQUEST_ARCHIVED_STAFF_RECORD = 'REQUEST_ARCHIVED_STAFF_RECORD'
+export const RECEIVE_ARCHIVED_STAFF_RECORD = 'RECEIVE_ARCHIVED_STAFF_RECORD'
+export const ERROR_ARCHIVED_STAFF_RECORD = 'ERROR_ARCHIVED_STAFF_RECORD'
+
+export const requestArchivedStaffRecord = () => ({
+  type: REQUEST_ARCHIVED_STAFF_RECORD
+})
+
+export const receiveArchivedStaffRecord = (json) => ({
+  type: RECEIVE_ARCHIVED_STAFF_RECORD,
+  archived_staff_record: json.archived_staff_record,
+  receivedAt: Date.now()
+})
+
+export const errorArchivedStaffRecord = () => ({
+  type: ERROR_ARCHIVED_STAFF_RECORD
+})
+
+export const fetchArchivedStaffRecord = () => {
+  return dispatch => {
+    dispatch(requestArchivedStaffRecord())
+    return fetch(`http://localhost:8080/archived-staff-record.api`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveArchivedStaffRecord(json)))
+      .catch((error) => {
+        dispatch(errorArchivedStaffRecord())
+      })
+  }
+}
