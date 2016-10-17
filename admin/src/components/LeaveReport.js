e97d5eac5f672c35bfffb2e294bcff5a3917c014
navinesh <navinesh.chand@gmail.com>
2016-10-18 11:27:34 +1200

    first commit

---
+++ admin/src/components/LeaveReport.js
@@ -0,0 +1,50 @@
+import React, { PropTypes } from 'react'
+
+const ReportList = ({ leave_record }) => {
+  const itemNodes = leave_record.map((record) => {
+    return (
+      <tr key={record.id}>
+        <td>{record.user.othernames} {record.user.surname}</td>
+        <td>{record.leave_name}</td>
+        <td>{record.leave_type}</td>
+        <td>{record.start_date}</td>
+        <td>{record.end_date}</td>
+        <td>{record.leave_status}</td>
+        <td>{record.date_posted}</td>
+        <td>{record.date_reviewed}</td>
+        <td>{record.declined_reason}</td>
+      </tr>
+    )
+  })
+  return (
+    <div className="table-responsive">
+    <table className="table table-bordered table-hover">
+      <thead className="thead-default">
+        <tr>
+          <th>Name</th>
+          <th>Leave</th>
+          <th>Type</th>
+          <th>Start date</th>
+          <th>End date</th>
+          <th>Status</th>
+          <th>Date posted</th>
+          <th>Date reviewed</th>
+          <th>Reason for decline</th>
+        </tr>
+      </thead>
+      <tbody>
+        {itemNodes}
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+const LeaveReportList = ({ leave_record }) =>
+  <ReportList leave_record={leave_record} />
+
+LeaveReportList.propTypes = {
+  leave_record: PropTypes.array.isRequired
+}
+
+export default LeaveReportList
