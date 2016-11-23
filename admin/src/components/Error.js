74d284eb726bdf6c18b5af153741ff25a5ae198f
navinesh <navinesh.chand@gmail.com>
2016-11-23 18:23:49 +1300

    first commit

---
+++ admin/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () =>
+  <div className="container text-xs-center">
+    <div className="offset-sm-2 col-sm-8">
+      <h2 className="display-2">404</h2>
+      <h4 className="display-3">That's an error.</h4>
+      <h5 className="display-4">The site configured at this address does not contain the requested file.</h5>
+    </div>
+  </div>
+
+export default Error
