11187ed62843eca2eb7a225460354e517a87185d
navinesh <navinesh.chand@gmail.com>
2016-11-29 14:33:42 +1300

    first commit

---
+++ admin/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Sidebar = () =>
+  <nav className="nav nav-pills nav-stacked">
+    <Link className="nav-link" to="/">
+        Dashboard
+    </Link>
+    <Link className="nav-link" to="/staffrecord">
+        Staff record
+    </Link>
+    <Link className="nav-link" to="/approvedleave">
+      Leave calendar
+    </Link>
+    <Link className="nav-link" to="/leavereport">
+      Leave report
+    </Link>
+    <Link className="nav-link" to="/sicksheetrecord">
+      Sick sheet record
+    </Link>
+    <Link className="nav-link" to="/archivedstaffrecord">
+      Archived staff
+    </Link>
+    <Link className="nav-link" to="/newrecord">
+      New record
+    </Link>
+  </nav>
+
+export default Sidebar
