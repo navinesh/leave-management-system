43a9fb42d9c96f6b564640f0c8f1fb922f92f9fe
navinesh <navinesh.chand@gmail.com>
2016-12-14 13:41:08 +1300

    first commit

---
+++ user/src/actions/resetpassword.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+export const REQUEST_PASSWORD_RESET = 'REQUEST_PASSWORD_RESET'
+export const PASSWORD_RESET_SUCCESS = 'PASSWORD_RESET_SUCCESS'
+export const PASSWORD_RESET_ERROR = 'PASSWORD_RESET_ERROR'
+
+export function requestPasswordReset(email) {
+  return {
+    type: REQUEST_PASSWORD_RESET,
+    email
+  }
+}
+
+export function passwordResetError(data){
+  return {
+    type: PASSWORD_RESET_ERROR,
+    message: data.message
+  }
+}
+
+export function passwordResetSuccess(data) {
+  return {
+    type: PASSWORD_RESET_SUCCESS,
+    message: data.message,
+  }
+}
+
+export function resetPassword(email) {
+  return dispatch => {
+    dispatch(requestPasswordReset(email))
+    axios.post('reset-password.api', {
+        email: email
+      })
+      .then((response) => {
+        if (response.status === 200){
+          dispatch(passwordResetError(response.data))
+        }
+        else {
+          dispatch(passwordResetSuccess(response.data))
+        }
+      })
+  }
+}
