43004f338a920d22c36d33869070d5150dba1601
navinesh <navinesh.chand@gmail.com>
2016-12-16 10:06:00 +1300

    refactor component and add headers

--- user/src/components/usererror.js
+++ user/src/components/usererror.js
@@ -1,15 +1,10 @@
 import React from 'react'
 
-const UserError = () => {
-    return(
-      <div className="container text-xs-center">
-        <div className="offset-sm-2 col-sm-8">
-          <h2>404</h2>
-            <h4>That's an error.</h4>
-          <h5>The site configured at this address does not contain the requested file.</h5>
-        </div>
-      </div>
-    )
-}
-
-export default UserError
+export default () =>
+  <div className="container text-xs-center">
+    <div className="offset-sm-2 col-sm-8">
+      <h2 className="display-2">404</h2>
+      <h4 className="display-3">That's an error.</h4>
+      <h5 className="display-4">The site configured at this address does not contain the requested file.</h5>
+    </div>
+  </div>
