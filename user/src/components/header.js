748641fa4def347789aae003b988d123d519b564
navinesh <navinesh.chand@gmail.com>
2016-12-16 10:04:37 +1300

    refactor component as a class

--- user/src/components/header.js
+++ user/src/components/header.js
@@ -1,46 +1,52 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import React, { PropTypes, Component } from 'react'
+import { Link, browserHistory } from 'react-router'
+import { logoutUser } from '../actions/userlogout'
 
-const Navs = ({ isAuthenticated, onLogoutClick, children }) => {
-  return (
-    <div className="Navs">
-      <nav className="navbar navbar-fixed-top">
-        <div className="container">
-          {!isAuthenticated &&
-            <div className="nav navbar-nav">
-              <Link className="nav-item nav-link" to="/">
-                Leave management system
-              </Link>
-            </div>
-          }
-          {isAuthenticated &&
-            <div className="nav navbar-nav">
-              <Link className="nav-item nav-link" to="/">
-                Leave management system
-              </Link>
-              <div className="float-xs-right">
-                <Link className="nav-item nav-link" to="/leaveapplication">
-                  Leave application
+export default class Navs extends Component {
+  userLogout(e) {
+    e.preventDefault()
+    this.props.dispatch(logoutUser())
+    browserHistory.push('/')
+  }
+
+  render() {
+    const { isAuthenticated } = this.props
+    return (
+      <div className="Navs">
+        <nav className="navbar navbar-fixed-top">
+          <div className="container">
+            {!isAuthenticated &&
+              <div className="nav navbar-nav">
+                <Link className="nav-item nav-link" to="/">
+                  Leave management system
                 </Link>
-                <Link className="nav-item nav-link" to="/leavecalendar">
-                  Leave calendar
-                </Link>
-                <button onClick={() => onLogoutClick()} className="btn btn-primary ml-1">
-                  Sign out
-                </button>
               </div>
-            </div>
-          }
-        </div>
-      </nav>
-      {children}
-    </div>
-  )
+            }
+            {isAuthenticated &&
+              <div className="nav navbar-nav">
+                <Link className="nav-item nav-link" to="/">
+                  Leave management system
+                </Link>
+                <div className="float-xs-right">
+                  <Link className="nav-item nav-link" to="/leaveapplication">
+                    Leave application
+                  </Link>
+                  <Link className="nav-item nav-link" to="/leavecalendar">
+                    Leave calendar
+                  </Link>
+                  <button onClick={this.userLogout.bind(this)} className="btn btn-primary ml-1">
+                    Sign out
+                  </button>
+                </div>
+              </div>
+            }
+          </div>
+        </nav>
+      </div>
+    )
+  }
 }
 
 Navs.propTypes = {
-  onLogoutClick: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired
 }
-
-export default Navs
