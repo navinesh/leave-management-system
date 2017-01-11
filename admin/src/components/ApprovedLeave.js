5facbb678c649fe6981bda978dedafdce10e4710
navinesh <navinesh.chand@gmail.com>
2017-01-12 12:51:47 +1300

    format code

--- admin/src/components/ApprovedLeave.js
+++ admin/src/components/ApprovedLeave.js
@@ -1,48 +1,53 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import React, { PropTypes } from "react";
 
-const moment = require('moment')
+import { Link } from "react-router";
+
+const moment = require("moment");
 
 const ApprovedRecordList = ({ approved_items }) => {
-  const items = approved_items.filter((record) => {
-    // get current date and format it
-    let dateToday = moment()
-    let todayDate = dateToday.format('DD/MM/YYYY')
+  const items = approved_items
+    .filter(record => {
+      // get current date and format it
+      let dateToday = moment();
 
-    // get end date and format it
-    let end_Date =   moment(record.end_date, 'DD/MM/YYYY').format('DD/MM/YYYY')
+      let todayDate = dateToday.format("DD/MM/YYYY");
 
-    // check if current date and end date is same
-    let isCurrentDate = (todayDate === end_Date ? true : false)
+      // get end date and format it
+      let end_Date = moment(record.end_date, "DD/MM/YYYY").format("DD/MM/YYYY");
 
-    // check if end date is same or greater than current date
-    let eDate =   moment(record.end_date, 'DD/MM/YYYY').format('MM/DD/YYYY')
-    let endDate = moment(new Date(eDate))
-    let isEndDate = endDate.isSameOrAfter(dateToday)
+      // check if current date and end date is same
+      let isCurrentDate = todayDate === end_Date ? true : false;
 
-    // return true for current and future leaves
-    return ((isCurrentDate || isEndDate) ? true : false)
+      // check if end date is same or greater than current date
+      let eDate = moment(record.end_date, "DD/MM/YYYY").format("MM/DD/YYYY");
 
-  }).map((data) => {
-    return (
-      <tr key={data.id}>
-        <td>{data.user.othernames} {data.user.surname}</td>
-        <td>{data.leave_name}</td>
-        <td>{data.leave_type}</td>
-        <td>{data.start_date}</td>
-        <td>{data.end_date}</td>
-        <td>{data.leave_days}</td>
-        <td>
-          <Link to="/reset" className="btn btn-info btn-sm">Edit</Link>
-        </td>
-        <td>
-          <Link to="/reset" className="btn btn-danger btn-sm">Delete</Link>
-        </td>
-      </tr>)
-  })
+      let endDate = moment(new Date(eDate));
 
-  return ((items.length > 0) ?
-    (<div className="table-responsive">
+      let isEndDate = endDate.isSameOrAfter(dateToday);
+
+      // return true for current and future leaves
+      return isCurrentDate || isEndDate ? true : false;
+    })
+    .map(data => {
+      return (
+        <tr key={data.id}>
+          <td>{data.user.othernames}{data.user.surname}</td>
+          <td>{data.leave_name}</td>
+          <td>{data.leave_type}</td>
+          <td>{data.start_date}</td>
+          <td>{data.end_date}</td>
+          <td>{data.leave_days}</td>
+          <td>
+            <Link to="/reset" className="btn btn-info btn-sm">Edit</Link>
+          </td>
+          <td>
+            <Link to="/reset" className="btn btn-danger btn-sm">Delete</Link>
+          </td>
+        </tr>
+      );
+    });
+
+  return items.length > 0 ? <div className="table-responsive">
       <table className="table table-bordered table-hover">
         <thead className="thead-default">
           <tr>
@@ -60,18 +65,16 @@ const ApprovedRecordList = ({ approved_items }) => {
           {items}
         </tbody>
       </table>
-    </div>) :
-    (<div className="container text-center" style={{paddingTop: '100px'}}>
-        <h1 className="display-3">There are no approved leave record.</h1>
-    </div>)
-  )
-}
+    </div> : <div className="container text-center" style={
+      { paddingTop: "100px" }
+    }>
+      <h1 className="display-3">There are no approved leave record.</h1>
+    </div>;
+};
 
 const ApprovedLeaveList = ({ approved_items }) =>
-  <ApprovedRecordList approved_items={approved_items} />
+  <ApprovedRecordList approved_items={approved_items} />;
 
-ApprovedLeaveList.propTypes = {
-  approved_items: PropTypes.array.isRequired
-}
+ApprovedLeaveList.propTypes = { approved_items: PropTypes.array.isRequired };
 
-export default ApprovedLeaveList
+export default ApprovedLeaveList;
