7a6bf3778d8a106186251209cb9d618690d51385
navinesh <navinesh.chand@gmail.com>
2017-01-11 16:42:42 +1300

    change class text-xs-center to text-center

--- admin/src/components/Error.js
+++ admin/src/components/Error.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default () =>
-  <div className="container text-xs-center">
-    <div className="offset-sm-2 col-sm-8">
+  <div className="container text-center">
+    <div className="col-md-8 offset-md-2">
       <h2 className="display-2">404</h2>
       <h4 className="display-3">That's an error.</h4>
       <h5 className="display-4">The site configured at this address does not contain the requested file.</h5>
