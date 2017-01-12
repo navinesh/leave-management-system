7dbf2bbc51b30e6ef82170cbde91617262f5a6a5
navinesh <navinesh.chand@gmail.com>
2017-01-12 13:00:16 +1300

    format code

--- admin/src/containers/SickSheetRecord.js
+++ admin/src/containers/SickSheetRecord.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchSickSheetRecord } from '../actions/SickSheetRecord'
-import SickSheetList from '../components/SickSheetRecord'
+import React, { Component } from "react";
 
-const BeatLoader = require('halogen/BeatLoader');
+import { connect } from "react-redux";
+
+import { fetchSickSheetRecord } from "../actions/SickSheetRecord";
+
+import SickSheetList from "../components/SickSheetRecord";
+
+const BeatLoader = require("halogen/BeatLoader");
 
 class SickSheetRecord extends Component {
   componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(fetchSickSheetRecord())
+    const { dispatch } = this.props;
+
+    dispatch(fetchSickSheetRecord());
   }
 
   render() {
-    const { isFetching, sickSheet_items, isAuthenticated } = this.props
+    const { isFetching, sickSheet_items, isAuthenticated } = this.props;
+
     return (
       <div className="container">
-        {isAuthenticated && (isFetching ?
-          <div className="text-xs-center"><BeatLoader color="#0275d8" size="12px" /></div> :
-          <SickSheetList sickSheet_items={sickSheet_items} />)
+        {
+          isAuthenticated &&
+            (isFetching
+              ? <div className="text-center"><BeatLoader color="#0275d8" size="12px" /></div>
+              : <SickSheetList sickSheet_items={sickSheet_items} />)
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { sickSheet, adminAuth } = state
+const mapStateToProps = state => {
+  const { sickSheet, adminAuth } = state;
 
-  const {
-    isFetching,
-    sickSheet_items
-  } = sickSheet
+  const { isFetching, sickSheet_items } = sickSheet;
 
-  const { isAuthenticated } = adminAuth
+  const { isAuthenticated } = adminAuth;
 
-  return {
-    isFetching,
-    sickSheet_items,
-    isAuthenticated
-  }
-}
+  return { isFetching, sickSheet_items, isAuthenticated };
+};
 
 export default connect(mapStateToProps)(SickSheetRecord)
