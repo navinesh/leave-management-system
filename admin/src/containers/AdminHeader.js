18b9b9d78dcc31b642428f7c80fd15abede18213
navinesh <navinesh.chand@gmail.com>
2017-01-13 09:03:08 +1300

    format code

--- admin/src/containers/AdminHeader.js
+++ admin/src/containers/AdminHeader.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Header from '../components/AdminHeader'
-import AdminLogin from './AdminLogin'
+import React from "react";
+
+import { connect } from "react-redux";
+
+import Header from "../components/AdminHeader";
+
+import AdminLogin from "./AdminLogin";
 
 const AdminHeader = ({ isAuthenticated, dispatch, children }) =>
   <div className="AdminHeader">
-    {
-      isAuthenticated ?
-      <Header dispatch={dispatch} /> :
-      <div>
-        <h1 className="display-4 text-center pb-4">
-          Leave Management System
-        </h1>
-      <AdminLogin />
-      </div>
-    }
+    {isAuthenticated ? <Header dispatch={dispatch} /> : <div>
+          <h1 className="display-4 text-center pb-4">
+            Leave Management System
+          </h1>
+          <AdminLogin />
+        </div>}
     {children}
-  </div>
+  </div>;
 
-const mapStateToProps = (state) => {
-  const { adminAuth } = state
-  const { isAuthenticated } = adminAuth
+const mapStateToProps = state => {
+  const { adminAuth } = state;
 
-  return { isAuthenticated }
-}
+  const { isAuthenticated } = adminAuth;
+
+  return { isAuthenticated };
+};
 
 export default connect(mapStateToProps)(AdminHeader)
