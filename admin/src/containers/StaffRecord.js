bfc34b980fbb6a054f7c743407026a7902ccbded
navinesh <navinesh.chand@gmail.com>
2017-01-12 13:01:10 +1300

    format code

--- admin/src/containers/StaffRecord.js
+++ admin/src/containers/StaffRecord.js
@@ -1,42 +1,50 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux'
-import { fetchStaffRecord, clearSearchStaffRecord } from '../actions/StaffRecord'
-import StaffRecordList from '../components/StaffRecord'
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+import { fetchStaffRecord, clearSearchStaffRecord } from "../actions/StaffRecord";
+
+import StaffRecordList from "../components/StaffRecord";
 
 class StaffRecord extends Component {
   componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(fetchStaffRecord())
+    const { dispatch } = this.props;
+
+    dispatch(fetchStaffRecord());
   }
 
   componentWillUnmount() {
-    const { dispatch } = this.props
-    dispatch(clearSearchStaffRecord())
+    const { dispatch } = this.props;
+
+    dispatch(clearSearchStaffRecord());
   }
 
-  render () {
-    const { staff_record, isAuthenticated, searchTerm, dispatch } = this.props
+  render() {
+    const { staff_record, isAuthenticated, searchTerm, dispatch } = this.props;
+
     return (
       <div className="container">
-        {isAuthenticated &&
-          <StaffRecordList  staff_record={staff_record} searchTerm={searchTerm} dispatch={dispatch} />
+        {
+          isAuthenticated &&
+            <StaffRecordList staff_record={staff_record} searchTerm={
+              searchTerm
+            } dispatch={dispatch} />
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { staffRecord, searchStaffRecord, adminAuth } = state
-  const { staff_record } = staffRecord
-  const { isAuthenticated } = adminAuth
-  const { searchTerm } = searchStaffRecord
+const mapStateToProps = state => {
+  const { staffRecord, searchStaffRecord, adminAuth } = state;
 
-  return {
-    staff_record,
-    isAuthenticated,
-    searchTerm
-  }
-}
+  const { staff_record } = staffRecord;
+
+  const { isAuthenticated } = adminAuth;
+
+  const { searchTerm } = searchStaffRecord;
+
+  return { staff_record, isAuthenticated, searchTerm };
+};
 
 export default connect(mapStateToProps)(StaffRecord)
