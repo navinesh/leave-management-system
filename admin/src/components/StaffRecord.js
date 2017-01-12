dbe75c0f3590c9f745493706e155c5fb606dff31
navinesh <navinesh.chand@gmail.com>
2017-01-13 12:46:31 +1300

    Format code
    Format code using prettier

--- admin/src/components/StaffRecord.js
+++ admin/src/components/StaffRecord.js
@@ -1,32 +1,41 @@
-import React, { PropTypes, Component } from 'react'
-const moment = require('moment')
-import { searchStaffRecord } from '../actions/StaffRecord'
+import React, { PropTypes, Component } from "react";
+
+const moment = require("moment");
+
+import { searchStaffRecord } from "../actions/StaffRecord";
 
 class StaffRecordList extends Component {
-  handleSearchChange (e) {
-    this.props.dispatch(searchStaffRecord((e.target.value).toLowerCase()))
+  handleSearchChange(e) {
+    this.props.dispatch(searchStaffRecord(e.target.value.toLowerCase()));
   }
 
-  editRecord (e) {
-    console.log('edit')
+  editRecord(e) {
+    console.log("edit");
   }
 
-  archiveRecord (e) {
-    console.log('archive')
+  archiveRecord(e) {
+    console.log("archive");
   }
 
-  render () {
-    const { staff_record, searchTerm } = this.props
+  render() {
+    const { staff_record, searchTerm } = this.props;
+
     const filteredElements = staff_record
-      .filter(e => e.othernames.toLowerCase().includes(searchTerm) || e.surname.toLowerCase().includes(searchTerm))
+      .filter(
+        e =>
+          e.othernames.toLowerCase().includes(searchTerm) ||
+            e.surname.toLowerCase().includes(searchTerm)
+      )
       .map(record => {
-        let dob = new Date(record.date_of_birth)
-        let dateOfBirth = moment(dob).format('DD/MM/YYYY')
+        let dob = new Date(record.date_of_birth);
+
+        let dateOfBirth = moment(dob).format("DD/MM/YYYY");
+
         return (
           <div className="col-md-3" key={record.id}>
             <div className="card card-block">
               <ul className="list-unstyled">
-                <li className="h5">{record.othernames} {record.surname}</li>
+                <li className="h5">{record.othernames}{record.surname}</li>
                 <li>
                   <span className="badge badge-primary badge-pill float-right">{record.annual}</span>
                   Annual
@@ -52,32 +61,35 @@ class StaffRecordList extends Component {
                   Maternity
                 </li>
                 <li>
-                  <button onClick={this.editRecord.bind(this)} className="btn btn-link p-0">Edit</button>
-                  <button onClick={this.archiveRecord.bind(this)} className="btn btn-link">Archive</button>
+                  <button onClick={
+                    this.editRecord.bind(this)
+                  } className="btn btn-link p-0">Edit</button>
+                  <button onClick={
+                    this.archiveRecord.bind(this)
+                  } className="btn btn-link">Archive</button>
                 </li>
               </ul>
             </div>
-          </div>)
-        })
+          </div>
+        );
+      });
 
     return (
       <div className="StaffRecordList">
         <div className="row">
           <div className="col-md-3">
             <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search"
-                onChange={this.handleSearchChange.bind(this)} />
+              <input type="text" className="form-control" placeholder="Search" onChange={
+                this.handleSearchChange.bind(this)
+              } />
             </div>
           </div>
         </div>
         <div className="row">
-        {filteredElements}
+          {filteredElements}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -85,6 +97,6 @@ StaffRecordList.propTypes = {
   staff_record: PropTypes.array.isRequired,
   searchTerm: React.PropTypes.string,
   dispatch: PropTypes.func.isRequired
-}
+};
 
-export default StaffRecordList
+export default StaffRecordList;
