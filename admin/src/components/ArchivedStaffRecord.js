cbe293d11881201ae3c92cd44c9c3c65f29d329b
navinesh <navinesh.chand@gmail.com>
2017-01-13 14:01:12 +1300

    Change class name
    Add new class to align content. Replace tag class with badge.

--- admin/src/components/ArchivedStaffRecord.js
+++ admin/src/components/ArchivedStaffRecord.js
@@ -1,71 +1,79 @@
-import React, { PropTypes, Component } from 'react'
-const moment = require('moment')
-import { searchStaffRecord } from '../actions/StaffRecord'
+import React, { PropTypes, Component } from "react";
+
+const moment = require("moment");
+
+import { searchStaffRecord } from "../actions/StaffRecord";
 
 class ArchivedStaffRecordList extends Component {
-  handleSearchChange (e) {
-    this.props.dispatch(searchStaffRecord((e.target.value).toLowerCase()))
+  handleSearchChange(e) {
+    this.props.dispatch(searchStaffRecord(e.target.value.toLowerCase()));
   }
 
-  render () {
-    const { archived_staff_record, searchTerm } = this.props
+  render() {
+    const { archived_staff_record, searchTerm } = this.props;
+
     const filteredElements = archived_staff_record
-      .filter(e => e.othernames.toLowerCase().includes(searchTerm) || e.surname.toLowerCase().includes(searchTerm))
+      .filter(
+        e =>
+          e.othernames.toLowerCase().includes(searchTerm) ||
+            e.surname.toLowerCase().includes(searchTerm)
+      )
       .map(record => {
-        let dob = new Date(record.date_of_birth)
-        let dateOfBirth = moment(dob).format('DD/MM/YYYY')
+        let dob = new Date(record.date_of_birth);
+
+        let dateOfBirth = moment(dob).format("DD/MM/YYYY");
+
         return (
-          <div className="col-sm-3" key={record.id}>
+          <div className="col-md-3" key={record.id}>
             <div className="card">
               <ul className="list-group list-group-flush">
-                <li className="list-group-item">{record.othernames} {record.surname}</li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{record.annual}</span>
+                <li className="list-group-item">{record.othernames}{record.surname}</li>
+                <li className="list-group-item justify-content-between">
                   Annual
+                  <span className="badge badge-primary badge-pill">{record.annual}</span>
                 </li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{record.sick}</span>
+                <li className="list-group-item justify-content-between">
                   Sick
+                  <span className="badge badge-primary badge-pill">{record.sick}</span>
                 </li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{record.bereavement}</span>
+                <li className="list-group-item justify-content-between">
                   Bereavement
+                  <span className="badge badge-primary badge-pill">{record.bereavement}</span>
                 </li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{record.christmas}</span>
+                <li className="list-group-item justify-content-between">
                   Christmas
+                  <span className="badge badge-primary badge-pill">{record.christmas}</span>
                 </li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{dateOfBirth}</span>
+                <li className="list-group-item justify-content-between">
                   DOB
+                  <span className="badge badge-primary badge-pill">{dateOfBirth}</span>
                 </li>
-                <li className="list-group-item">
-                  <span className="tag tag-primary tag-pill float-xs-right">{record.maternity}</span>
+                <li className="list-group-item justify-content-between">
                   Maternity
+                  <span className="badge badge-primary badge-pill">{record.maternity}</span>
                 </li>
               </ul>
             </div>
-          </div>)
-        })
+          </div>
+        );
+      });
 
     return (
       <div className="ArchivedStaffRecordList">
         <div className="row">
-          <div className="col-sm-3">
+          <div className="col-md-3">
             <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search"
-                onChange={this.handleSearchChange.bind(this)} />
+              <input type="text" className="form-control" placeholder="Search" onChange={
+                this.handleSearchChange.bind(this)
+              } />
             </div>
           </div>
         </div>
         <div className="row">
-        {filteredElements}
+          {filteredElements}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -73,6 +81,6 @@ ArchivedStaffRecordList.propTypes = {
   archived_staff_record: PropTypes.array.isRequired,
   searchTerm: React.PropTypes.string,
   dispatch: PropTypes.func.isRequired
-}
+};
 
-export default ArchivedStaffRecordList
+export default ArchivedStaffRecordList;
