1e0b64e5ad9def6de7030d667bdbcf43743cb181
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:26:46 +1200

    Format code using prettier

--- user/src/index.js
+++ user/src/index.js
@@ -1,84 +1,83 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import axios from 'axios'
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import axios from "axios";
 
-import configureStore from './stores/configureStore'
+import configureStore from "./stores/configureStore";
 
-import Header from './containers/header'
-import Main from './containers/main'
-import StaffLeaveCalendar from './containers/staffleavecalendar'
-import ResetPassword from './containers/resetpassword'
-import UserChangePassword from './containers/changepassword'
-import UserError from './components/usererror'
-import LeaveApplication from './containers/leaveapplication'
+import Header from "./containers/header";
+import Main from "./containers/main";
+import StaffLeaveCalendar from "./containers/staffleavecalendar";
+import ResetPassword from "./containers/resetpassword";
+import UserChangePassword from "./containers/changepassword";
+import UserError from "./components/usererror";
+import LeaveApplication from "./containers/leaveapplication";
 
-import { requestUserLoginFromToken, loginUserErrorFromToken, receiveUserLoginFromToken } from './actions/userlogin'
+import {
+  requestUserLoginFromToken,
+  loginUserErrorFromToken,
+  receiveUserLoginFromToken
+} from "./actions/userlogin";
 
+import "./index.css";
+import "./bootstrap.min.css";
+import "./react-datepicker.css";
 
-import './bootstrap.min.css'
-import './react-datepicker.css'
-
-const store = configureStore()
+const store = configureStore();
 
 export const requireAuthentication = (nextState, replace, callback) => {
-  let auth_token = store.getState().userAuth.auth_info.auth_token
-  if(auth_token) {
-    store.dispatch(requestUserLoginFromToken(auth_token))
-    axios.post('http://localhost:8080/usertoken', {
-      auth_token: auth_token
-    })
-    .then((response) => {
-      if (response.status === 200){
-        if (location.pathname !== '/') {
-          replace('/')
-        }
-        localStorage.removeItem('auth_token')
-        store.dispatch(loginUserErrorFromToken(response.data))
-      }
-      else {
-        store.dispatch(receiveUserLoginFromToken(response.data))
-      }
-      callback()
-    })
-    .catch((error) => {
-      callback(error)
-    })
-  }
-  else {
-    auth_token = localStorage.getItem('auth_token')
-    if(auth_token) {
-      store.dispatch(requestUserLoginFromToken(auth_token))
-      axios.post('http://localhost:8080/usertoken', {
-        auth_token: auth_token
-      })
-      .then((response) => {
-        if (response.status === 200){
-          if (location.pathname !== '/') {
-            replace('/')
+  let auth_token = store.getState().userAuth.auth_info.auth_token;
+  if (auth_token) {
+    store.dispatch(requestUserLoginFromToken(auth_token));
+    axios
+      .post("http://localhost:8080/usertoken", { auth_token: auth_token })
+      .then(response => {
+        if (response.status === 200) {
+          if (location.pathname !== "/") {
+            replace("/");
           }
-          localStorage.removeItem('auth_token')
-          store.dispatch(loginUserErrorFromToken(response.data))
+          localStorage.removeItem("auth_token");
+          store.dispatch(loginUserErrorFromToken(response.data));
+        } else {
+          store.dispatch(receiveUserLoginFromToken(response.data));
         }
-        else {
-          store.dispatch(receiveUserLoginFromToken(response.data))
-        }
-        callback()
+        callback();
       })
-      .catch((error) => {
-        callback(error)
-      })
+      .catch(error => {
+        callback(error);
+      });
+  } else {
+    auth_token = localStorage.getItem("auth_token");
+    if (auth_token) {
+      store.dispatch(requestUserLoginFromToken(auth_token));
+      axios
+        .post("http://localhost:8080/usertoken", { auth_token: auth_token })
+        .then(response => {
+          if (response.status === 200) {
+            if (location.pathname !== "/") {
+              replace("/");
+            }
+            localStorage.removeItem("auth_token");
+            store.dispatch(loginUserErrorFromToken(response.data));
+          } else {
+            store.dispatch(receiveUserLoginFromToken(response.data));
+          }
+          callback();
+        })
+        .catch(error => {
+          callback(error);
+        });
     }
   }
-  let isAuthenticated = store.getState().userAuth.isAuthenticated
+  let isAuthenticated = store.getState().userAuth.isAuthenticated;
   if (!isAuthenticated) {
-    if (location.pathname !== '/') {
-      replace('/')
+    if (location.pathname !== "/") {
+      replace("/");
     }
-    callback()
+    callback();
   }
-}
+};
 
 render(
   <Provider store={store}>
@@ -87,11 +86,19 @@ render(
         <IndexRoute component={Main} />
         <Route path="/leavecalendar" component={StaffLeaveCalendar} />
         <Route path="/reset" component={ResetPassword} />
-        <Route path="/leaveapplication" onEnter={requireAuthentication} component={LeaveApplication} />
-        <Route path="/changepassword" onEnter={requireAuthentication} component={UserChangePassword} />
+        <Route
+          path="/leaveapplication"
+          onEnter={requireAuthentication}
+          component={LeaveApplication}
+        />
+        <Route
+          path="/changepassword"
+          onEnter={requireAuthentication}
+          component={UserChangePassword}
+        />
       </Route>
-      <Route path="*" component={UserError}/>
+      <Route path="*" component={UserError} />
     </Router>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
