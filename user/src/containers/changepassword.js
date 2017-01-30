39e6ba88559586116d8913ea730372ed07407156
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:33:30 +1200

    Format code

--- user/src/containers/changepassword.js
+++ user/src/containers/changepassword.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import UserChange from '../components/changepassword'
-import { changePassword, clearChangePasswordError } from '../actions/changepassword'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import UserChange from "../components/changepassword";
+import {
+  changePassword,
+  clearChangePasswordError
+} from "../actions/changepassword";
 
 class UserChangePassword extends Component {
   componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(clearChangePasswordError())
+    const { dispatch } = this.props;
+    dispatch(clearChangePasswordError());
   }
 
   render() {
-    const { dispatch, isAuthenticated, message, isFetching, auth_info } = this.props
+    const {
+      dispatch,
+      isAuthenticated,
+      message,
+      isFetching,
+      auth_info
+    } = this.props;
     return (
       <div className="UserAccount">
-        {isAuthenticated &&
-          <div className="col-xs-12 col-sm-4 offset-sm-4 pt-3">
-            <UserChange
-            isFetching={isFetching}
-            message={message}
-            auth_info={auth_info}
-            onChangeClick={ creds => dispatch(changePassword(creds)) } />
-          </div>
+        {
+          isAuthenticated && (
+              <div
+                className="col col-md-4 offset-md-4"
+                style={{ paddingTop: "80px" }}
+              >
+                <UserChange
+                  isFetching={isFetching}
+                  message={message}
+                  auth_info={auth_info}
+                  onChangeClick={creds => dispatch(changePassword(creds))}
+                />
+              </div>
+            )
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { changePassword } = state
-  const { userAuth } = state
-  const {
-    isAuthenticated,
-    auth_info
-  } = userAuth
-  const {
-    isFetching,
-    message
-  } = changePassword
+const mapStateToProps = state => {
+  const { changePassword } = state;
+  const { userAuth } = state;
+  const { isAuthenticated, auth_info } = userAuth;
+  const { isFetching, message } = changePassword;
 
-  return {
-    isAuthenticated,
-    auth_info,
-    isFetching,
-    message
-  }
-}
+  return { isAuthenticated, auth_info, isFetching, message };
+};
 
-export default connect(mapStateToProps)(UserChangePassword)
+export default connect(mapStateToProps)(UserChangePassword);
