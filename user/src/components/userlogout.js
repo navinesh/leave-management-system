0c22d67c6c1be5d7fb2d04ea228b770ce6fe9466
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:34:54 +1200

    Format code

--- user/src/components/userlogout.js
+++ user/src/components/userlogout.js
@@ -1,15 +1,13 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from "react";
 
 const Logout = ({ onLogoutClick }) => {
-  return(
+  return (
     <button onClick={() => onLogoutClick()} className="btn btn-primary-outline">
       Log out
     </button>
-  )
-}
+  );
+};
 
-Logout.propTypes = {
-  onLogoutClick: PropTypes.func.isRequired,
-}
+Logout.propTypes = { onLogoutClick: PropTypes.func.isRequired };
 
-export default Logout
+export default Logout;
