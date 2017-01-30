5514d600a81dd7548f7dff8cafa90f53b5494763
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:31:03 +1200

    Format code

--- user/src/containers/userlogin.js
+++ user/src/containers/userlogin.js
@@ -1,34 +1,30 @@
-import React  from 'react'
-import { connect } from 'react-redux'
-import { fetchLogin } from '../actions/userlogin'
-import Login from '../components/userlogin'
+import React from "react";
+import { connect } from "react-redux";
+import { fetchLogin } from "../actions/userlogin";
+import Login from "../components/userlogin";
 
 const UserLogin = ({ dispatch, message, isAuthenticated, isFetching }) => {
   return (
     <div className="UserLogin">
-      {!isAuthenticated &&
-        <Login
-        isFetching={isFetching}
-        message={message}
-        onLoginClick={ creds => dispatch(fetchLogin(creds)) } />
+      {
+        !isAuthenticated &&
+          (
+            <Login
+              isFetching={isFetching}
+              message={message}
+              onLoginClick={creds => dispatch(fetchLogin(creds))}
+            />
+          )
       }
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  const { userAuth } = state
-  const {
-    isAuthenticated,
-    message,
-    isFetching
-  } = userAuth
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { isAuthenticated, message, isFetching } = userAuth;
 
-  return {
-    message,
-    isAuthenticated,
-    isFetching
-  }
-}
+  return { message, isAuthenticated, isFetching };
+};
 
-export default connect(mapStateToProps)(UserLogin)
+export default connect(mapStateToProps)(UserLogin);
