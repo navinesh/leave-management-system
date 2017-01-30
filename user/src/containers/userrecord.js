2821e1e132bb9cb40400f9d76c85fca53fe5c229
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:30:02 +1200

    Format code andadd boostrap alpha 6 class

--- user/src/containers/userrecord.js
+++ user/src/containers/userrecord.js
@@ -1,60 +1,65 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchUserDetailsIfNeeded } from '../actions/userdetails'
-import { fetchUserRecordIfNeeded } from '../actions/userrecord'
-import { UserRecord, RecordList } from '../components/userrecord'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchUserDetailsIfNeeded } from "../actions/userdetails";
+import { fetchUserRecordIfNeeded } from "../actions/userrecord";
+import { UserRecord, RecordList } from "../components/userrecord";
 
-const BeatLoader = require('halogen/BeatLoader');
-const PulseLoader = require('halogen/PulseLoader');
+const BeatLoader = require("halogen/BeatLoader");
+const PulseLoader = require("halogen/PulseLoader");
 
 class UserRecords extends Component {
   componentDidMount() {
-    const { dispatch, auth_info } = this.props
+    const { dispatch, auth_info } = this.props;
     let auth_token = auth_info.auth_token;
-    if(auth_token) {
-      dispatch(fetchUserDetailsIfNeeded(auth_token))
-      dispatch(fetchUserRecordIfNeeded(auth_token))
-    }
-    else {
-      auth_token= localStorage.getItem('auth_token')
-      if(auth_token) {
-        dispatch(fetchUserDetailsIfNeeded(auth_token))
-        dispatch(fetchUserRecordIfNeeded(auth_token))
+    if (auth_token) {
+      dispatch(fetchUserDetailsIfNeeded(auth_token));
+      dispatch(fetchUserRecordIfNeeded(auth_token));
+    } else {
+      auth_token = localStorage.getItem("auth_token");
+      if (auth_token) {
+        dispatch(fetchUserDetailsIfNeeded(auth_token));
+        dispatch(fetchUserRecordIfNeeded(auth_token));
       }
     }
   }
 
-
   render() {
-    const { isFetching, isRecordFetching, user_detail, user_record, message } = this.props
+    const {
+      isFetching,
+      isRecordFetching,
+      user_detail,
+      user_record,
+      message
+    } = this.props;
+
     return (
       <div className="UserRecords">
-        {isFetching ? <div className="text-xs-center">
-          <BeatLoader color="#0275d8" size="12px" /></div> :
-          <UserRecord user_detail={user_detail} message={message} />
+        {
+          isFetching ? <div className="text-center">
+              <BeatLoader color="#0275d8" size="12px" />
+            </div> : <UserRecord user_detail={user_detail} message={message} />
         }
-        {isRecordFetching ? <div className="text-xs-center" style={{paddingTop: '100px'}}>
-          <PulseLoader color="#0275d8" size="12px" /></div> :
-          <div className="container">
-            <div className="row">
-              <RecordList user_record={user_record} />
+        {
+          isRecordFetching
+            ? <div className="text-center" style={{ paddingTop: "100px" }}>
+              <PulseLoader color="#0275d8" size="12px" />
             </div>
-          </div>
+            : <RecordList user_record={user_record} />
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { userRecords, userAuth, userDetails } = state
-  const { auth_info } = userAuth
-  const { isFetching, userDetail: user_detail } = userDetails
+const mapStateToProps = state => {
+  const { userRecords, userAuth, userDetails } = state;
+  const { auth_info } = userAuth;
+  const { isFetching, userDetail: user_detail } = userDetails;
   const {
     isFetching: isRecordFetching,
     userRecord: user_record,
     message
-  } = userRecords
+  } = userRecords;
 
   return {
     isFetching,
@@ -63,7 +68,7 @@ const mapStateToProps = (state) => {
     message,
     auth_info,
     user_detail
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(UserRecords)
+export default connect(mapStateToProps)(UserRecords);
