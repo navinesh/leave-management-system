1dea3f7cb8540d6b537a5685c52aec30c6ed7269
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:30:12 +1200

    Format code andadd boostrap alpha 6 class

--- user/src/containers/userloginbox.js
+++ user/src/containers/userloginbox.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import UserLoginBox from './userlogincontainer'
+import React from "react";
+import { connect } from "react-redux";
+import UserLoginBox from "./userlogincontainer";
 
 const MainUserLoginBox = ({ isAuthenticated }) => {
   return (
     <div className="MainView">
-      {!isAuthenticated &&
-        <div className="row">
-          <div className="col-xs-12 col-sm-4 offset-sm-4">
-            <UserLoginBox />
-          </div>
-        </div>
+      {
+        !isAuthenticated && (
+            <div className="row">
+              <div className="col-md-4 offset-md-4">
+                <UserLoginBox />
+              </div>
+            </div>
+          )
       }
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  const { userAuth } = state
-  const { isAuthenticated } = userAuth
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { isAuthenticated } = userAuth;
 
-  return { isAuthenticated }
-}
+  return { isAuthenticated };
+};
 
-export default connect(mapStateToProps)(MainUserLoginBox)
+export default connect(mapStateToProps)(MainUserLoginBox);
