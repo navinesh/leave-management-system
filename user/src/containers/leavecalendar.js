7a78412e12078545d57b90bae2c3f76b84cd41f7
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:32:27 +1200

    Format code and add boostrap alpha 6 class

--- user/src/containers/leavecalendar.js
+++ user/src/containers/leavecalendar.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchLeaveIfNeeded } from '../actions/leavecalendar'
-import Leaves from '../components/leavecalendar'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchLeaveIfNeeded } from "../actions/leavecalendar";
+import Leaves from "../components/leavecalendar";
 
-const BeatLoader = require('halogen/BeatLoader');
+const BeatLoader = require("halogen/BeatLoader");
 
 class LeaveCalendar extends Component {
   componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(fetchLeaveIfNeeded())
+    const { dispatch } = this.props;
+    dispatch(fetchLeaveIfNeeded());
   }
 
   render() {
-    const { records, isFetching } = this.props
+    const { records, isFetching } = this.props;
     return (
       <div className="LeaveCalendar">
-        {isFetching ?
-          <div className="text-xs-center"><BeatLoader color="#0275d8" size="12px" /></div> :
-          <Leaves records={records} />
+        {
+          isFetching
+            ? <div className="text-center">
+              <BeatLoader color="#0275d8" size="12px" />
+            </div>
+            : <Leaves records={records} />
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { leaveRecords } = state
-  const {
-    isFetching,
-    items: records
-  } = leaveRecords
+const mapStateToProps = state => {
+  const { leaveRecords } = state;
+  const { isFetching, items: records } = leaveRecords;
 
-  return {
-    records,
-    isFetching
-  }
-}
+  return { records, isFetching };
+};
 
-export default connect(mapStateToProps)(LeaveCalendar)
+export default connect(mapStateToProps)(LeaveCalendar);
