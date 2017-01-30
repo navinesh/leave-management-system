ba6dccf36f67ee2795f65e6f617d51f2629f3f6b
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:33:16 +1200

    Format code

--- user/src/containers/header.js
+++ user/src/containers/header.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Navs from '../components/header'
+import React from "react";
+import { connect } from "react-redux";
+import Navs from "../components/header";
 
 const Header = ({ isAuthenticated, dispatch, children }) => {
   return (
     <div className="Header">
-      <Navs
-        isAuthenticated={isAuthenticated}
-        dispatch={dispatch} />
+      <Navs isAuthenticated={isAuthenticated} dispatch={dispatch} />
       {children}
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  const { userAuth } = state
-  const { isAuthenticated } = userAuth
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { isAuthenticated } = userAuth;
 
-  return { isAuthenticated }
-}
+  return { isAuthenticated };
+};
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
