0d55e6d100108bf64642f139e36f759cf2f8ee5f
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:35:08 +1200

    Format code

--- user/src/components/usererror.js
+++ user/src/components/usererror.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default () =>
+export default () => (
   <div className="container text-xs-center">
     <div className="offset-sm-2 col-sm-8">
       <h2 className="display-2">404</h2>
       <h4 className="display-3">That's an error.</h4>
-      <h5 className="display-4">The site configured at this address does not contain the requested file.</h5>
+      <h5 className="display-4">
+        The site configured at this address does not contain the requested file.
+      </h5>
     </div>
   </div>
+);
