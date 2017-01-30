1c078dbb73d57c0d636d7540be5b910c6f8a5ac9
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:32:17 +1200

    Format code and add boostrap alpha 6 class

--- user/src/containers/main.js
+++ user/src/containers/main.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import LeaveCalendar from './leavecalendar'
-import UserLogin from './userlogin'
-import UserRecord from './userrecord'
+import React from "react";
+import { connect } from "react-redux";
+import LeaveCalendar from "./leavecalendar";
+import UserLogin from "./userlogin";
+import UserRecord from "./userrecord";
 
 const Main = ({ isAuthenticated }) => {
   return (
     <div className="Main">
-      {!isAuthenticated &&
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-8">
-              <LeaveCalendar />
+      {
+        !isAuthenticated && (
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8">
+                  <LeaveCalendar />
+                </div>
+                <div className="col-md-4">
+                  <UserLogin />
+                </div>
+              </div>
             </div>
-            <div className="col-sm-4">
-              <UserLogin />
-            </div>
-          </div>
-        </div>
+          )
       }
-      {isAuthenticated &&
-        <UserRecord />
-      }
+      {isAuthenticated && <UserRecord />}
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  const { userAuth } = state
-  const { isAuthenticated } = userAuth
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { isAuthenticated } = userAuth;
 
-  return { isAuthenticated }
-}
+  return { isAuthenticated };
+};
 
-export default connect(mapStateToProps)(Main)
+export default connect(mapStateToProps)(Main);
