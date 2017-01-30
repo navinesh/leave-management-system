ca1ab195e1e537f525cb67c31179c3266e7ef61a
navinesh <navinesh.chand@gmail.com>
2017-01-30 15:31:40 +1200

    Format code and add boostrap alpha 6 class

--- user/src/containers/resetpassword.js
+++ user/src/containers/resetpassword.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import UserResetPassword from '../components/resetpassword'
-import { resetPassword } from '../actions/resetpassword'
+import React from "react";
+import { connect } from "react-redux";
+import UserResetPassword from "../components/resetpassword";
+import { resetPassword } from "../actions/resetpassword";
 
-const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching  }) => {
+const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching }) => {
   return (
     <div className="ResetPassword">
-      {!isAuthenticated &&
-        <div className="col-xs-12 col-sm-4 offset-sm-4 pt-3">
-          <UserResetPassword
-          isFetching={isFetching}
-          message={message}
-          onResetClick={ email => dispatch(resetPassword(email)) } />
-        </div>
+      {
+        !isAuthenticated && (
+            <div
+              className="col col-md-4 offset-md-4"
+              style={{ paddingTop: "100px" }}
+            >
+              <UserResetPassword
+                isFetching={isFetching}
+                message={message}
+                onResetClick={email => dispatch(resetPassword(email))}
+              />
+            </div>
+          )
       }
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  const { userAuth } = state
-  const {
-    isAuthenticated,
-    isFetching,
-  } = userAuth
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { isAuthenticated, isFetching } = userAuth;
 
-  return {
-    isAuthenticated,
-    isFetching
-  }
-}
+  return { isAuthenticated, isFetching };
+};
 
-export default connect(mapStateToProps)(ResetPassword)
+export default connect(mapStateToProps)(ResetPassword);
