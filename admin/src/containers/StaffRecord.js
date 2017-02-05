552f3ff83b5d75c6620450f8ae325a84c179e1e5
navinesh <navinesh.chand@gmail.com>
2017-02-06 09:40:46 +1200

    Add modify user reducer

--- admin/src/containers/StaffRecord.js
+++ admin/src/containers/StaffRecord.js
@@ -5,6 +5,7 @@ import {
   clearSearchStaffRecord
 } from "../actions/StaffRecord";
 import StaffRecordList from "../components/StaffRecord";
+import { submitModifyUserRecord } from "../actions/ModifyRecord";
 
 class StaffRecord extends Component {
   componentDidMount() {
@@ -18,7 +19,14 @@ class StaffRecord extends Component {
   }
 
   render() {
-    const { staff_record, isAuthenticated, searchTerm, dispatch } = this.props;
+    const {
+      staff_record,
+      isAuthenticated,
+      searchTerm,
+      dispatch,
+      isFetching,
+      message
+    } = this.props;
 
     return (
       <div className="container">
@@ -29,6 +37,10 @@ class StaffRecord extends Component {
                 staff_record={staff_record}
                 searchTerm={searchTerm}
                 dispatch={dispatch}
+                isFetching={isFetching}
+                message={message}
+                onModifyUserRecordSubmit={modifyUserDetails =>
+                  dispatch(submitModifyUserRecord(modifyUserDetails))}
               />
             )
         }
@@ -38,12 +50,13 @@ class StaffRecord extends Component {
 }
 
 const mapStateToProps = state => {
-  const { staffRecord, searchStaffRecord, adminAuth } = state;
+  const { staffRecord, searchStaffRecord, adminAuth, modifyUser } = state;
   const { staff_record } = staffRecord;
   const { isAuthenticated } = adminAuth;
   const { searchTerm } = searchStaffRecord;
+  const { isFetching, message } = modifyUser;
 
-  return { staff_record, isAuthenticated, searchTerm };
+  return { staff_record, isAuthenticated, searchTerm, isFetching, message };
 };
 
 export default connect(mapStateToProps)(StaffRecord);
