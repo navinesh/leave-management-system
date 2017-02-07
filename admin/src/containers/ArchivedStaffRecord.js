50f1f43a79473b6691eeed4b8cf5259f6175af27
navinesh <navinesh.chand@gmail.com>
2017-02-08 11:02:58 +1200

    Add unarchived state

--- admin/src/containers/ArchivedStaffRecord.js
+++ admin/src/containers/ArchivedStaffRecord.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { fetchArchivedStaffRecord } from "../actions/ArchivedStaffRecord";
 import { clearSearchStaffRecord } from "../actions/StaffRecord";
 import ArchivedStaffRecordList from "../components/ArchivedStaffRecord";
+import { submitUnArchiveUser } from "../actions/UnArchiveUser";
 
 class ArchivedStaffRecord extends Component {
   componentDidMount() {
@@ -20,32 +21,46 @@ class ArchivedStaffRecord extends Component {
       archived_staff_record,
       isAuthenticated,
       searchTerm,
-      dispatch
+      dispatch,
+      isUnArchiveFetching,
+      unArchiveMessage
     } = this.props;
     return (
       <div className="container">
-        {
-          isAuthenticated &&
-            (
-              <ArchivedStaffRecordList
-                archived_staff_record={archived_staff_record}
-                searchTerm={searchTerm}
-                dispatch={dispatch}
-              />
-            )
-        }
+        {isAuthenticated &&
+          <ArchivedStaffRecordList
+            archived_staff_record={archived_staff_record}
+            searchTerm={searchTerm}
+            dispatch={dispatch}
+            isUnArchiveFetching={isUnArchiveFetching}
+            unArchiveMessage={unArchiveMessage}
+            onUnArchiveUserSubmit={unArchiveUser =>
+              dispatch(submitUnArchiveUser(unArchiveUser))}
+          />}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { archivedStaffRecord, searchStaffRecord, adminAuth } = state;
+  const {
+    archivedStaffRecord,
+    searchStaffRecord,
+    adminAuth,
+    unArchiveUser
+  } = state;
   const { archived_staff_record } = archivedStaffRecord;
   const { isAuthenticated } = adminAuth;
   const { searchTerm } = searchStaffRecord;
+  const { isUnArchiveFetching, unArchiveMessage } = unArchiveUser;
 
-  return { archived_staff_record, isAuthenticated, searchTerm };
+  return {
+    archived_staff_record,
+    isAuthenticated,
+    searchTerm,
+    isUnArchiveFetching,
+    unArchiveMessage
+  };
 };
 
-export default connect(mapStateToProps)(ArchivedStaffRecord)
+export default connect(mapStateToProps)(ArchivedStaffRecord);
