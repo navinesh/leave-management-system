6819af9b67b9e73a71d67f731b2554b315a7f373
navinesh <navinesh.chand@gmail.com>
2017-02-15 09:46:35 +1200

    Add filter to check for current and future approved dates only

--- user/src/components/leavecalendar.js
+++ user/src/components/leavecalendar.js
@@ -1,38 +1,66 @@
 import React, { PropTypes } from "react";
 
+const moment = require("moment");
+
 const RecordList = ({ records }) => {
-  var itemNodes = records.map(record => {
-    return (
-      <tr key={record.id}>
-        <td>{record.user.othernames} {record.user.surname}</td>
-        <td>{record.leave_name}</td>
-        <td>{record.start_date}</td>
-        <td>{record.end_date}</td>
-        <td>{record.leave_days}</td>
-      </tr>
-    );
-  });
-  return (
-    <div className="table-responsive">
-      <table
-        className="table table-bordered table-hover"
-        style={{ backgroundColor: "#FFFFFF" }}
-      >
-        <thead className="thead-default">
-          <tr>
-            <th>Name</th>
-            <th>Leave type</th>
-            <th>Start date</th>
-            <th>End date</th>
-            <th>Leave days</th>
-          </tr>
-        </thead>
-        <tbody>
-          {itemNodes}
-        </tbody>
-      </table>
-    </div>
-  );
+  const itemNodes = records
+    .filter(record => {
+      // get current date and format it
+      let dateToday = moment();
+
+      let todayDate = dateToday.format("DD/MM/YYYY");
+
+      // get end date and format it
+      let end_Date = moment(record.end_date, "DD/MM/YYYY").format("DD/MM/YYYY");
+
+      // check if current date and end date is same
+      let isCurrentDate = todayDate === end_Date ? true : false;
+
+      // check if end date is same or greater than current date
+      let eDate = moment(record.end_date, "DD/MM/YYYY").format("MM/DD/YYYY");
+
+      let endDate = moment(new Date(eDate));
+
+      let isEndDate = endDate.isSameOrAfter(dateToday);
+
+      // return true for current and future leaves
+      return isCurrentDate || isEndDate ? true : false;
+    })
+    .map(data => {
+      return (
+        <tr key={data.id}>
+          <td>{data.user.othernames} {data.user.surname}</td>
+          <td>{data.leave_name}</td>
+          <td>{data.start_date}</td>
+          <td>{data.end_date}</td>
+          <td>{data.leave_days}</td>
+        </tr>
+      );
+    });
+
+  return itemNodes.length > 0
+    ? <div className="table-responsive">
+        <table
+          className="table table-bordered table-hover"
+          style={{ backgroundColor: "#FFFFFF" }}
+        >
+          <thead className="thead-default">
+            <tr>
+              <th>Name</th>
+              <th>Leave type</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>Leave days</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemNodes}
+          </tbody>
+        </table>
+      </div>
+    : <div className="container text-center" style={{ paddingTop: "100px" }}>
+        <h1 className="display-3">There are no approved leave record.</h1>
+      </div>;
 };
 
 const Leaves = ({ records }) => {
