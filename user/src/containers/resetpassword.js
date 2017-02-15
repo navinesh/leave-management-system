43225b41842a158234e6ee8a4f67738615f4ef95
navinesh <navinesh.chand@gmail.com>
2017-02-16 07:47:15 +1200

    Remove return statement

--- user/src/containers/resetpassword.js
+++ user/src/containers/resetpassword.js
@@ -3,23 +3,18 @@ import { connect } from "react-redux";
 import UserResetPassword from "../components/resetpassword";
 import { resetPassword } from "../actions/resetpassword";
 
-const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching }) => {
-  return (
-    <div className="ResetPassword">
-      {
-        !isAuthenticated && (
-            <div className="col col-md-4 offset-md-4">
-              <UserResetPassword
-                isFetching={isFetching}
-                message={message}
-                onResetClick={email => dispatch(resetPassword(email))}
-              />
-            </div>
-          )
-      }
-    </div>
-  );
-};
+const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching }) => (
+  <div className="ResetPassword">
+    {!isAuthenticated &&
+      <div className="col col-md-4 offset-md-4">
+        <UserResetPassword
+          isFetching={isFetching}
+          message={message}
+          onResetClick={email => dispatch(resetPassword(email))}
+        />
+      </div>}
+  </div>
+);
 
 const mapStateToProps = state => {
   const { userAuth } = state;
