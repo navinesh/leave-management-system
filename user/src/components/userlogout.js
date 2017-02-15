8febce782e98cc969b28ccc840fefeea680c8304
navinesh <navinesh.chand@gmail.com>
2017-02-16 07:49:29 +1200

    Remove return statement

--- user/src/components/userlogout.js
+++ user/src/components/userlogout.js
@@ -1,12 +1,10 @@
 import React, { PropTypes } from "react";
 
-const Logout = ({ onLogoutClick }) => {
-  return (
-    <button onClick={() => onLogoutClick()} className="btn btn-primary-outline">
-      Log out
-    </button>
-  );
-};
+const Logout = ({ onLogoutClick }) => (
+  <button onClick={() => onLogoutClick()} className="btn btn-primary-outline">
+    Log out
+  </button>
+);
 
 Logout.propTypes = { onLogoutClick: PropTypes.func.isRequired };
 
