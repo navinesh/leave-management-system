015dfcc16fbd367a1ea7a3b20c7228a77a825d3e
navinesh <navinesh.chand@gmail.com>
2017-02-16 07:47:38 +1200

    Format code

--- user/src/containers/main.js
+++ user/src/containers/main.js
@@ -7,20 +7,17 @@ import UserRecord from "./userrecord";
 const Main = ({ isAuthenticated }) => {
   return (
     <div className="Main">
-      {
-        !isAuthenticated && (
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8">
-                  <LeaveCalendar />
-                </div>
-                <div className="col-md-4">
-                  <UserLogin />
-                </div>
-              </div>
+      {!isAuthenticated &&
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <LeaveCalendar />
             </div>
-          )
-      }
+            <div className="col-md-4">
+              <UserLogin />
+            </div>
+          </div>
+        </div>}
       {isAuthenticated && <UserRecord />}
     </div>
   );
