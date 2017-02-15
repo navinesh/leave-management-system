92d447e04ed8675fe5353b746fc28852442e8db2
navinesh <navinesh.chand@gmail.com>
2017-02-16 07:46:19 +1200

    Remove return statement

--- user/src/containers/userloginbox.js
+++ user/src/containers/userloginbox.js
@@ -2,21 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import UserLoginBox from "./userlogincontainer";
 
-const MainUserLoginBox = ({ isAuthenticated }) => {
-  return (
-    <div className="MainView">
-      {
-        !isAuthenticated && (
-            <div className="row">
-              <div className="col-md-4 offset-md-4">
-                <UserLoginBox />
-              </div>
-            </div>
-          )
-      }
-    </div>
-  );
-};
+const MainUserLoginBox = ({ isAuthenticated }) => (
+  <div className="MainView">
+    {!isAuthenticated &&
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <UserLoginBox />
+        </div>
+      </div>}
+  </div>
+);
 
 const mapStateToProps = state => {
   const { userAuth } = state;
