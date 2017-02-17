4a1fd2a8926649c002f2d1523e2b2cfe1af91401
navinesh <navinesh.chand@gmail.com>
2017-02-17 15:13:50 +1200

    Get public holiday

--- admin/src/containers/PendingLeave.js
+++ admin/src/containers/PendingLeave.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPendingLeave } from "../actions/PendingLeave";
+import { fetchPublicHoliday } from "../actions/PublicHoliday";
 import PendingLeaveList from "../components/PendingLeave";
 
 const BeatLoader = require("halogen/BeatLoader");
@@ -9,28 +10,39 @@ class PendingLeave extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(fetchPendingLeave());
+    dispatch(fetchPublicHoliday());
   }
 
   render() {
-    const { isFetching, pending_items, isAuthenticated } = this.props;
+    const {
+      isFetching,
+      pending_items,
+      isAuthenticated,
+      public_holiday
+    } = this.props;
     return (
       <div className="container">
-        {isAuthenticated && (isFetching ? (
-                <div className="text-center">
-                  <BeatLoader color="#0275d8" size="12px" />
-                </div>
-              ) : <PendingLeaveList pending_items={pending_items} />)}
+        {isAuthenticated &&
+          (isFetching
+            ? <div className="text-center">
+                <BeatLoader color="#0275d8" size="12px" />
+              </div>
+            : <PendingLeaveList
+                pending_items={pending_items}
+                public_holiday={public_holiday}
+              />)}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { pendingLeave, adminAuth } = state;
+  const { pendingLeave, adminAuth, publicHoliday } = state;
   const { isFetching, pending_items } = pendingLeave;
   const { isAuthenticated } = adminAuth;
+  const { public_holiday } = publicHoliday;
 
-  return { isFetching, pending_items, isAuthenticated };
+  return { isFetching, pending_items, isAuthenticated, public_holiday };
 };
 
-export default connect(mapStateToProps)(PendingLeave)
+export default connect(mapStateToProps)(PendingLeave);
