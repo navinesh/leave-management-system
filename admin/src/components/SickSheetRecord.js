450a814839f94bd9f18758f533a819e0034afdc8
navinesh <navinesh.chand@gmail.com>
2017-02-20 13:15:11 +1200

    Delete return statement

--- admin/src/components/SickSheetRecord.js
+++ admin/src/components/SickSheetRecord.js
@@ -2,25 +2,23 @@ import React, { PropTypes } from "react";
 import { Link } from "react-router";
 
 const SickSheetList = ({ sickSheet_items }) => {
-  const itemNodes = sickSheet_items.map(record => {
-    return (
-      <tr key={record.id}>
-        <td>{record.user.othernames}{" "}{record.user.surname}</td>
-        <td>{record.start_date}</td>
-        <td>{record.end_date}</td>
-        <td>{record.leave_days}</td>
-        <td>{record.date_posted}</td>
-        <td>
-          <Link
-            className="btn btn-primary btn-sm"
-            to={`/sicksheetrecord/${record.file_name}`}
-          >
-            Download
-          </Link>
-        </td>
-      </tr>
-    );
-  });
+  const itemNodes = sickSheet_items.map(record => (
+    <tr key={record.id}>
+      <td>{record.user.othernames}{" "}{record.user.surname}</td>
+      <td>{record.start_date}</td>
+      <td>{record.end_date}</td>
+      <td>{record.leave_days}</td>
+      <td>{record.date_posted}</td>
+      <td>
+        <Link
+          className="btn btn-primary btn-sm"
+          to={`/sicksheetrecord/${record.file_name}`}
+        >
+          Download
+        </Link>
+      </td>
+    </tr>
+  ));
 
   return (
     <div className="table-responsive">
