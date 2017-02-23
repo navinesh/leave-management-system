1f547cffcda900c14aca8365de365ec6ff6fa482
navinesh <navinesh.chand@gmail.com>
2017-02-23 16:03:11 +1200

    Add condition to check for empty array

--- admin/src/components/SickSheetRecord.js
+++ admin/src/components/SickSheetRecord.js
@@ -20,28 +20,32 @@ const SickSheetList = ({ sickSheet_items }) => {
     </tr>
   ));
 
-  return (
-    <div className="table-responsive">
-      <table
-        className="table table-bordered table-hover"
-        style={{ backgroundColor: "#FFFFFF" }}
-      >
-        <thead className="thead-default">
-          <tr>
-            <th>Name</th>
-            <th>Start date</th>
-            <th>End date</th>
-            <th>Leave days</th>
-            <th>Date applied</th>
-            <th>Sick sheet</th>
-          </tr>
-        </thead>
-        <tbody>
-          {itemNodes}
-        </tbody>
-      </table>
-    </div>
-  );
+  return itemNodes.length > 0
+    ? <div className="table-responsive">
+        <table
+          className="table table-bordered table-hover"
+          style={{ backgroundColor: "#FFFFFF" }}
+        >
+          <thead className="thead-default">
+            <tr>
+              <th>Name</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>Leave days</th>
+              <th>Date applied</th>
+              <th>Sick sheet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemNodes}
+          </tbody>
+        </table>
+      </div>
+    : <div className="text-center" style={{ paddingTop: "40px" }}>
+        <h1 className="display-4">
+          There are no approved sick leave record with medical certificate.
+        </h1>
+      </div>;
 };
 
 SickSheetList.propTypes = { sickSheet_items: PropTypes.array.isRequired };
