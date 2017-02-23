560325598754eca17604ffc4a4329bdb891e1598
navinesh <navinesh.chand@gmail.com>
2017-02-23 16:03:57 +1200

    Add an event handler to approve leave

--- admin/src/components/PendingLeave.js
+++ admin/src/components/PendingLeave.js
@@ -47,6 +47,7 @@ class PendingLeaveList extends Component {
     this.handleCloseModal1 = this.handleCloseModal1.bind(this);
     this.handleOpenModal2 = this.handleOpenModal2.bind(this);
     this.handleCloseModal2 = this.handleCloseModal2.bind(this);
+    this.handleApproveLeave = this.handleApproveLeave.bind(this);
     this.handleDeclineReason = this.handleDeclineReason.bind(this);
     this.handleDeclineSubmit = this.handleDeclineSubmit.bind(this);
     this.handleEditSubmit = this.handleEditSubmit.bind(this);
@@ -87,6 +88,25 @@ class PendingLeaveList extends Component {
 
   handleDeclineReason(e) {
     this.setState({ declineReason: e.target.value });
+  }
+
+  handleApproveLeave(e) {
+    const leaveID = e.target.id ? e.target.id : null;
+    const LeaveStatus = "approved";
+
+    if (!leaveID) {
+      this.setState({
+        errorMessage: "Could not get leave id!"
+      });
+      return;
+    }
+    console.log(leaveID);
+    const approveLeaveData = {
+      leaveID: leaveID,
+      LeaveStatus: LeaveStatus
+    };
+
+    this.props.onApproveLeaveSubmit(approveLeaveData);
   }
 
   handleDeclineSubmit(e) {
@@ -265,7 +285,7 @@ class PendingLeaveList extends Component {
         <td>
           <button
             className="btn btn-link"
-            onClick={this.handleOpenModal1}
+            onClick={this.handleApproveLeave}
             id={record.id}
           >
             Approve
@@ -292,226 +312,229 @@ class PendingLeaveList extends Component {
       </tr>
     ));
 
-    return (
-      <div className="row">
-        <div className="table-responsive">
-          <table
-            className="table table-bordered table-hover"
-            style={{ backgroundColor: "#FFFFFF" }}
-          >
-            <thead className="thead-default">
-              <tr>
-                <th>Name</th>
-                <th>Leave</th>
-                <th>Type</th>
-                <th>Start date</th>
-                <th>End date</th>
-                <th>Leave days</th>
-                <th>Reason</th>
-                <th>Approve</th>
-                <th>Decline</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {itemNodes}
-            </tbody>
-          </table>
-        </div>
-        {this.props.pending_items.filter(e => e.id === listID).map(record => (
-          <div key={record.id}>
-            <Modal
-              className="Modal__Bootstrap modal-dialog"
-              isOpen={this.state.showModal1}
-              onRequestClose={this.handleCloseModal1}
-              contentLabel="Modal #1"
-              overlayClassName="Overlay"
-              style={customStyles}
+    return itemNodes.length > 0
+      ? <div className="row">
+          <div className="table-responsive">
+            <table
+              className="table table-bordered table-hover"
+              style={{ backgroundColor: "#FFFFFF" }}
             >
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">
-                    Decline
-                  </h5>
-                </div>
-                <form onSubmit={this.handleDeclineSubmit}>
-                  <div className="modal-body">
-                    <p>{record.user.othernames}{" "}{record.user.surname}</p>
-                    <div className="form-group">
-                      <label htmlFor="reason">Reason</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter reason"
-                        id="reason"
-                        onChange={this.handleDeclineReason}
-                      />
-                    </div>
-                  </div>
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn btn-outline-primary"
-                      onClick={this.handleCloseModal1}
-                    >
-                      Close
-                    </button>
-                    <button type="submit" className="btn btn-primary">
-                      Decline
-                    </button>
-                  </div>
-                </form>
-                <div className="text-danger text-center">
-                  <div className="pb-4">{this.state.errorMessage}</div>
-                </div>
-              </div>
-            </Modal>
+              <thead className="thead-default">
+                <tr>
+                  <th>Name</th>
+                  <th>Leave</th>
+                  <th>Type</th>
+                  <th>Start date</th>
+                  <th>End date</th>
+                  <th>Leave days</th>
+                  <th>Reason</th>
+                  <th>Approve</th>
+                  <th>Decline</th>
+                  <th>Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                {itemNodes}
+              </tbody>
+            </table>
           </div>
-        ))}
-        {this.props.pending_items.filter(e => e.id === listID).map(record => (
-          <div key={record.id}>
-            <Modal
-              className="Modal__Bootstrap modal-dialog"
-              isOpen={this.state.showModal2}
-              onRequestClose={this.handleCloseModal2}
-              contentLabel="Modal #2"
-              overlayClassName="Overlay"
-              style={customStyles}
-            >
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">
-                    Edit
-                  </h5>
-                </div>
-                <form
-                  encType="multipart/form-data"
-                  onSubmit={this.handleEditSubmit}
-                >
-                  <div className="modal-body">
-                    <div className="row">
-                      <div className="col">
-                        <p className="h5">
-                          {record.user.othernames}{" "}{record.user.surname}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="leave">
-                            Leave
-                          </label>
-                          <select
-                            className="form-control"
-                            id="leave"
-                            defaultValue={record.leave_name}
-                            ref={select => this.leave_name = select}
-                          >
-                            <option>{record.leave_name}</option>
-                            <option>annual</option>
-                            <option>sick</option>
-                            <option>bereavement</option>
-                            <option>christmas</option>
-                            <option>birthday</option>
-                            {record.user.gender === "female"
-                              ? <option>maternity</option>
-                              : null}
-                            <option>lwop</option>
-                            <option>other</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="leave type">Leave type</label>
-                          <select
-                            className="form-control"
-                            id="leave type"
-                            defaultValue={record.leave_type}
-                            ref={select => this.leave_type = select}
-                          >
-                            <option>{record.leave_type}</option>
-                            <option>full</option>
-                            <option>half day am</option>
-                            <option>half day pm</option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="startDate">Start date</label>
-                          <DatePicker
-                            className="form-control"
-                            placeholderText="Click to select a date"
-                            selected={this.state.startDate}
-                            startDate={this.state.startDate}
-                            endDate={this.state.endDate}
-                            onChange={this.handleStartDateChange}
-                            showMonthDropdown
-                            dropdownMode="select"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="endDate">End date</label>
-                          <DatePicker
-                            className="form-control"
-                            placeholderText="Click to select a date"
-                            selected={this.state.endDate}
-                            startDate={this.state.startDate}
-                            endDate={this.state.endDate}
-                            onChange={this.handleEndDateChange}
-                            showMonthDropdown
-                            dropdownMode="select"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col">
-                        {this.state.numDays
-                          ? <p>
-                              You selected {this.state.numDays} day(s)
-                            </p>
-                          : null}
-                      </div>
-                    </div>
+          {this.props.pending_items.filter(e => e.id === listID).map(record => (
+            <div key={record.id}>
+              <Modal
+                className="Modal__Bootstrap modal-dialog"
+                isOpen={this.state.showModal1}
+                onRequestClose={this.handleCloseModal1}
+                contentLabel="Modal #1"
+                overlayClassName="Overlay"
+                style={customStyles}
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">
+                      Decline
+                    </h5>
                   </div>
-                  <div className="modal-footer">
-                    <div className="form-group">
+                  <form onSubmit={this.handleDeclineSubmit}>
+                    <div className="modal-body">
+                      <p>{record.user.othernames}{" "}{record.user.surname}</p>
+                      <div className="form-group">
+                        <label htmlFor="reason">Reason</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Enter reason"
+                          id="reason"
+                          onChange={this.handleDeclineReason}
+                        />
+                      </div>
+                    </div>
+                    <div className="modal-footer">
                       <button
                         type="button"
                         className="btn btn-outline-primary"
-                        onClick={this.handleCloseModal2}
+                        onClick={this.handleCloseModal1}
                       >
                         Close
                       </button>
-                    </div>
-                    <div className="form-group">
-                      <button type="submit" className="btn btn-primary col">
-                        Save changes
+                      <button type="submit" className="btn btn-primary">
+                        Decline
                       </button>
                     </div>
+                  </form>
+                  <div className="text-danger text-center">
+                    <div className="pb-4">{this.state.errorMessage}</div>
                   </div>
-                </form>
-                <div className="text-primary text-center" />
-                <div className="text-danger text-center" />
-              </div>
-            </Modal>
-          </div>
-        ))}
-      </div>
-    );
+                </div>
+              </Modal>
+            </div>
+          ))}
+          {this.props.pending_items.filter(e => e.id === listID).map(record => (
+            <div key={record.id}>
+              <Modal
+                className="Modal__Bootstrap modal-dialog"
+                isOpen={this.state.showModal2}
+                onRequestClose={this.handleCloseModal2}
+                contentLabel="Modal #2"
+                overlayClassName="Overlay"
+                style={customStyles}
+              >
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">
+                      Edit
+                    </h5>
+                  </div>
+                  <form
+                    encType="multipart/form-data"
+                    onSubmit={this.handleEditSubmit}
+                  >
+                    <div className="modal-body">
+                      <div className="row">
+                        <div className="col">
+                          <p className="h5">
+                            {record.user.othernames}{" "}{record.user.surname}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <label htmlFor="leave">
+                              Leave
+                            </label>
+                            <select
+                              className="form-control"
+                              id="leave"
+                              defaultValue={record.leave_name}
+                              ref={select => this.leave_name = select}
+                            >
+                              <option>{record.leave_name}</option>
+                              <option>annual</option>
+                              <option>sick</option>
+                              <option>bereavement</option>
+                              <option>christmas</option>
+                              <option>birthday</option>
+                              {record.user.gender === "female"
+                                ? <option>maternity</option>
+                                : null}
+                              <option>lwop</option>
+                              <option>other</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <label htmlFor="leave type">Leave type</label>
+                            <select
+                              className="form-control"
+                              id="leave type"
+                              defaultValue={record.leave_type}
+                              ref={select => this.leave_type = select}
+                            >
+                              <option>{record.leave_type}</option>
+                              <option>full</option>
+                              <option>half day am</option>
+                              <option>half day pm</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <label htmlFor="startDate">Start date</label>
+                            <DatePicker
+                              className="form-control"
+                              placeholderText="Click to select a date"
+                              selected={this.state.startDate}
+                              startDate={this.state.startDate}
+                              endDate={this.state.endDate}
+                              onChange={this.handleStartDateChange}
+                              showMonthDropdown
+                              dropdownMode="select"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <div className="form-group">
+                            <label htmlFor="endDate">End date</label>
+                            <DatePicker
+                              className="form-control"
+                              placeholderText="Click to select a date"
+                              selected={this.state.endDate}
+                              startDate={this.state.startDate}
+                              endDate={this.state.endDate}
+                              onChange={this.handleEndDateChange}
+                              showMonthDropdown
+                              dropdownMode="select"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          {this.state.numDays
+                            ? <p>
+                                You selected {this.state.numDays} day(s)
+                              </p>
+                            : null}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="modal-footer">
+                      <div className="form-group">
+                        <button
+                          type="button"
+                          className="btn btn-outline-primary"
+                          onClick={this.handleCloseModal2}
+                        >
+                          Close
+                        </button>
+                      </div>
+                      <div className="form-group">
+                        <button type="submit" className="btn btn-primary col">
+                          Save changes
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                  <div className="text-primary text-center" />
+                  <div className="text-danger text-center" />
+                </div>
+              </Modal>
+            </div>
+          ))}
+        </div>
+      : <div className="text-center" style={{ paddingTop: "40px" }}>
+          <h1 className="display-4">There are no pending leave record.</h1>
+        </div>;
   }
 }
 
 PendingLeaveList.propTypes = {
   pending_items: PropTypes.array.isRequired,
-  public_holiday: PropTypes.array.isRequired
+  public_holiday: PropTypes.array.isRequired,
+  onApproveLeaveSubmit: PropTypes.func.isRequired
 };
 
 export default PendingLeaveList;
