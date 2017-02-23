f41f60d41998fd763b1a23b9f4a68fe311c18361
navinesh <navinesh.chand@gmail.com>
2017-02-23 14:45:51 +1200

    First commit

---
+++ admin/src/actions/ApproveLeave.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+import { fetchPendingLeave } from "../actions/PendingLeave";
+
+export const APPROVE_LEAVE_REQUEST = "APPROVE_LEAVE_REQUEST";
+export const APPROVE_LEAVE_SUCCESS = "APPROVE_LEAVE_SUCCESS";
+export const APPROVE_LEAVE_ERROR = "APPROVE_LEAVE_ERROR";
+
+export const requestApproveLeave = () => ({
+  type: APPROVE_LEAVE_REQUEST
+});
+
+export const receiveApproveLeave = data => ({
+  type: APPROVE_LEAVE_SUCCESS,
+  message: data.message
+});
+
+export const errorApproveLeave = data => ({
+  type: APPROVE_LEAVE_ERROR,
+  message: data.message
+});
+
+export function submitApproveLeave(approveLeaveData) {
+  return dispatch => {
+    dispatch(requestApproveLeave(approveLeaveData));
+    let data = new FormData();
+    data.append("leave_id", approveLeaveData.leaveID);
+    data.append("LeaveStatus", approveLeaveData.LeaveStatus);
+    axios
+      .post("http://localhost:8080/approveleave", data)
+      .then(response => {
+        if (response.status === 200) {
+          dispatch(errorApproveLeave(response.data));
+        } else {
+          dispatch(receiveApproveLeave(response.data));
+          dispatch(fetchPendingLeave());
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
