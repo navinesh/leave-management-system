9f8871aff45df582dd8b600b09cbaa16140a37e8
navinesh <navinesh.chand@gmail.com>
2017-02-28 12:59:59 +1200

    First commit

---
+++ admin/src/actions/EditLeave.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export const EDIT_LEAVE_REQUEST = "EDIT_LEAVE_REQUEST";
+export const EDIT_LEAVE_SUCCESS = "EDIT_LEAVE_SUCCESS";
+export const EDIT_LEAVE_FAILURE = "EDIT_LEAVE_FAILURE";
+export const CLEAR_EDIT_LEAVE = "CLEAR_EDIT_LEAVE";
+
+export function requestEditLeave(editLeaveData) {
+  return { type: EDIT_LEAVE_REQUEST, editLeaveData };
+}
+
+export function successEditLeave(data) {
+  return { type: EDIT_LEAVE_SUCCESS, message: data.message };
+}
+
+export function failureEditLeave(data) {
+  return { type: EDIT_LEAVE_FAILURE, message: data.message };
+}
+
+export const clearEditLeave = () => ({ type: CLEAR_EDIT_LEAVE });
+
+export function submitEditLeave(editLeaveData) {
+  return dispatch => {
+    dispatch(requestEditLeave(editLeaveData));
+    axios
+      .post("http://localhost:8080/editleave", {
+        leave_id: editLeaveData.leave_id,
+        leave: editLeaveData.leave,
+        leaveType: editLeaveData.leaveType,
+        startDate: editLeaveData.startDate,
+        endDate: editLeaveData.endDate,
+        reason: editLeaveData.reason,
+        leaveDays: editLeaveData.leaveDays,
+        applicationDays: editLeaveData.applicationDays
+      })
+      .then(response => {
+        if (response.status === 200) {
+          dispatch(failureEditLeave(response.data));
+        } else {
+          dispatch(successEditLeave(response.data));
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
