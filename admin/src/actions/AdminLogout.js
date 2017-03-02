033d0b2ad857a0db0a6ef0bc3eef0d2d01721aaf
navinesh <navinesh.chand@gmail.com>
2017-03-02 15:33:41 +1200

    Format code

--- admin/src/actions/AdminLogout.js
+++ admin/src/actions/AdminLogout.js
@@ -1,18 +1,18 @@
-export const LOGOUT_ADMIN_REQUEST = 'LOGOUT_ADMIN_REQUEST'
-export const LOGOUT_ADMIN_SUCCESS = 'LOGOUT_ADMIN_SUCCESS'
+export const LOGOUT_ADMIN_REQUEST = "LOGOUT_ADMIN_REQUEST";
+export const LOGOUT_ADMIN_SUCCESS = "LOGOUT_ADMIN_SUCCESS";
 
 export const requestLogout = () => ({
   type: LOGOUT_ADMIN_REQUEST
-})
+});
 
 export const receiveLogout = () => ({
   type: LOGOUT_ADMIN_SUCCESS
-})
+});
 
 export const logoutAdmin = () => {
   return dispatch => {
-    dispatch(requestLogout())
-    dispatch(receiveLogout())
-    localStorage.removeItem('admin_token')
-  }
-}
+    dispatch(requestLogout());
+    dispatch(receiveLogout());
+    localStorage.removeItem("admin_token");
+  };
+};
