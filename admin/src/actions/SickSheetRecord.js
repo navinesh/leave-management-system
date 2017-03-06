2c0651b66df184a3cc98d9ad5822b1be2eca47b4
navinesh <navinesh.chand@gmail.com>
2017-03-06 17:29:40 +1200

    Format code

--- admin/src/actions/SickSheetRecord.js
+++ admin/src/actions/SickSheetRecord.js
@@ -1,29 +1,29 @@
-export const REQUEST_SICKSHEET_RECORD = 'REQUEST_SICKSHEET_RECORD'
-export const RECEIVE_SICKSHEET_RECORD = 'RECEIVE_SICKSHEET_RECORD'
-export const ERROR_SICKSHEET_RECORD = 'ERROR_SICKSHEET_RECORD'
+export const REQUEST_SICKSHEET_RECORD = "REQUEST_SICKSHEET_RECORD";
+export const RECEIVE_SICKSHEET_RECORD = "RECEIVE_SICKSHEET_RECORD";
+export const ERROR_SICKSHEET_RECORD = "ERROR_SICKSHEET_RECORD";
 
 export const requestSickSheetRecord = () => ({
   type: REQUEST_SICKSHEET_RECORD
-})
+});
 
-export const receiveSickSheetRecord = (json) => ({
+export const receiveSickSheetRecord = json => ({
   type: RECEIVE_SICKSHEET_RECORD,
   sickSheet_records: json.sick_sheet_records,
   receivedAt: Date.now()
-})
+});
 
 export const errorSickSheetRecord = () => ({
   type: ERROR_SICKSHEET_RECORD
-})
+});
 
 export const fetchSickSheetRecord = () => {
   return dispatch => {
-    dispatch(requestSickSheetRecord())
+    dispatch(requestSickSheetRecord());
     return fetch(`http://localhost:8080/sicksheet-record.api`)
       .then(response => response.json())
       .then(json => dispatch(receiveSickSheetRecord(json)))
-      .catch((error) => {
-        dispatch(errorSickSheetRecord())
-      })
-  }
-}
+      .catch(error => {
+        dispatch(errorSickSheetRecord());
+      });
+  };
+};
