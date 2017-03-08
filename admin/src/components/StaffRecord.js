97fc9174cb4b7956e8fd6b1af116edeb93fc617f
navinesh <navinesh.chand@gmail.com>
2017-03-08 15:30:08 +1200

    Delete custom styles and import it from Styles module

--- admin/src/components/StaffRecord.js
+++ admin/src/components/StaffRecord.js
@@ -10,28 +10,7 @@ const moment = require("moment");
 
 var Loader = require("halogen/ClipLoader");
 
-const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.75)"
-  },
-  content: {
-    position: "absolute",
-    top: "40px",
-    left: "0px",
-    right: "0px",
-    bottom: "0px",
-    border: "0",
-    background: "#fff",
-    overflow: "auto",
-    WebkitOverflowScrolling: "touch",
-    outline: "none"
-  }
-};
+import customStyles from "../Styles";
 
 class StaffRecordList extends Component {
   constructor() {
