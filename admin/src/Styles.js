919456ca35dfa70daa4e640b98eca9ae78029980
navinesh <navinesh.chand@gmail.com>
2017-03-08 15:27:47 +1200

    First commit

---
+++ admin/src/Styles.js
@@ -0,0 +1,24 @@
+const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.75)"
+  },
+  content: {
+    position: "absolute",
+    top: "40px",
+    left: "0px",
+    right: "0px",
+    bottom: "0px",
+    border: "0",
+    background: "#fff",
+    overflow: "auto",
+    WebkitOverflowScrolling: "touch",
+    outline: "none"
+  }
+};
+
+export default customStyles;
