f642617ddc884a864e72fe48e9adf9809c8f3075
navinesh <navinesh.chand@gmail.com>
2017-03-10 15:12:41 +1200

    Delete children components

--- admin/src/containers/AdminHeader.js
+++ admin/src/containers/AdminHeader.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import Header from "../components/AdminHeader";
 import AdminLogin from "./AdminLogin";
 
-const AdminHeader = ({ isAuthenticated, dispatch, children }) => (
+const AdminHeader = ({ isAuthenticated, dispatch }) => (
   <div className="AdminHeader">
-    {isAuthenticated ? <Header dispatch={dispatch} /> : (
-          <div>
-            <h1 className="display-4 text-center pb-4">
-              Leave Management System
-            </h1>
-            <AdminLogin />
-          </div>
-        )}
-    {children}
+    {isAuthenticated
+      ? <Header dispatch={dispatch} />
+      : <div>
+          <h1 className="display-4 text-center pb-4">
+            Leave Management System
+          </h1>
+          <AdminLogin />
+        </div>}
   </div>
 );
 
@@ -24,4 +24,4 @@ const mapStateToProps = state => {
   return { isAuthenticated };
 };
 
-export default connect(mapStateToProps)(AdminHeader)
+export default connect(mapStateToProps)(AdminHeader);
