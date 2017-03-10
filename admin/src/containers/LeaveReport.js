5da15f9b38f62ea47b44efa0624c52c892933e73
navinesh <navinesh.chand@gmail.com>
2017-03-10 14:58:29 +1200

    Add react router v4 Redirect component

--- admin/src/containers/LeaveReport.js
+++ admin/src/containers/LeaveReport.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+
 import { fetchLeaveRecord } from "../actions/LeaveReport";
 import LeaveReportList from "../components/LeaveReport";
 
@@ -11,27 +13,28 @@ class LeaveReport extends Component {
   }
 
   render() {
-    const { isFetching, leave_record, isAuthenticated } = this.props;
+    const { isAuthenticated, isFetching, leave_record } = this.props;
 
     return (
       <div className="container">
-        {isAuthenticated &&
-          (isFetching
-            ? <div className="text-center">
-                <BeatLoader color="#0275d8" size="12px" />
-              </div>
-            : <LeaveReportList leave_record={leave_record} />)}
+        {isAuthenticated
+          ? isFetching
+              ? <div className="text-center">
+                  <BeatLoader color="#0275d8" size="12px" />
+                </div>
+              : <LeaveReportList leave_record={leave_record} />
+          : <Redirect to="/" />}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { leaveReport, adminAuth } = state;
-  const { isFetching, leave_record } = leaveReport;
+  const { adminAuth, leaveReport } = state;
   const { isAuthenticated } = adminAuth;
+  const { isFetching, leave_record } = leaveReport;
 
-  return { isFetching, leave_record, isAuthenticated };
+  return { isAuthenticated, isFetching, leave_record };
 };
 
 export default connect(mapStateToProps)(LeaveReport);
