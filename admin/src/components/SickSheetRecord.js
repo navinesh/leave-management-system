f080c0a186030c35a64b9bc76dfcccf86f7ae019
navinesh <navinesh.chand@gmail.com>
2017-03-10 15:03:55 +1200

    Add react router v4 Link

--- admin/src/components/SickSheetRecord.js
+++ admin/src/components/SickSheetRecord.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 const SickSheetList = ({ sickSheet_items }) => {
   const itemNodes = sickSheet_items.map(record => (
