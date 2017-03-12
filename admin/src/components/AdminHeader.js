9a62fb9b45d4a2f7f4dd16ad76cf2f5b8c7b063f
navinesh <navinesh.chand@gmail.com>
2017-03-13 08:49:19 +1200

    Format code

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -1,5 +1,6 @@
 import React, { PropTypes, Component } from "react";
 import { Link } from "react-router-dom";
+
 import { logoutAdmin } from "../actions/AdminLogout";
 
 export default class Header extends Component {
