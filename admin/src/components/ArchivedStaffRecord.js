207fd16dd347245c815cd66ffc1d5d26a41070b5
navinesh <navinesh.chand@gmail.com>
2017-03-13 08:48:56 +1200

    Format code

--- admin/src/components/ArchivedStaffRecord.js
+++ admin/src/components/ArchivedStaffRecord.js
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from "react";
+
 import { searchStaffRecord } from "../actions/StaffRecord";
 import { fetchArchivedStaffRecord } from "../actions/ArchivedStaffRecord";
 
