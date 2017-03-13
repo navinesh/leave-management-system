e10bda97638b7cb57505a7a7d5921efc26c2c2c7
navinesh <navinesh.chand@gmail.com>
2017-03-14 08:48:25 +1200

    Format code

--- user/src/containers/resetpassword.js
+++ user/src/containers/resetpassword.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import UserResetPassword from "../components/resetpassword";
 import { resetPassword } from "../actions/resetpassword";
 
