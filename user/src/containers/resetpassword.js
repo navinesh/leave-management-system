5c58166ca22db49cab1abfdfe78404c1354bae3e
navinesh <navinesh.chand@gmail.com>
2017-03-14 11:25:13 +1200

    Redirect if not authenticated

--- user/src/containers/resetpassword.js
+++ user/src/containers/resetpassword.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 import UserResetPassword from "../components/resetpassword";
 import { resetPassword } from "../actions/resetpassword";
 
 const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching }) => (
   <div className="ResetPassword">
-    {!isAuthenticated &&
-      <div className="col col-md-4 offset-md-4">
-        <UserResetPassword
-          isFetching={isFetching}
-          message={message}
-          onResetClick={email => dispatch(resetPassword(email))}
-        />
-      </div>}
+    {!isAuthenticated
+      ? <div className="col col-md-4 offset-md-4">
+          <UserResetPassword
+            isFetching={isFetching}
+            message={message}
+            onResetClick={email => dispatch(resetPassword(email))}
+          />
+        </div>
+      : <Redirect to="/" />}
   </div>
 );
 
