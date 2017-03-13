7aca99ca7999cfab95dd596ee705ab85b7e6e210
navinesh <navinesh.chand@gmail.com>
2017-03-14 08:50:19 +1200

    Add react-router-dom

--- user/src/components/userrecord.js
+++ user/src/components/userrecord.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 const PendingRecordList = ({ user_record }) => {
   const pendingList = user_record
