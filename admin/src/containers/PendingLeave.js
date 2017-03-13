59ccda1facb84f1719e903cdd7b76adac8666e17
navinesh <navinesh.chand@gmail.com>
2017-03-13 17:18:10 +1200

    Check token

--- admin/src/containers/PendingLeave.js
+++ admin/src/containers/PendingLeave.js
@@ -3,18 +3,30 @@ import { connect } from "react-redux";
 
 const BeatLoader = require("halogen/BeatLoader");
 
-import AdminLogin from "./AdminLogin";
+import { fetchLoginFromToken } from "../actions/AdminLogin";
 import { fetchPendingLeave } from "../actions/PendingLeave";
 import { fetchPublicHoliday } from "../actions/PublicHoliday";
 import { submitApproveLeave } from "../actions/ApproveLeave";
 import { submitDeclineLeave } from "../actions/DeclineLeave";
 import { submitEditLeave } from "../actions/EditLeave";
+import AdminLogin from "./AdminLogin";
 import PendingLeaveList from "../components/PendingLeave";
 
 class PendingLeave extends Component {
   componentDidMount() {
-    this.props.dispatch(fetchPendingLeave());
-    this.props.dispatch(fetchPublicHoliday());
+    const { dispatch, auth_info } = this.props;
+    let admin_token = auth_info.admin_token
+      ? auth_info.admin_token
+      : localStorage.getItem("admin_token");
+
+    if (admin_token) {
+      dispatch(
+        fetchLoginFromToken(admin_token, fetchPendingLeave, fetchPublicHoliday)
+      );
+    } else {
+      dispatch(fetchPendingLeave());
+      dispatch(fetchPublicHoliday());
+    }
   }
 
   render() {
@@ -60,12 +72,13 @@ class PendingLeave extends Component {
 
 const mapStateToProps = state => {
   const { adminAuth, pendingLeave, publicHoliday, editLeave } = state;
-  const { isAuthenticated } = adminAuth;
+  const { auth_info, isAuthenticated } = adminAuth;
   const { isFetching, pending_items } = pendingLeave;
   const { public_holiday } = publicHoliday;
   const { isEditLeaveFetching, editLeaveMessage } = editLeave;
 
   return {
+    auth_info,
     isAuthenticated,
     isFetching,
     pending_items,
