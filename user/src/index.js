4ddc2783d6f4cdd99387e305597b676ad21d9f2e
navinesh <navinesh.chand@gmail.com>
2017-03-15 07:28:25 +1200

    Add private route

--- user/src/index.js
+++ user/src/index.js
@@ -5,18 +5,11 @@ import { Provider } from "react-redux";
 import configureStore from "./stores/configureStore";
 const store = configureStore();
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-import axios from "axios";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import "./index.css";
 import "./bootstrap.min.css";
 
-import {
-  requestUserLoginFromToken,
-  loginUserErrorFromToken,
-  receiveUserLoginFromToken
-} from "./actions/userlogin";
 import Header from "./containers/header";
 import Main from "./containers/main";
 import StaffLeaveCalendar from "./containers/staffleavecalendar";
@@ -25,58 +18,19 @@ import UserChangePassword from "./containers/changepassword";
 import UserError from "./components/usererror";
 import LeaveApplication from "./containers/leaveapplication";
 
-export const requireAuthentication = (nextState, replace, callback) => {
-  let auth_token = store.getState().userAuth.auth_info.auth_token;
-  if (auth_token) {
-    store.dispatch(requestUserLoginFromToken(auth_token));
-    axios
-      .post("http://localhost:8080/usertoken", { auth_token: auth_token })
-      .then(response => {
-        if (response.status === 200) {
-          if (location.pathname !== "/") {
-            replace("/");
-          }
-          localStorage.removeItem("auth_token");
-          store.dispatch(loginUserErrorFromToken(response.data));
-        } else {
-          store.dispatch(receiveUserLoginFromToken(response.data));
-        }
-        callback();
-      })
-      .catch(error => {
-        callback(error);
-      });
-  } else {
-    auth_token = localStorage.getItem("auth_token");
-    if (auth_token) {
-      store.dispatch(requestUserLoginFromToken(auth_token));
-      axios
-        .post("http://localhost:8080/usertoken", { auth_token: auth_token })
-        .then(response => {
-          if (response.status === 200) {
-            if (location.pathname !== "/") {
-              replace("/");
-            }
-            localStorage.removeItem("auth_token");
-            store.dispatch(loginUserErrorFromToken(response.data));
-          } else {
-            store.dispatch(receiveUserLoginFromToken(response.data));
-          }
-          callback();
-        })
-        .catch(error => {
-          callback(error);
-        });
-    }
-  }
-  let isAuthenticated = store.getState().userAuth.isAuthenticated;
-  if (!isAuthenticated) {
-    if (location.pathname !== "/") {
-      replace("/");
-    }
-    callback();
-  }
-};
+const PrivateRoute = ({ component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => store.getState().userAuth.isAuthenticated
+      ? React.createElement(component, props)
+      : <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location }
+          }}
+        />}
+  />
+);
 
 const App = () => (
   <Provider store={store}>
@@ -87,8 +41,8 @@ const App = () => (
           <Route exact path="/" component={Main} />
           <Route path="/leavecalendar" component={StaffLeaveCalendar} />
           <Route path="/reset" component={ResetPassword} />
-          <Route path="/leaveapplication" component={LeaveApplication} />
-          <Route path="/changepassword" component={UserChangePassword} />
+          <PrivateRoute path="/leaveapplication" component={LeaveApplication} />
+          <PrivateRoute path="/changepassword" component={UserChangePassword} />
           <Route component={UserError} />
         </Switch>
       </div>
