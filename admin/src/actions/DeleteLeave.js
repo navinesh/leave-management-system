72912169ff6baf7318cc7cfa5a0462006ddfa3c8
navinesh <navinesh.chand@gmail.com>
2017-03-24 10:26:05 +1200

    Add delete leave action

---
+++ admin/src/actions/DeleteLeave.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const DELETE_LEAVE_REQUEST = "DELETE_LEAVE_REQUEST";
+export const DELETE_LEAVE_SUCCESS = "DELETE_LEAVE_SUCCESS";
+export const DELETE_LEAVE_ERROR = "DELETE_LEAVE_ERROR";
+
+export const requestDeleteLeave = () => ({
+  type: DELETE_LEAVE_REQUEST
+});
+
+export const receiveDeleteLeave = data => ({
+  type: DELETE_LEAVE_SUCCESS,
+  message: data.message
+});
+
+export const errorDeleteLeave = data => ({
+  type: DELETE_LEAVE_ERROR,
+  message: data.message
+});
+
+export function submitDeleteLeave(deleteLeaveData) {
+  return dispatch => {
+    dispatch(requestDeleteLeave(deleteLeaveData));
+    axios
+      .post("http://localhost:8080/deleteleave", {
+        leave_id: deleteLeaveData.leaveID,
+        DeleteReason: deleteLeaveData.DeleteReason
+      })
+      .then(response => {
+        if (response.status === 200) {
+          dispatch(errorDeleteLeave(response.data));
+        } else {
+          dispatch(receiveDeleteLeave(response.data));
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
