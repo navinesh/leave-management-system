3f631bd61d621199f6eaaf6701bd4a28c50a8cf4
navinesh <navinesh.chand@gmail.com>
2017-04-11 11:30:48 +1200

    Add import prop-types

--- admin/src/components/AdminLogin.js
+++ admin/src/components/AdminLogin.js
@@ -1,5 +1,6 @@
 // @flow
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 var Loader = require("halogen/ClipLoader");
