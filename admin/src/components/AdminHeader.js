97a1af733cff535efb668fe708843f3499fb2aaa
navinesh <navinesh.chand@gmail.com>
2017-04-11 11:28:34 +1200

    Add import prop-types

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -1,5 +1,6 @@
 // @flow
-import React, { PropTypes, Component } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 import { logoutAdmin } from "../actions/AdminLogout";
