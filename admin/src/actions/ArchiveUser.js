388c107bd16f223c1efbdafad99a81af8b554db5
navinesh <navinesh.chand@gmail.com>
2017-04-18 10:52:08 +1200

    Add flow type

--- admin/src/actions/ArchiveUser.js
+++ admin/src/actions/ArchiveUser.js
@@ -1,3 +1,4 @@
+// @flow
 import axios from "axios";
 
 export const ARCHIVE_USER_REQUEST = "ARCHIVE_USER_REQUEST";
@@ -5,22 +6,22 @@ export const ARCHIVE_USER_SUCCESS = "ARCHIVE_USER_SUCCESS";
 export const ARCHIVE_USER_FAILURE = "ARCHIVE_USER_FAILURE";
 export const CLEAR_ARCHIVE_MESSAGE = "CLEAR_ARCHIVE_MESSAGE";
 
-export function requestArchiveUser(archiveUser) {
+export function requestArchiveUser(archiveUser: Object) {
   return { type: ARCHIVE_USER_REQUEST, archiveUser };
 }
 
-export function successArchiveUser(data) {
+export function successArchiveUser(data: Object) {
   return { type: ARCHIVE_USER_SUCCESS, message: data.message };
 }
 
-export function failureArchiveUser(data) {
+export function failureArchiveUser(data: Object) {
   return { type: ARCHIVE_USER_FAILURE, message: data.message };
 }
 
 export const clearArchiveMessage = () => ({ type: CLEAR_ARCHIVE_MESSAGE });
 
-export function submitArchiveUser(archiveUser) {
-  return dispatch => {
+export function submitArchiveUser(archiveUser: Object) {
+  return (dispatch: Function) => {
     dispatch(requestArchiveUser(archiveUser));
     axios
       .post("http://localhost:8080/archiveuser", {
