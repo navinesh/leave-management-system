e366d7f3f76e2c72f76ca7f07b336b053398ad65
navinesh <navinesh.chand@gmail.com>
2017-04-18 14:30:07 +1200

    Add flowtype

--- admin/src/actions/StaffRecord.js
+++ admin/src/actions/StaffRecord.js
@@ -1,42 +1,43 @@
-export const REQUEST_STAFF_RECORD = 'REQUEST_STAFF_RECORD'
-export const RECEIVE_STAFF_RECORD = 'RECEIVE_STAFF_RECORD'
-export const ERROR_STAFF_RECORD = 'ERROR_STAFF_RECORD'
-export const STAFF_RECORD_SEARCH = 'STAFF_RECORD_SEARCH'
-export const CLEAR_STAFF_RECORD_SEARCH = 'CLEAR_STAFF_RECORD_SEARCH'
+// @flow
+export const REQUEST_STAFF_RECORD = "REQUEST_STAFF_RECORD";
+export const RECEIVE_STAFF_RECORD = "RECEIVE_STAFF_RECORD";
+export const ERROR_STAFF_RECORD = "ERROR_STAFF_RECORD";
+export const STAFF_RECORD_SEARCH = "STAFF_RECORD_SEARCH";
+export const CLEAR_STAFF_RECORD_SEARCH = "CLEAR_STAFF_RECORD_SEARCH";
 
 export const requestStaffRecord = () => ({
   type: REQUEST_STAFF_RECORD
-})
+});
 
-export const receiveStaffRecord = (json) => ({
+export const receiveStaffRecord = (json: Object) => ({
   type: RECEIVE_STAFF_RECORD,
   staff_record: json.staff_record,
   receivedAt: Date.now()
-})
+});
 
 export const errorStaffRecord = () => ({
   type: ERROR_STAFF_RECORD
-})
+});
 
-export const searchStaffRecord = (searchTerm) => ({
-    type: STAFF_RECORD_SEARCH,
-    searchTerm
-})
+export const searchStaffRecord = (searchTerm: string) => ({
+  type: STAFF_RECORD_SEARCH,
+  searchTerm
+});
 
 export const clearSearchStaffRecord = () => {
   return {
     type: CLEAR_STAFF_RECORD_SEARCH
-  }
-}
+  };
+};
 
 export const fetchStaffRecord = () => {
-  return dispatch => {
-    dispatch(requestStaffRecord())
+  return (dispatch: Function) => {
+    dispatch(requestStaffRecord());
     return fetch(`http://localhost:8080/staff-record.api`)
       .then(response => response.json())
       .then(json => dispatch(receiveStaffRecord(json)))
-      .catch((error) => {
-        dispatch(errorStaffRecord())
-      })
-  }
-}
+      .catch(error => {
+        dispatch(errorStaffRecord());
+      });
+  };
+};
