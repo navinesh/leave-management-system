c029ac6b1013b79da8f8d2117837e8e48dd4160c
navinesh <navinesh.chand@gmail.com>
2017-04-21 11:34:30 +1200

    Use single quotes

--- admin/src/actions/DeletePublicHoliday.js
+++ admin/src/actions/DeletePublicHoliday.js
@@ -1,11 +1,11 @@
 // @flow
-import axios from "axios";
-import { fetchPublicHoliday } from "../actions/PublicHoliday";
+import axios from 'axios';
+import { fetchPublicHoliday } from '../actions/PublicHoliday';
 
-export const DELETE_PUBLIC_HOLIDAY_REQUEST = "DELETE_PUBLIC_HOLIDAY_REQUEST";
-export const DELETE_PUBLIC_HOLIDAY_SUCCESS = "DELETE_PUBLIC_HOLIDAY_SUCCESS";
-export const DELETE_PUBLIC_HOLIDAY_FAILURE = "DELETE_PUBLIC_HOLIDAY_FAILURE";
-export const CLEAR_DELETE_PUBLIC_MESSAGE = "CLEAR_DELETE_PUBLIC_MESSAGE";
+export const DELETE_PUBLIC_HOLIDAY_REQUEST = 'DELETE_PUBLIC_HOLIDAY_REQUEST';
+export const DELETE_PUBLIC_HOLIDAY_SUCCESS = 'DELETE_PUBLIC_HOLIDAY_SUCCESS';
+export const DELETE_PUBLIC_HOLIDAY_FAILURE = 'DELETE_PUBLIC_HOLIDAY_FAILURE';
+export const CLEAR_DELETE_PUBLIC_MESSAGE = 'CLEAR_DELETE_PUBLIC_MESSAGE';
 
 export function requestDeletePublicHoliday(archiveUser: Object) {
   return { type: DELETE_PUBLIC_HOLIDAY_REQUEST, archiveUser };
@@ -25,7 +25,7 @@ export function submitDeletePublicHoliday(deletePublicHolidayDate: Object) {
   return (dispatch: Function) => {
     dispatch(requestDeletePublicHoliday(deletePublicHolidayDate));
     axios
-      .post("http://localhost:8080/deletepublicholiday", {
+      .post('http://localhost:8080/deletepublicholiday', {
         id: deletePublicHolidayDate.id
       })
       .then(response => {
