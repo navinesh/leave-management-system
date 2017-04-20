80cd97077f2931879f68d4c4e3cd0bb02fb29a07
navinesh <navinesh.chand@gmail.com>
2017-04-21 10:34:00 +1200

    Use single quotes

--- admin/src/actions/AdminLogin.js
+++ admin/src/actions/AdminLogin.js
@@ -1,13 +1,13 @@
 // @flow
-import axios from "axios";
+import axios from 'axios';
 
-export const LOGIN_ADMIN_REQUEST = "LOGIN_ADMIN_REQUEST";
-export const LOGIN_ADMIN_SUCCESS = "LOGIN_ADMIN_SUCCESS";
-export const LOGIN_ADMIN_FAILURE = "LOGIN_ADMIN_FAILURE";
+export const LOGIN_ADMIN_REQUEST = 'LOGIN_ADMIN_REQUEST';
+export const LOGIN_ADMIN_SUCCESS = 'LOGIN_ADMIN_SUCCESS';
+export const LOGIN_ADMIN_FAILURE = 'LOGIN_ADMIN_FAILURE';
 
-export const LOGIN_ADMIN_REQUEST_FROM_TOKEN = "LOGIN_ADMIN_REQUEST_FROM_TOKEN";
-export const LOGIN_ADMIN_SUCCESS_FROM_TOKEN = "LOGIN_ADMIN_SUCCESS_FROM_TOKEN";
-export const LOGIN_ADMIN_FAILURE_FROM_TOKEN = "LOGIN_ADMIN_FAILURE_FROM_TOKEN";
+export const LOGIN_ADMIN_REQUEST_FROM_TOKEN = 'LOGIN_ADMIN_REQUEST_FROM_TOKEN';
+export const LOGIN_ADMIN_SUCCESS_FROM_TOKEN = 'LOGIN_ADMIN_SUCCESS_FROM_TOKEN';
+export const LOGIN_ADMIN_FAILURE_FROM_TOKEN = 'LOGIN_ADMIN_FAILURE_FROM_TOKEN';
 
 export const requestAdminLogin = (creds: Object) => ({
   type: LOGIN_ADMIN_REQUEST,
@@ -43,7 +43,7 @@ export const fetchLogin = (creds: Object) => {
   return (dispatch: Function) => {
     dispatch(requestAdminLogin(creds));
     axios
-      .post("http://localhost:8080/adminlogin", {
+      .post('http://localhost:8080/adminlogin', {
         email: creds.email,
         password: creds.password
       })
@@ -51,7 +51,7 @@ export const fetchLogin = (creds: Object) => {
         if (response.status === 200) {
           dispatch(loginAdminError(response.data));
         } else {
-          localStorage.setItem("admin_token", response.data.admin_token);
+          localStorage.setItem('admin_token', response.data.admin_token);
           dispatch(receiveAdminLogin(response.data));
         }
       })
@@ -65,12 +65,12 @@ export const fetchLoginFromToken = (admin_token: string, action?: Function) => {
   return (dispatch: Function) => {
     dispatch(requestAdminLoginFromToken(admin_token));
     axios
-      .post("http://localhost:8080/admintoken", {
+      .post('http://localhost:8080/admintoken', {
         admin_token: admin_token
       })
       .then(response => {
         if (response.status === 200) {
-          localStorage.removeItem("admin_token");
+          localStorage.removeItem('admin_token');
           dispatch(loginAdminErrorFromToken(response.data));
         } else {
           dispatch(receiveAdminLoginFromToken(response.data));
