83331d6692b2da1d820267ee6d8b54c60b6e365d
navinesh <navinesh.chand@gmail.com>
2017-04-21 12:20:42 +1200

    Use single quotes

--- admin/src/actions/ModifyRecord.js
+++ admin/src/actions/ModifyRecord.js
@@ -1,11 +1,11 @@
 // @flow
-import axios from "axios";
-import { fetchStaffRecord } from "../actions/StaffRecord";
+import axios from 'axios';
+import { fetchStaffRecord } from '../actions/StaffRecord';
 
-export const MODIFY_USER_RECORD_REQUEST = "MODIFY_USER_RECORD_REQUEST";
-export const MODIFY_USER_RECORD_SUCCESS = "MODIFY_USER_RECORD_SUCCESS";
-export const MODIFY_USER_RECORD_FAILURE = "Modify_USER_RECORD_FAILURE";
-export const CLEAR_MODIFY_USER_MESSAGE = "CLEAR_MODIFY_USER_MESSAGE";
+export const MODIFY_USER_RECORD_REQUEST = 'MODIFY_USER_RECORD_REQUEST';
+export const MODIFY_USER_RECORD_SUCCESS = 'MODIFY_USER_RECORD_SUCCESS';
+export const MODIFY_USER_RECORD_FAILURE = 'Modify_USER_RECORD_FAILURE';
+export const CLEAR_MODIFY_USER_MESSAGE = 'CLEAR_MODIFY_USER_MESSAGE';
 
 export function requestModifyUserRecord(modifyUserDetails: Object) {
   return { type: MODIFY_USER_RECORD_REQUEST, modifyUserDetails };
@@ -25,7 +25,7 @@ export function submitModifyUserRecord(modifyUserDetails: Object) {
   return (dispatch: Function) => {
     dispatch(requestModifyUserRecord(modifyUserDetails));
     axios
-      .post("http://localhost:8080/modifyuser", {
+      .post('http://localhost:8080/modifyuser', {
         user_id: modifyUserDetails.id,
         surname: modifyUserDetails.surname,
         othernames: modifyUserDetails.othernames,
