388fa34550a71e5ddff394262d0496d1767b6560
navinesh <navinesh.chand@gmail.com>
2017-04-24 09:07:53 +1200

    Use single quotes

--- admin/src/components/SickSheetRecord.js
+++ admin/src/components/SickSheetRecord.js
@@ -1,11 +1,11 @@
 // @flow
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SickSheetList = (props: { sickSheet_items: Array<any> }) => {
   const itemNodes = props.sickSheet_items.map(record => (
     <tr key={record.id}>
-      <td>{record.user.othernames}{" "}{record.user.surname}</td>
+      <td>{record.user.othernames}{' '}{record.user.surname}</td>
       <td>{record.start_date}</td>
       <td>{record.end_date}</td>
       <td>{record.leave_days}</td>
@@ -25,7 +25,7 @@ const SickSheetList = (props: { sickSheet_items: Array<any> }) => {
     ? <div className="table-responsive">
         <table
           className="table table-bordered table-hover"
-          style={{ backgroundColor: "#FFFFFF" }}
+          style={{ backgroundColor: '#FFFFFF' }}
         >
           <thead className="thead-default">
             <tr>
@@ -42,7 +42,7 @@ const SickSheetList = (props: { sickSheet_items: Array<any> }) => {
           </tbody>
         </table>
       </div>
-    : <div className="text-center" style={{ paddingTop: "40px" }}>
+    : <div className="text-center" style={{ paddingTop: '40px' }}>
         <h1 className="display-4">
           There are no approved sick leave record with medical certificate.
         </h1>
