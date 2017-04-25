8c4281dcd5de08700b6bf61a48a715e3a4ca0725
navinesh <navinesh.chand@gmail.com>
2017-04-26 07:41:58 +1200

    First commit

---
+++ user/flow-typed/modules.js
@@ -0,0 +1,36 @@
+// @flow
+declare module 'react-router-dom' {
+  declare var exports: any;
+}
+
+declare module 'react-modal' {
+  declare var exports: any;
+}
+
+declare module 'redux' {
+  declare var exports: any;
+}
+
+declare module 'react-redux' {
+  declare var exports: any;
+}
+
+declare module 'moment' {
+  declare var exports: any;
+}
+
+declare module 'moment-range' {
+  declare var exports: any;
+}
+
+declare module 'react-datepicker' {
+  declare var exports: any;
+}
+
+declare module 'react-datepicker/dist/react-datepicker.css' {
+  declare var exports: any;
+}
+
+declare module 'axios' {
+  declare var exports: any;
+}
