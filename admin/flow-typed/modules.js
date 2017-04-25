6bba9b99d3efcd005b52ca847134231e00b8ae86
navinesh <navinesh.chand@gmail.com>
2017-04-25 14:04:52 +1200

    Use single quotes and remove halogen module

--- admin/flow-typed/modules.js
+++ admin/flow-typed/modules.js
@@ -1,44 +1,36 @@
 // @flow
-declare module "react-router-dom" {
+declare module 'react-router-dom' {
   declare var exports: any;
 }
 
-declare module "react-modal" {
+declare module 'react-modal' {
   declare var exports: any;
 }
 
-declare module "redux" {
+declare module 'redux' {
   declare var exports: any;
 }
 
-declare module "react-redux" {
+declare module 'react-redux' {
   declare var exports: any;
 }
 
-declare module "moment" {
+declare module 'moment' {
   declare var exports: any;
 }
 
-declare module "moment-range" {
+declare module 'moment-range' {
   declare var exports: any;
 }
 
-declare module "react-datepicker" {
+declare module 'react-datepicker' {
   declare var exports: any;
 }
 
-declare module "react-datepicker/dist/react-datepicker.css" {
+declare module 'react-datepicker/dist/react-datepicker.css' {
   declare var exports: any;
 }
 
-declare module "halogen/ClipLoader" {
-  declare var exports: any;
-}
-
-declare module "halogen/BeatLoader" {
-  declare var exports: any;
-}
-
-declare module "axios" {
+declare module 'axios' {
   declare var exports: any;
 }
