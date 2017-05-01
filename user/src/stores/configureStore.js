545950f51571a333438d4491297c74fc11fed248
navinesh <navinesh.chand@gmail.com>
2017-05-01 12:07:20 +1200

    Add single quotes

--- user/src/stores/configureStore.js
+++ user/src/stores/configureStore.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import createLogger from "redux-logger";
-import rootReducer from "../reducers/reducers";
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from '../reducers/reducers';
 
 const configureStore = () => {
   const store = createStore(
