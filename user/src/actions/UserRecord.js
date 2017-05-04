2b7ca07d49ff0343ca344f30d01f56ee4c41dd28
navinesh <navinesh.chand@gmail.com>
2017-05-04 15:43:15 +1200

    Add flow type

--- user/src/actions/UserRecord.js
+++ user/src/actions/UserRecord.js
@@ -1,32 +1,33 @@
-import axios from "axios";
+// @flow
+import axios from 'axios';
 
-export const REQUEST_USER_RECORD = "REQUEST_USER_RECORD";
-export const RECEIVE_USER_RECORD = "RECEIVE_USER_RECORD";
-export const USER_RECORD_ERROR = "USER_RECORD_ERROR";
-export const CLEAR_USER_RECORD = "CLEAR_USER_RECORD";
+export const REQUEST_USER_RECORD = 'REQUEST_USER_RECORD';
+export const RECEIVE_USER_RECORD = 'RECEIVE_USER_RECORD';
+export const USER_RECORD_ERROR = 'USER_RECORD_ERROR';
+export const CLEAR_USER_RECORD = 'CLEAR_USER_RECORD';
 
-export const requestUserRecord = auth_token => ({
+export const requestUserRecord = (auth_token: string) => ({
   type: REQUEST_USER_RECORD,
   auth_token
 });
 
-export const userRecordError = data => ({
+export const userRecordError = (data: Object) => ({
   type: USER_RECORD_ERROR,
   message: data.message
 });
 
-export const receiveUserRecord = data => ({
+export const receiveUserRecord = (data: Object) => ({
   type: RECEIVE_USER_RECORD,
   user_record: data.user_record
 });
 
 export const clearUserRecord = () => ({ type: CLEAR_USER_RECORD });
 
-export const fetchUserRecord = auth_token => {
-  return dispatch => {
+export const fetchUserRecord = (auth_token: string) => {
+  return (dispatch: Function) => {
     dispatch(requestUserRecord(auth_token));
     axios({
-      url: "http://localhost:8080/user-record.api",
+      url: 'http://localhost:8080/user-record.api',
       auth: { username: auth_token }
     })
       .then(response => {
@@ -37,15 +38,18 @@ export const fetchUserRecord = auth_token => {
         }
       })
       .catch(error => {
-        localStorage.removeItem("auth_token");
-        dispatch({ type: "LOGIN_FAILURE_FROM_TOKEN" });
-        dispatch({ type: "CLEAR_USER_RECORD" });
-        dispatch({ type: "CLEAR_USER_DETAILS" });
+        localStorage.removeItem('auth_token');
+        dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
+        dispatch({ type: 'CLEAR_USER_RECORD' });
+        dispatch({ type: 'CLEAR_USER_DETAILS' });
       });
   };
 };
 
-export const shouldfetchUserRecord = (state, userRecords) => {
+export const shouldfetchUserRecord = (
+  state: Object,
+  userRecords?: Array<any>
+) => {
   const recordState = state.userRecords;
   const { userRecord } = recordState;
   const records = userRecord.length;
@@ -57,9 +61,9 @@ export const shouldfetchUserRecord = (state, userRecords) => {
   }
 };
 
-export const fetchUserRecordIfNeeded = auth_token => {
-  return (dispatch, getState) => {
-    if (shouldfetchUserRecord(getState(), auth_token)) {
+export const fetchUserRecordIfNeeded = (auth_token: string) => {
+  return (dispatch: Function, getState: Function) => {
+    if (shouldfetchUserRecord(getState())) {
       // Dispatch a thunk from thunk!
       return dispatch(fetchUserRecord(auth_token));
     } else {
