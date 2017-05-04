aaa802858965d7a9c7ef0d5ee720c22640b6f470
navinesh <navinesh.chand@gmail.com>
2017-05-04 14:10:45 +1200

    Add flow type

--- user/src/actions/UserLogin.js
+++ user/src/actions/UserLogin.js
@@ -1,48 +1,52 @@
-import axios from "axios";
+// @flow
+import axios from 'axios';
 
-export const LOGIN_USER_REQUEST = "LOGIN_USER_REQUEST";
-export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
-export const LOGIN_USER_FAILURE = "LOGIN_USER_FAILURE";
+export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST';
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
 
-export const LOGIN_USER_REQUEST_FROM_TOKEN = "LOGIN_USER_REQUEST_FROM_TOKEN";
-export const LOGIN_USER_SUCCESS_FROM_TOKEN = "LOGIN_USER_SUCCESS_FROM_TOKEN";
-export const LOGIN_USER_FAILURE_FROM_TOKEN = "LOGIN_USER_FAILURE_FROM_TOKEN";
-export const LOGIN_FAILURE_FROM_TOKEN = "LOGIN_FAILURE_FROM_TOKEN";
+export const LOGIN_USER_REQUEST_FROM_TOKEN = 'LOGIN_USER_REQUEST_FROM_TOKEN';
+export const LOGIN_USER_SUCCESS_FROM_TOKEN = 'LOGIN_USER_SUCCESS_FROM_TOKEN';
+export const LOGIN_USER_FAILURE_FROM_TOKEN = 'LOGIN_USER_FAILURE_FROM_TOKEN';
+export const LOGIN_FAILURE_FROM_TOKEN = 'LOGIN_FAILURE_FROM_TOKEN';
 
-export const requestUserLogin = creds => ({ type: LOGIN_USER_REQUEST, creds });
+export const requestUserLogin = (creds: Object) => ({
+  type: LOGIN_USER_REQUEST,
+  creds
+});
 
-export const receiveUserLogin = data => ({
+export const receiveUserLogin = (data: Object) => ({
   type: LOGIN_USER_SUCCESS,
   auth_info: data
 });
 
-export const loginUserError = data => ({
+export const loginUserError = (data: Object) => ({
   type: LOGIN_USER_FAILURE,
   message: data.message
 });
 
-export const requestUserLoginFromToken = auth_token => ({
+export const requestUserLoginFromToken = (auth_token: string) => ({
   type: LOGIN_USER_REQUEST_FROM_TOKEN,
   auth_token
 });
 
-export const receiveUserLoginFromToken = data => ({
+export const receiveUserLoginFromToken = (data: Object) => ({
   type: LOGIN_USER_SUCCESS_FROM_TOKEN,
   auth_info: data
 });
 
-export const loginUserErrorFromToken = data => ({
+export const loginUserErrorFromToken = (data: Object) => ({
   type: LOGIN_USER_FAILURE_FROM_TOKEN,
   message: data.message
 });
 
 export const loginFailureFromToken = () => ({ type: LOGIN_FAILURE_FROM_TOKEN });
 
-export const fetchLogin = creds => {
-  return dispatch => {
+export const fetchLogin = (creds: Object) => {
+  return (dispatch: Function) => {
     dispatch(requestUserLogin(creds));
     axios
-      .post("http://localhost:8080/userlogin", {
+      .post('http://localhost:8080/userlogin', {
         email: creds.email,
         password: creds.password
       })
@@ -50,24 +54,24 @@ export const fetchLogin = creds => {
         if (response.status === 200) {
           dispatch(loginUserError(response.data));
         } else {
-          localStorage.setItem("auth_token", response.data.auth_token);
+          localStorage.setItem('auth_token', response.data.auth_token);
           dispatch(receiveUserLogin(response.data));
         }
       });
   };
 };
 
-export const fetchLoginFromToken = auth_token => {
-  return dispatch => {
+export const fetchLoginFromToken = (auth_token: string) => {
+  return (dispatch: Function) => {
     dispatch(requestUserLoginFromToken(auth_token));
     axios
-      .post("http://localhost:8080/usertoken", { auth_token: auth_token })
+      .post('http://localhost:8080/usertoken', { auth_token: auth_token })
       .then(response => {
         if (response.status === 200) {
-          localStorage.removeItem("auth_token");
+          localStorage.removeItem('auth_token');
           dispatch(loginUserErrorFromToken(response.data));
-          dispatch({ type: "CLEAR_USER_RECORD" });
-          dispatch({ type: "CLEAR_USER_DETAILS" });
+          dispatch({ type: 'CLEAR_USER_RECORD' });
+          dispatch({ type: 'CLEAR_USER_DETAILS' });
         } else {
           dispatch(receiveUserLoginFromToken(response.data));
         }
