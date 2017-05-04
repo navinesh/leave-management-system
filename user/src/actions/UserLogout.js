e4afc9c71a3b640807dd17f1cb6745bf7b3a54a2
navinesh <navinesh.chand@gmail.com>
2017-05-04 14:53:27 +1200

    Add flow type

--- user/src/actions/UserLogout.js
+++ user/src/actions/UserLogout.js
@@ -1,3 +1,4 @@
+// @flow
 export const LOGOUT_USER_REQUEST = "LOGOUT_USER_REQUEST";
 export const LOGOUT_USER_SUCCESS = "LOGOUT_USER_SUCCESS";
 
@@ -6,7 +7,7 @@ export const requestLogout = () => ({ type: LOGOUT_USER_REQUEST });
 export const receiveLogout = () => ({ type: LOGOUT_USER_SUCCESS });
 
 export const logoutUser = () => {
-  return dispatch => {
+  return (dispatch: Function) => {
     dispatch(requestLogout());
     dispatch(receiveLogout());
     localStorage.removeItem("auth_token");
