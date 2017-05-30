1cf257371f906fdb49f8e99f7774f90118fc4d57
navinesh <navinesh.chand@gmail.com>
2017-05-31 09:49:07 +1200

    Use es6 arrow functions

--- admin/src/actions/ApproveLeave.js
+++ admin/src/actions/ApproveLeave.js
@@ -7,21 +7,22 @@ export const APPROVE_LEAVE_REQUEST = 'APPROVE_LEAVE_REQUEST';
 export const APPROVE_LEAVE_SUCCESS = 'APPROVE_LEAVE_SUCCESS';
 export const APPROVE_LEAVE_ERROR = 'APPROVE_LEAVE_ERROR';
 
-export const requestApproveLeave = () => ({
-  type: APPROVE_LEAVE_REQUEST
+export const requestApproveLeave = (approveLeaveData: Object) => ({
+  type: APPROVE_LEAVE_REQUEST,
+  approveLeaveData
 });
 
-export const receiveApproveLeave = data => ({
+export const receiveApproveLeave = (data: Object) => ({
   type: APPROVE_LEAVE_SUCCESS,
   message: data.message
 });
 
-export const errorApproveLeave = data => ({
+export const errorApproveLeave = (data: Object) => ({
   type: APPROVE_LEAVE_ERROR,
   message: data.message
 });
 
-export function submitApproveLeave(approveLeaveData) {
+export const submitApproveLeave = (approveLeaveData: Object) => {
   return dispatch => {
     dispatch(requestApproveLeave(approveLeaveData));
     axios
@@ -44,4 +45,4 @@ export function submitApproveLeave(approveLeaveData) {
         console.log(error);
       });
   };
-}
+};
