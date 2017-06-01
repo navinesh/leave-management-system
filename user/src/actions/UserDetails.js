e40824e1d52360b480fd2a6748454afdc7020326
navinesh <navinesh.chand@gmail.com>
2017-06-02 07:59:34 +1200

    Add Async/Await

--- user/src/actions/UserDetails.js
+++ user/src/actions/UserDetails.js
@@ -23,27 +23,27 @@ export const receiveUserDetails = (data: Object) => ({
 
 export const clearUserDetails = () => ({ type: CLEAR_USER_DETAILS });
 
-export const fetchUserDetails = (auth_token: string) => {
-  return (dispatch: Function) => {
+export const fetchUserDetails = (auth_token: string) => async (
+  dispatch: Function
+) => {
+  try {
     dispatch(requestUserDetails(auth_token));
-    axios({
+    const response = await axios({
       url: 'http://localhost:8080/user-detail.api',
       auth: { username: auth_token }
-    })
-      .then(response => {
-        if (response.status === 200) {
-          dispatch(userDetailsError(response.data));
-        } else {
-          dispatch(receiveUserDetails(response.data));
-        }
-      })
-      .catch(error => {
-        localStorage.removeItem('auth_token');
-        dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
-        dispatch({ type: 'CLEAR_USER_RECORD' });
-        dispatch({ type: 'CLEAR_USER_DETAILS' });
-      });
-  };
+    });
+
+    if (response.status !== 201) {
+      dispatch(userDetailsError(response.data));
+    } else {
+      dispatch(receiveUserDetails(response.data));
+    }
+  } catch (error) {
+    localStorage.removeItem('auth_token');
+    dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
+    dispatch({ type: 'CLEAR_USER_RECORD' });
+    dispatch({ type: 'CLEAR_USER_DETAILS' });
+  }
 };
 
 export const shouldfetchUserDetails = (
@@ -61,14 +61,15 @@ export const shouldfetchUserDetails = (
   }
 };
 
-export const fetchUserDetailsIfNeeded = (auth_token: string) => {
-  return (dispatch: Function, getState: Function) => {
-    if (shouldfetchUserDetails(getState())) {
-      // Dispatch a thunk from thunk!
-      return dispatch(fetchUserDetails(auth_token));
-    } else {
-      // Let the calling code know there's nothing to wait for.
-      return Promise.resolve();
-    }
-  };
+export const fetchUserDetailsIfNeeded = (auth_token: string) => (
+  dispatch: Function,
+  getState: Function
+) => {
+  if (shouldfetchUserDetails(getState())) {
+    // Dispatch a thunk from thunk!
+    return dispatch(fetchUserDetails(auth_token));
+  } else {
+    // Let the calling code know there's nothing to wait for.
+    return Promise.resolve();
+  }
 };
