3c8579b4211b404e14b1e7b0c938f3902c5525b9
navinesh <navinesh.chand@gmail.com>
2017-06-07 15:24:16 +1200

    Format

--- user/src/components/UserError.js
+++ user/src/components/UserError.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export default () => (
+export default () =>
   <div className="container text-xs-center">
     <div className="offset-sm-2 col-sm-8">
       <h2 className="display-2">404</h2>
@@ -9,5 +9,4 @@ export default () => (
         The site configured at this address does not contain the requested file.
       </h5>
     </div>
-  </div>
-);
+  </div>;
