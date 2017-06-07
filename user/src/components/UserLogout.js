423635523d3db6f80256faca28fb45eeb20217f2
navinesh <navinesh.chand@gmail.com>
2017-06-07 15:24:10 +1200

    Format

--- user/src/components/UserLogout.js
+++ user/src/components/UserLogout.js
@@ -1,8 +1,7 @@
 // @flow
 import React from 'react';
 
-export default ({ onLogoutClick }: { onLogoutClick: Function }) => (
+export default ({ onLogoutClick }: { onLogoutClick: Function }) =>
   <button onClick={() => onLogoutClick()} className="btn btn-primary-outline">
     Log out
-  </button>
-);
+  </button>;
