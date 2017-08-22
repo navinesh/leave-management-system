62d318506745f1e65151aeb15dbba42c752538e9
navinesh <navinesh.chand@gmail.com>
2017-08-23 11:27:33 +1200

    First commit

---
+++ admin/src/actions/AdminResetPassword.js
@@ -0,0 +1,36 @@
+// @flow
+import axios from 'axios';
+
+export const REQUEST_PASSWORD_RESET = 'REQUEST_PASSWORD_RESET';
+export const PASSWORD_RESET_SUCCESS = 'PASSWORD_RESET_SUCCESS';
+export const PASSWORD_RESET_ERROR = 'PASSWORD_RESET_ERROR';
+
+export const requestPasswordReset = (email: string) => {
+  return { type: REQUEST_PASSWORD_RESET, email };
+};
+
+export const passwordResetError = (data: Object) => {
+  return { type: PASSWORD_RESET_ERROR, message: data.message };
+};
+
+export const passwordResetSuccess = (data: Object) => {
+  return { type: PASSWORD_RESET_SUCCESS, message: data.message };
+};
+
+export const resetPassword = (email: string) => async (dispatch: Function) => {
+  try {
+    dispatch(requestPasswordReset(email));
+    const response = await axios.post(
+      'http://localhost:8080/admin-reset-password',
+      { email: email }
+    );
+
+    if (response.status !== 201) {
+      dispatch(passwordResetError(response.data));
+    } else {
+      dispatch(passwordResetSuccess(response.data));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
