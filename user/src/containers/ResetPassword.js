574bb01d475cc33e603a2aef79aa652515257042
navinesh <navinesh.chand@gmail.com>
2017-08-25 10:01:55 +1200

    Add new flow type

--- user/src/containers/ResetPassword.js
+++ user/src/containers/ResetPassword.js
@@ -6,14 +6,21 @@ import { Redirect } from 'react-router-dom';
 import UserResetPassword from '../components/ResetPassword';
 import { resetPassword } from '../actions/ResetPassword';
 
-const ResetPassword = ({ dispatch, isAuthenticated, message, isFetching }) =>
+type Props = {
+  dispatch: Function,
+  isAuthenticated: boolean,
+  message: string,
+  isFetching: boolean
+};
+
+const ResetPassword = (props: Props) =>
   <div className="ResetPassword">
-    {!isAuthenticated
+    {!props.isAuthenticated
       ? <div className="col col-md-4 offset-md-4">
           <UserResetPassword
-            isFetching={isFetching}
-            message={message}
-            onResetClick={email => dispatch(resetPassword(email))}
+            isFetching={props.isFetching}
+            message={props.message}
+            onResetClick={email => props.dispatch(resetPassword(email))}
           />
         </div>
       : <Redirect to="/" />}
