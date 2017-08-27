665907c54c858449c3bbadd7ed30538bc5810973
navinesh <navinesh.chand@gmail.com>
2017-08-28 07:18:34 +1200

    Add bootstrap 4 beta class

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -16,58 +16,46 @@ export default class Header extends Component<Props> {
 
   render() {
     return (
-      <nav className="navbar fixed-top navbar-toggleable-md">
-        <div className="container">
-          <button
-            className="navbar-toggler navbar-toggler-right"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <Link className="navbar-brand" to="/" style={{ color: '#707070' }}>
-            Leave Management System
-          </Link>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <div className="navbar-nav">
-              <Link className="nav-item nav-link" to="/approvedleave">
-                Approved
-              </Link>
-              <Link className="nav-item nav-link" to="/staffrecord">
-                Staff
-              </Link>
-              <Link className="nav-item nav-link" to="/archivedstaffrecord">
-                Archived
-              </Link>
-              <Link className="nav-item nav-link" to="/newrecord">
-                New
-              </Link>
-              <Link className="nav-item nav-link" to="/leavereport">
-                Report
-              </Link>
-              <Link className="nav-item nav-link" to="/sicksheetrecord">
-                Sick sheet
-              </Link>
-              <Link className="nav-item nav-link" to="/publicholiday">
-                Public Holidays
-              </Link>
-              <button
-                onClick={this.adminLogout.bind(this)}
-                className="btn btn-primary ml-1"
-              >
-                Sign out
-              </button>
+      <div>
+        <nav className="navbar fixed-top navbar-expand-lg">
+          <div className="container">
+            <Link className="navbar-brand" to="/" style={{ color: '#707070' }}>
+              Leave Management System
+            </Link>
+            <div className="justify-content-end" id="navbarNav">
+              <div className="navbar-nav">
+                <Link className="nav-item nav-link" to="/approvedleave">
+                  Approved
+                </Link>
+                <Link className="nav-item nav-link" to="/staffrecord">
+                  Staff
+                </Link>
+                <Link className="nav-item nav-link" to="/archivedstaffrecord">
+                  Archived
+                </Link>
+                <Link className="nav-item nav-link" to="/newrecord">
+                  New
+                </Link>
+                <Link className="nav-item nav-link" to="/leavereport">
+                  Report
+                </Link>
+                <Link className="nav-item nav-link" to="/sicksheetrecord">
+                  Sick sheet
+                </Link>
+                <Link className="nav-item nav-link" to="/publicholiday">
+                  Public Holidays
+                </Link>
+                <button
+                  onClick={this.adminLogout.bind(this)}
+                  className="btn btn-primary ml-1"
+                >
+                  Sign out
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
