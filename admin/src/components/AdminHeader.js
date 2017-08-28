5d09c65aed3be0643d23d1d296d6993ba2f9aa4f
navinesh <navinesh.chand@gmail.com>
2017-08-29 07:48:36 +1200

    Add bootstrap 4 beta class

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -9,6 +9,13 @@ type Props = {
 };
 
 export default class Header extends Component<Props> {
+  adminLogout: Function;
+
+  constructor() {
+    super();
+    this.adminLogout = this.adminLogout.bind(this);
+  }
+
   adminLogout(e: Event) {
     e.preventDefault();
     this.props.dispatch(logoutAdmin());
@@ -22,35 +29,54 @@ export default class Header extends Component<Props> {
             <Link className="navbar-brand" to="/" style={{ color: '#707070' }}>
               Leave Management System
             </Link>
-            <div className="justify-content-end" id="navbarNav">
-              <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/approvedleave">
-                  Approved
-                </Link>
-                <Link className="nav-item nav-link" to="/staffrecord">
-                  Staff
-                </Link>
-                <Link className="nav-item nav-link" to="/archivedstaffrecord">
-                  Archived
-                </Link>
-                <Link className="nav-item nav-link" to="/newrecord">
-                  New
-                </Link>
-                <Link className="nav-item nav-link" to="/leavereport">
-                  Report
-                </Link>
-                <Link className="nav-item nav-link" to="/sicksheetrecord">
-                  Sick sheet
-                </Link>
-                <Link className="nav-item nav-link" to="/publicholiday">
-                  Public Holidays
-                </Link>
-                <button
-                  onClick={this.adminLogout.bind(this)}
-                  className="btn btn-primary ml-1"
-                >
-                  Sign out
-                </button>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="justify-content-end">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <div className="navbar-nav">
+                  <Link className="nav-item nav-link" to="/">
+                    Pending
+                  </Link>
+                  <Link className="nav-item nav-link" to="/approvedleave">
+                    Approved
+                  </Link>
+                  <Link className="nav-item nav-link" to="/staffrecord">
+                    Staff
+                  </Link>
+                  <Link className="nav-item nav-link" to="/archivedstaffrecord">
+                    Archived
+                  </Link>
+                  <Link className="nav-item nav-link" to="/newrecord">
+                    New
+                  </Link>
+                  <Link className="nav-item nav-link" to="/leavereport">
+                    Report
+                  </Link>
+                  <Link className="nav-item nav-link" to="/sicksheetrecord">
+                    Sick sheet
+                  </Link>
+                  <Link className="nav-item nav-link" to="/publicholiday">
+                    Public Holidays
+                  </Link>
+                  <button
+                    onClick={this.adminLogout}
+                    className="btn btn-primary ml-1"
+                  >
+                    Sign out
+                  </button>
+                </div>
               </div>
             </div>
           </div>
