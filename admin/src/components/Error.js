a4080624457e103141149b45b15500e2191c74ba
navinesh <navinesh.chand@gmail.com>
2017-08-29 07:48:56 +1200

    Add bootstrap 4 beta class

--- admin/src/components/Error.js
+++ admin/src/components/Error.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () =>
   <div className="container text-center">
-    <div className="col-md-8 offset-md-2">
+    <div className="col-md-8 ml-auto mr-auto">
       <h2 className="display-2">404</h2>
       <h4 className="display-3">That's an error.</h4>
       <h5 className="display-4">
