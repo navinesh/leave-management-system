088dae29da70737fbfe5bbc03033aa2a0106578c
navinesh <navinesh.chand@gmail.com>
2017-08-29 07:49:01 +1200

    Add bootstrap 4 beta class

--- admin/src/components/NewRecord.js
+++ admin/src/components/NewRecord.js
@@ -193,7 +193,7 @@ export default class NewRecordForm extends Component<Props, State> {
 
     return (
       <div className="container">
-        <div className="col-md-6 offset-md-3 pb-2">
+        <div className="col-md-6 ml-auto mr-auto pb-2">
           <div className="card card-body">
             <form encType="multipart/form-data" onSubmit={this.handleSubmit}>
               <div className="row">
