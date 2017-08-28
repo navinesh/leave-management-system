de918af236d8bbfc6024a32b7899c0815497201a
navinesh <navinesh.chand@gmail.com>
2017-08-29 07:49:47 +1200

    Add bootstrap 4 beta class

--- user/src/components/Header.js
+++ user/src/components/Header.js
@@ -17,39 +17,43 @@ export default class Navs extends Component<Props> {
 
   render() {
     return (
-      <nav className="navbar fixed-top navbar-toggleable-md">
+      <nav className="navbar fixed-top navbar-expand-lg">
         <div className="container">
+          <Link className="navbar-brand" to="/" style={{ color: '#707070' }}>
+            Leave Management System
+          </Link>
           <button
-            className="navbar-toggler navbar-toggler-right"
+            className="navbar-toggler"
             type="button"
             data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <Link className="navbar-brand" to="/" style={{ color: '#707070' }}>
-            Leave Management System
-          </Link>
           {this.props.isAuthenticated &&
-            <div
-              className="collapse navbar-collapse justify-content-end"
-              id="navbarNav"
-            >
-              <Link className="nav-item nav-link" to="/leaveapplication">
-                Leave application
-              </Link>
-              <Link className="nav-item nav-link" to="/leavecalendar">
-                Leave calendar
-              </Link>
-              <button
-                onClick={this.userLogout.bind(this)}
-                className="btn btn-primary ml-2"
+            <div className="justify-content-end">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
               >
-                Sign out
-              </button>
+                <div className="navbar-nav">
+                  <Link className="nav-item nav-link" to="/leaveapplication">
+                    Leave application
+                  </Link>
+                  <Link className="nav-item nav-link" to="/leavecalendar">
+                    Leave calendar
+                  </Link>
+                  <button
+                    onClick={this.userLogout.bind(this)}
+                    className="btn btn-primary ml-2"
+                  >
+                    Sign out
+                  </button>
+                </div>
+              </div>
             </div>}
         </div>
       </nav>
