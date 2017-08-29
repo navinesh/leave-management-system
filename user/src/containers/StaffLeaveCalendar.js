7b9c0a89b939db9c2dd8c8f71a3aea90e8a427cd
navinesh <navinesh.chand@gmail.com>
2017-08-30 08:49:29 +1200

    Add class

--- user/src/containers/StaffLeaveCalendar.js
+++ user/src/containers/StaffLeaveCalendar.js
@@ -20,7 +20,7 @@ class LeaveCalendar extends Component<Props> {
 
   render() {
     return (
-      <div>
+      <div className="container">
         {this.props.isFetching
           ? <div className="text-center">
               <div className="loader1" />
