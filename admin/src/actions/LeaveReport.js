0563ef8cb20cd886bf52ef93403040e3bc7e0016
navinesh <navinesh.chand@gmail.com>
2017-09-04 08:35:39 +1200

    Add redux action

--- admin/src/actions/LeaveReport.js
+++ admin/src/actions/LeaveReport.js
@@ -1,7 +1,11 @@
 // @flow
-export const REQUEST_LEAVE_RECORD = 'REQUEST_LEAVE_RECORD';
-export const RECEIVE_LEAVE_RECORD = 'RECEIVE_LEAVE_RECORD';
-export const ERROR_LEAVE_RECORD = 'ERROR_LEAVE_RECORD';
+export const REQUEST_LEAVE_RECORD = "REQUEST_LEAVE_RECORD";
+export const RECEIVE_LEAVE_RECORD = "RECEIVE_LEAVE_RECORD";
+export const ERROR_LEAVE_RECORD = "ERROR_LEAVE_RECORD";
+
+export const REQUEST_USER_UPDATES = "REQUEST_USER_UPDATES";
+export const RECEIVE_USER_UPDATES = "RECEIVE_USER_UPDATES";
+export const ERROR_USER_UPDATES = "ERROR_USER_UPDATES";
 
 export const requestLeaveRecord = () => ({
   type: REQUEST_LEAVE_RECORD
@@ -25,5 +29,30 @@ export const fetchLeaveRecord = () => async (dispatch: Function) => {
     dispatch(receiveLeaveRecord(data));
   } catch (error) {
     dispatch(errorLeaveRecord());
+  }
+};
+
+export const requestUserUpdates = () => ({
+  type: REQUEST_USER_UPDATES
+});
+
+export const receiveUserUpdates = (data: Object) => ({
+  type: RECEIVE_USER_UPDATES,
+  user_updates: data.user_updates,
+  receivedAt: Date.now()
+});
+
+export const errorUserUpdates = () => ({
+  type: ERROR_USER_UPDATES
+});
+
+export const fetchUserUpdates = () => async (dispatch: Function) => {
+  try {
+    dispatch(requestUserUpdates());
+    const response = await fetch(`http://localhost:8080/user-updates.api`);
+    const data = await response.json();
+    dispatch(receiveUserUpdates(data));
+  } catch (error) {
+    dispatch(errorUserUpdates());
   }
 };
