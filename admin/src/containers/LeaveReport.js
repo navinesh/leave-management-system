8ae1624b910a6229ef831b73b3196d450e8c0289
navinesh <navinesh.chand@gmail.com>
2017-09-05 10:03:55 +1200

    Add redux state

--- admin/src/containers/LeaveReport.js
+++ admin/src/containers/LeaveReport.js
@@ -1,13 +1,18 @@
 // @flow
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-import { fetchLoginFromToken } from "../actions/AdminLogin";
-import { fetchLeaveRecord } from "../actions/LeaveReport";
-import { fetchApprovedLeave } from "../actions/ApprovedLeave";
+import { fetchLoginFromToken } from '../actions/AdminLogin';
+import {
+  fetchCancelledRecord,
+  fetchDeclinedRecord,
+  fetchUserUpdates,
+  fetchLeaveUpdates
+} from '../actions/LeaveReport';
+import { fetchApprovedLeave } from '../actions/ApprovedLeave';
 import { fetchPendingLeave } from '../actions/PendingLeave';
-import LeaveReportList from "../components/LeaveReport";
+import LeaveReportList from '../components/LeaveReport';
 
 type Props = {
   isAuthenticated: boolean,
@@ -16,6 +21,8 @@ type Props = {
   leave_record: Array<any>,
   approved_items: Array<any>,
   pending_items: Array<any>,
+  user_updates: Array<any>,
+  leave_updates: Array<any>,
   dispatch: Function
 };
 
@@ -24,7 +31,7 @@ class LeaveReport extends Component<Props> {
     const { dispatch, auth_info } = this.props;
     let admin_token = auth_info.admin_token
       ? auth_info.admin_token
-      : localStorage.getItem("admin_token");
+      : localStorage.getItem('admin_token');
 
     if (admin_token) {
       dispatch(fetchLoginFromToken(admin_token));
@@ -33,9 +40,12 @@ class LeaveReport extends Component<Props> {
 
   componentDidMount() {
     if (this.props.isAuthenticated) {
-      this.props.dispatch(fetchLeaveRecord());
       this.props.dispatch(fetchApprovedLeave());
       this.props.dispatch(fetchPendingLeave());
+      this.props.dispatch(fetchCancelledRecord());
+      this.props.dispatch(fetchDeclinedRecord());
+      this.props.dispatch(fetchUserUpdates());
+      this.props.dispatch(fetchLeaveUpdates());
     }
   }
 
@@ -43,9 +53,12 @@ class LeaveReport extends Component<Props> {
     const {
       isAuthenticated,
       isFetching,
-      leave_record,
       approved_items,
-      pending_items
+      pending_items,
+      cancelled_record,
+      declined_record,
+      user_updates,
+      leave_updates
     } = this.props;
 
     return (
@@ -56,9 +69,12 @@ class LeaveReport extends Component<Props> {
                 <div className="loader1" />
               </div>
             : <LeaveReportList
-                leave_record={leave_record}
+                cancelled_record={cancelled_record}
+                declined_record={declined_record}
                 approved_items={approved_items}
                 pending_items={pending_items}
+                user_updates={user_updates}
+                leave_updates={leave_updates}
               />
           : <Redirect to="/login" />}
       </div>
@@ -67,19 +83,33 @@ class LeaveReport extends Component<Props> {
 }
 
 const mapStateToProps = state => {
-  const { adminAuth, leaveReport, approvedLeave, pendingLeave } = state;
+  const {
+    adminAuth,
+    approvedLeave,
+    pendingLeave,
+    cancelledReport,
+    declinedReport,
+    userUpdates,
+    leaveUpdates
+  } = state;
   const { auth_info, isAuthenticated } = adminAuth;
-  const { isFetching, leave_record } = leaveReport;
-  const { approved_items } = approvedLeave;
+  const { isFetching, approved_items } = approvedLeave;
   const { pending_items } = pendingLeave;
+  const { cancelled_record } = cancelledReport;
+  const { declined_record } = declinedReport;
+  const { user_updates } = userUpdates;
+  const { leave_updates } = leaveUpdates;
 
   return {
     auth_info,
     isAuthenticated,
     isFetching,
-    leave_record,
     approved_items,
-    pending_items
+    pending_items,
+    cancelled_record,
+    declined_record,
+    user_updates,
+    leave_updates
   };
 };
 
