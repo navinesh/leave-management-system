e3d59ec73ae96e951f106dd941b8822f30bfe18a
navinesh <navinesh.chand@gmail.com>
2017-09-08 09:24:07 +1200

    Add redux actions

--- admin/src/actions/LeaveReport.js
+++ admin/src/actions/LeaveReport.js
@@ -1,4 +1,12 @@
 // @flow
+export const REQUEST_APPROVED_LEAVE_REPORT = 'REQUEST_APPROVED_LEAVE_REPORT';
+export const RECEIVE_APPROVED_LEAVE_REPORT = 'RECEIVE_APPROVED_LEAVE_REPORT';
+export const ERROR_APPROVED_LEAVE_REPORT = 'ERROR_APPROVED_LEAVE_REPORT';
+
+export const REQUEST_PENDING_LEAVE_REPORT = 'REQUEST_PENDING_LEAVE_REPORT';
+export const RECEIVE_PENDING_LEAVE_REPORT = 'RECEIVE_PENDING_LEAVE_REPORT';
+export const ERROR_PENDING_LEAVE_REPORT = 'ERROR_PENDING_LEAVE_REPORT';
+
 export const REQUEST_CANCELLED_RECORD = 'REQUEST_CANCELLED_RECORD';
 export const RECEIVE_CANCELLED_RECORD = 'RECEIVE_CANCELLED_RECORD';
 export const ERROR_CANCELLED_RECORD = 'ERROR_CANCELLED_RECORD';
@@ -14,6 +22,60 @@ export const ERROR_USER_UPDATES = 'ERROR_USER_UPDATES';
 export const REQUEST_LEAVE_UPDATES = 'REQUEST_LEAVE_UPDATES';
 export const RECEIVE_LEAVE_UPDATES = 'RECEIVE_LEAVE_UPDATES';
 export const ERROR_LEAVE_UPDATES = 'ERROR_LEAVE_UPDATES';
+
+export const requestApprovedLeaveReport = () => ({
+  type: REQUEST_APPROVED_LEAVE_REPORT
+});
+
+export const receiveApprovedLeaveReport = (data: Object) => ({
+  type: RECEIVE_APPROVED_LEAVE_REPORT,
+  approved_record: data.approved_leave_report,
+  receivedAt: Date.now()
+});
+
+export const errorApprovedLeaveReport = () => ({
+  type: ERROR_APPROVED_LEAVE_REPORT
+});
+
+export const fetchApprovedLeaveReport = () => async (dispatch: Function) => {
+  try {
+    dispatch(requestApprovedLeaveReport());
+    const response = await fetch(
+      `http://localhost:8080/approved-leave-report.api`
+    );
+    const data = await response.json();
+    dispatch(receiveApprovedLeaveReport(data));
+  } catch (error) {
+    dispatch(errorApprovedLeaveReport());
+  }
+};
+
+export const requestPendingLeaveReport = () => ({
+  type: REQUEST_PENDING_LEAVE_REPORT
+});
+
+export const receivePendingLeaveReport = (data: Object) => ({
+  type: RECEIVE_PENDING_LEAVE_REPORT,
+  pending_record: data.pending_leave_report,
+  receivedAt: Date.now()
+});
+
+export const errorPendingLeaveReport = () => ({
+  type: ERROR_PENDING_LEAVE_REPORT
+});
+
+export const fetchPendingLeaveReport = () => async (dispatch: Function) => {
+  try {
+    dispatch(requestPendingLeaveReport());
+    const response = await fetch(
+      `http://localhost:8080/pending-leave-report.api`
+    );
+    const data = await response.json();
+    dispatch(receivePendingLeaveReport(data));
+  } catch (error) {
+    dispatch(errorPendingLeaveReport());
+  }
+};
 
 export const requestCancelledRecord = () => ({
   type: REQUEST_CANCELLED_RECORD
