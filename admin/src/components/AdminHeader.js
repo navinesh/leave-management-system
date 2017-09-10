ef95b17c92cfa4dfb31f56eaa5b875f2a7755635
navinesh <navinesh.chand@gmail.com>
2017-09-11 11:25:50 +1200

    Format

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -23,62 +23,60 @@ export default class Header extends Component<Props> {
 
   render() {
     return (
-      <div>
-        <nav className="navbar fixed-top navbar-expand-lg">
-          <div className="container">
-            <Link className="navbar-brand" to="/">
-              Leave Management System
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+      <nav className="navbar fixed-top navbar-expand-lg">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Leave Management System
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="justify-content-end">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
             >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="justify-content-end">
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <div className="navbar-nav">
-                  <Link className="nav-item nav-link" to="/approvedleave">
-                    Approved
-                  </Link>
-                  <Link className="nav-item nav-link" to="/staffrecord">
-                    Staff
-                  </Link>
-                  <Link className="nav-item nav-link" to="/archivedstaffrecord">
-                    Archived
-                  </Link>
-                  <Link className="nav-item nav-link" to="/newrecord">
-                    New
-                  </Link>
-                  <Link className="nav-item nav-link" to="/leavereport">
-                    Reports
-                  </Link>
-                  <Link className="nav-item nav-link" to="/sicksheetrecord">
-                    Sick sheet
-                  </Link>
-                  <Link className="nav-item nav-link" to="/publicholiday">
-                    Public Holidays
-                  </Link>
-                  <button
-                    onClick={this.adminLogout}
-                    className="btn btn-primary ml-1"
-                  >
-                    Sign out
-                  </button>
-                </div>
+              <div className="navbar-nav">
+                <Link className="nav-item nav-link" to="/approvedleave">
+                  Approved
+                </Link>
+                <Link className="nav-item nav-link" to="/staffrecord">
+                  Staff
+                </Link>
+                <Link className="nav-item nav-link" to="/archivedstaffrecord">
+                  Archived
+                </Link>
+                <Link className="nav-item nav-link" to="/newrecord">
+                  New
+                </Link>
+                <Link className="nav-item nav-link" to="/leavereport">
+                  Reports
+                </Link>
+                <Link className="nav-item nav-link" to="/sicksheetrecord">
+                  Sick sheet
+                </Link>
+                <Link className="nav-item nav-link" to="/publicholiday">
+                  Public Holidays
+                </Link>
+                <button
+                  onClick={this.adminLogout}
+                  className="btn btn-primary ml-1"
+                >
+                  Sign out
+                </button>
               </div>
             </div>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
