3198e9f0f5f0cc7640e6a20cbe6a1987b52628ee
navinesh <navinesh.chand@gmail.com>
2017-09-18 11:40:49 +1200

    Format

--- admin/src/containers/ApprovedLeave.js
+++ admin/src/containers/ApprovedLeave.js
@@ -55,25 +55,29 @@ class ApprovedLeave extends Component<Props> {
 
     return (
       <div className="container">
-        {isAuthenticated
-          ? isFetching
-            ? <div className="text-center">
-                <div className="loader1" />
-              </div>
-            : <ApprovedLeaveList
-                approved_items={approved_items}
-                public_holiday={public_holiday}
-                dispatch={dispatch}
-                isEditLeaveFetching={isEditLeaveFetching}
-                editLeaveMessage={editLeaveMessage}
-                isCancelLeaveFetching={isCancelLeaveFetching}
-                cancelLeaveMessage={cancelLeaveMessage}
-                onEditApprovedLeaveSubmit={editLeaveData =>
-                  dispatch(submitEditApprovedLeave(editLeaveData))}
-                onCancelLeaveSubmit={cancelLeaveData =>
-                  dispatch(submitCancelLeave(cancelLeaveData))}
-              />
-          : <Redirect to="/login" />}
+        {isAuthenticated ? (
+          isFetching ? (
+            <div className="text-center">
+              <div className="loader1" />
+            </div>
+          ) : (
+            <ApprovedLeaveList
+              approved_items={approved_items}
+              public_holiday={public_holiday}
+              dispatch={dispatch}
+              isEditLeaveFetching={isEditLeaveFetching}
+              editLeaveMessage={editLeaveMessage}
+              isCancelLeaveFetching={isCancelLeaveFetching}
+              cancelLeaveMessage={cancelLeaveMessage}
+              onEditApprovedLeaveSubmit={editLeaveData =>
+                dispatch(submitEditApprovedLeave(editLeaveData))}
+              onCancelLeaveSubmit={cancelLeaveData =>
+                dispatch(submitCancelLeave(cancelLeaveData))}
+            />
+          )
+        ) : (
+          <Redirect to="/login" />
+        )}
       </div>
     );
   }
