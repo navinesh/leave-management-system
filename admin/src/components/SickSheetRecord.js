2ffd4dfdbca3aec0b544df7ff37bfba268245806
navinesh <navinesh.chand@gmail.com>
2017-09-19 07:19:33 +1200

    Format

--- admin/src/components/SickSheetRecord.js
+++ admin/src/components/SickSheetRecord.js
@@ -7,23 +7,15 @@ type Props = {
 };
 
 export default (props: Props) => {
-  const itemNodes = props.sickSheet_items.map(record =>
+  const itemNodes = props.sickSheet_items.map(record => (
     <tr key={record.id}>
       <td>
         {record.user.othernames} {record.user.surname}
       </td>
-      <td>
-        {record.start_date}
-      </td>
-      <td>
-        {record.end_date}
-      </td>
-      <td>
-        {record.leave_days}
-      </td>
-      <td>
-        {record.date_posted}
-      </td>
+      <td>{record.start_date}</td>
+      <td>{record.end_date}</td>
+      <td>{record.leave_days}</td>
+      <td>{record.date_posted}</td>
       <td>
         <Link
           className="btn btn-primary btn-sm"
@@ -33,35 +25,35 @@ export default (props: Props) => {
         </Link>
       </td>
     </tr>
-  );
+  ));
 
-  return itemNodes.length > 0
-    ? <div className="table-responsive">
-        <table
-          className="table table-bordered table-hover"
-          style={{ backgroundColor: '#FFFFFF' }}
-        >
-          <thead className="thead-default">
-            <tr>
-              <th>Name</th>
-              <th>Start date</th>
-              <th>End date</th>
-              <th>Leave days</th>
-              <th>Date applied</th>
-              <th>Sick sheet</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemNodes}
-          </tbody>
-        </table>
-      </div>
-    : <div
-        className="card card-body border-0"
-        style={{ paddingTop: '100px', paddingBottom: '260px' }}
+  return itemNodes.length > 0 ? (
+    <div className="table-responsive">
+      <table
+        className="table table-bordered table-hover"
+        style={{ backgroundColor: '#FFFFFF' }}
       >
-        <h1 className="display-4 text-center">
-          <em>There is no record to display.</em>
-        </h1>
-      </div>;
+        <thead className="thead-default">
+          <tr>
+            <th>Name</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Leave days</th>
+            <th>Date applied</th>
+            <th>Sick sheet</th>
+          </tr>
+        </thead>
+        <tbody>{itemNodes}</tbody>
+      </table>
+    </div>
+  ) : (
+    <div
+      className="card card-body border-0"
+      style={{ paddingTop: '100px', paddingBottom: '260px' }}
+    >
+      <h1 className="display-4 text-center">
+        <em>There is no record to display.</em>
+      </h1>
+    </div>
+  );
 };
