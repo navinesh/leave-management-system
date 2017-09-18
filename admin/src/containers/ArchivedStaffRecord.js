13aa23f259e058a6213622b986cf93b89ea03f8b
navinesh <navinesh.chand@gmail.com>
2017-09-19 07:51:08 +1200

    Use local state
    Use local state for search term instead of pushing it to redux store because it's not used by another component

--- admin/src/containers/ArchivedStaffRecord.js
+++ admin/src/containers/ArchivedStaffRecord.js
@@ -5,7 +5,6 @@ import { Redirect } from 'react-router-dom';
 
 import { fetchLoginFromToken } from '../actions/AdminLogin';
 import { fetchArchivedStaffRecord } from '../actions/ArchivedStaffRecord';
-import { clearSearchStaffRecord } from '../actions/StaffRecord';
 import ArchivedStaffRecordList from '../components/ArchivedStaffRecord';
 import { submitUnArchiveUser } from '../actions/UnArchiveUser';
 
@@ -13,7 +12,6 @@ type Props = {
   isAuthenticated: boolean,
   auth_info: Object,
   archived_staff_record: Array<any>,
-  searchTerm: string,
   dispatch: Function,
   isUnArchiveFetching: boolean,
   unArchiveMessage: string,
@@ -38,15 +36,10 @@ class ArchivedStaffRecord extends Component<Props> {
     }
   }
 
-  componentWillUnmount() {
-    this.props.dispatch(clearSearchStaffRecord());
-  }
-
   render() {
     const {
       isAuthenticated,
       archived_staff_record,
-      searchTerm,
       dispatch,
       isUnArchiveFetching,
       unArchiveMessage,
@@ -63,7 +56,6 @@ class ArchivedStaffRecord extends Component<Props> {
           ) : (
             <ArchivedStaffRecordList
               archived_staff_record={archived_staff_record}
-              searchTerm={searchTerm}
               dispatch={dispatch}
               isUnArchiveFetching={isUnArchiveFetching}
               unArchiveMessage={unArchiveMessage}
@@ -80,26 +72,19 @@ class ArchivedStaffRecord extends Component<Props> {
 }
 
 const mapStateToProps = state => {
-  const {
-    adminAuth,
-    archivedStaffRecord,
-    searchStaffRecord,
-    unArchiveUser
-  } = state;
+  const { adminAuth, archivedStaffRecord, unArchiveUser } = state;
 
   const { auth_info, isAuthenticated } = adminAuth;
   const {
     archived_staff_record,
     isFetching: isDataFetching
   } = archivedStaffRecord;
-  const { searchTerm } = searchStaffRecord;
   const { isUnArchiveFetching, unArchiveMessage } = unArchiveUser;
 
   return {
     auth_info,
     isAuthenticated,
     archived_staff_record,
-    searchTerm,
     isUnArchiveFetching,
     unArchiveMessage,
     isDataFetching
