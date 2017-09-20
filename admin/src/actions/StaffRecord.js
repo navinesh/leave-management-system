914577b32717b423f8d51af3d48243e35507a00e
navinesh <navinesh.chand@gmail.com>
2017-09-21 07:12:56 +1200

    Remove action creattor

--- admin/src/actions/StaffRecord.js
+++ admin/src/actions/StaffRecord.js
@@ -3,7 +3,6 @@ export const REQUEST_STAFF_RECORD = 'REQUEST_STAFF_RECORD';
 export const RECEIVE_STAFF_RECORD = 'RECEIVE_STAFF_RECORD';
 export const ERROR_STAFF_RECORD = 'ERROR_STAFF_RECORD';
 export const STAFF_RECORD_SEARCH = 'STAFF_RECORD_SEARCH';
-export const CLEAR_STAFF_RECORD_SEARCH = 'CLEAR_STAFF_RECORD_SEARCH';
 
 export const requestStaffRecord = () => ({
   type: REQUEST_STAFF_RECORD
@@ -18,17 +17,6 @@ export const receiveStaffRecord = (data: Object) => ({
 export const errorStaffRecord = () => ({
   type: ERROR_STAFF_RECORD
 });
-
-export const searchStaffRecord = (searchTerm: string) => ({
-  type: STAFF_RECORD_SEARCH,
-  searchTerm
-});
-
-export const clearSearchStaffRecord = () => {
-  return {
-    type: CLEAR_STAFF_RECORD_SEARCH
-  };
-};
 
 export const fetchStaffRecord = () => async (dispatch: Function) => {
   try {
