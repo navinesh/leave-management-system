caeaf5f7f54d89893a8edeca7e84a2b938419fa6
navinesh <navinesh.chand@gmail.com>
2017-09-25 13:15:04 +1200

    Add react-apollo module

--- user/flow-typed/modules.js
+++ user/flow-typed/modules.js
@@ -3,6 +3,10 @@ declare module 'react-router-dom' {
   declare var exports: any;
 }
 
+declare module 'react-apollo' {
+  declare var exports: any;
+}
+
 declare module 'redux' {
   declare var exports: any;
 }
