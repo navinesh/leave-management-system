0b6b149676bc7f016b00f46a732b17b11b19699a
navinesh <navinesh.chand@gmail.com>
2017-09-28 10:32:41 +1200

    First commit

---
+++ user/src/components/UserDetail.js
@@ -0,0 +1,116 @@
+// @flow
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { graphql, gql } from 'react-apollo';
+
+import '../spinners.css';
+
+const USER_DETAIL = gql`
+  query($id: Int) {
+    findUser(id: $id) {
+      othernames
+      surname
+      annual
+      sick
+      bereavement
+      christmas
+      maternity
+      gender
+    }
+  }
+`;
+
+type Props = {
+  userDetail: Object
+};
+
+const UserDetail = (props: Props) => {
+  const { userDetail: { loading, error, findUser: user } } = props;
+
+  if (loading) {
+    return (
+      <div className="container text-center" style={{ paddingTop: '100px' }}>
+        <div className="col-md-8 ml-auto mr-auto">
+          <div className="loader1" />
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    console.log(error.message);
+    return (
+      <div className="container text-center" style={{ paddingTop: '100px' }}>
+        <div className="col-md-8 ml-auto mr-auto">
+          <p>Something went wrong!</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="jumbotron jumbotron-fluid"
+      style={{ backgroundColor: '#FFFFFF', paddingTop: '80px' }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <p className="display-4">
+              {user.othernames} {user.surname}
+            </p>
+            <p>
+              <Link to="/changepassword" className="btn btn-primary">
+                Change password
+              </Link>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <ul className="list-group">
+              <li className="list-group-item">
+                Annual
+                <span className="badge badge-primary badge-pill float-right">
+                  {user.annual}
+                </span>
+              </li>
+              <li className="list-group-item">
+                Sick
+                <span className="badge badge-primary badge-pill float-right">
+                  {user.sick}
+                </span>
+              </li>
+              <li className="list-group-item">
+                Bereavement
+                <span className="badge badge-primary badge-pill float-right">
+                  {user.bereavement}
+                </span>
+              </li>
+              <li className="list-group-item">
+                Christmas
+                <span className="badge badge-primary badge-pill float-right">
+                  {user.christmas}
+                </span>
+              </li>
+              {user.gender.toLowerCase() === 'female' &&
+                user.maternity > 0 && (
+                  <li className="list-group-item">
+                    Maternity
+                    <span className="badge badge-primary badge-pill float-right">
+                      {user.maternity}
+                    </span>
+                  </li>
+                )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default graphql(USER_DETAIL, {
+  options: ({ id }) => ({
+    variables: { id }
+  }),
+  name: 'userDetail'
+})(UserDetail);
