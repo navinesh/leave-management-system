2be57e934b21aac0e6fd99f6b171facb3faaf362
navinesh <navinesh.chand@gmail.com>
2017-09-28 10:32:27 +1200

    First commit

---
+++ user/src/containers/UserDetail.js
@@ -0,0 +1,26 @@
+// @flow
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+import UserDetail from '../components/UserDetail';
+
+type Props = {
+  auth_info: Object
+};
+
+const UserDetails = (props: Props) => {
+  let userID = props.auth_info.user_id
+    ? props.auth_info.user_id
+    : localStorage.getItem('user_id');
+
+  return <UserDetail id={userID} />;
+};
+
+const mapStateToProps = state => {
+  const { userAuth } = state;
+  const { auth_info } = userAuth;
+  return { auth_info };
+};
+
+export default connect(mapStateToProps)(UserDetails);
