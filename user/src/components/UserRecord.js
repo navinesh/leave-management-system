9807a730b9d1d9f49a69d48c945eac1bb692d3a6
navinesh <navinesh.chand@gmail.com>
2017-09-29 10:56:12 +1200

    Add react-apollo query

--- user/src/components/UserRecord.js
+++ user/src/components/UserRecord.js
@@ -1,8 +1,28 @@
 // @flow
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { graphql, gql } from 'react-apollo';
 
 import '../spinners.css';
+
+const USER_RECORD = gql`
+  query($id: Int) {
+    findUser(id: $id) {
+      leaverecord {
+        edges {
+          node {
+            id
+            leaveName
+            leaveDays
+            startDate
+            endDate
+            leaveReason
+            leaveStatus
+          }
+        }
+      }
+    }
+  }
+`;
 
 const PendingRecordList = props => {
   const pendingList = props.user_record.leaverecord.edges
@@ -83,11 +103,11 @@ const ApprovedRecordList = props => {
 };
 
 type Props = {
-  user_record: Object
+  userRecord: Object
 };
 
-export const RecordList = (props: Props) => {
-  const { user_record: { loading, error, user } } = props;
+export const UserRecord = (props: Props) => {
+  const { userRecord: { loading, error, findUser: user } } = props;
 
   if (loading) {
     return (
@@ -120,92 +140,9 @@ export const RecordList = (props: Props) => {
   );
 };
 
-type userRecordProps = {
-  user_detail: Object
-};
-
-export const UserRecord = (props: userRecordProps) => {
-  const { user_detail: { loading, error, user } } = props;
-
-  if (loading) {
-    return (
-      <div className="container text-center" style={{ paddingTop: '100px' }}>
-        <div className="col-md-8 ml-auto mr-auto">
-          <div className="loader1" />
-        </div>
-      </div>
-    );
-  }
-
-  if (error) {
-    console.log(error.message);
-    return (
-      <div className="container text-center" style={{ paddingTop: '100px' }}>
-        <div className="col-md-8 ml-auto mr-auto">
-          <p>Something went wrong!</p>
-        </div>
-      </div>
-    );
-  }
-
-  let gender = user.gender ? user.gender.toLowerCase() : null;
-
-  return (
-    <div
-      className="jumbotron jumbotron-fluid"
-      style={{ backgroundColor: '#FFFFFF', paddingTop: '80px' }}
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">
-            <p className="display-4">
-              {user.othernames} {user.surname}
-            </p>
-            <p>
-              <Link to="/changepassword" className="btn btn-primary">
-                Change password
-              </Link>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <ul className="list-group">
-              <li className="list-group-item">
-                Annual
-                <span className="badge badge-primary badge-pill float-right">
-                  {user.annual}
-                </span>
-              </li>
-              <li className="list-group-item">
-                Sick
-                <span className="badge badge-primary badge-pill float-right">
-                  {user.sick}
-                </span>
-              </li>
-              <li className="list-group-item">
-                Bereavement
-                <span className="badge badge-primary badge-pill float-right">
-                  {user.bereavement}
-                </span>
-              </li>
-              <li className="list-group-item">
-                Christmas
-                <span className="badge badge-primary badge-pill float-right">
-                  {user.christmas}
-                </span>
-              </li>
-              {gender === 'female' &&
-                user.maternity > 0 && (
-                  <li className="list-group-item">
-                    Maternity
-                    <span className="badge badge-primary badge-pill float-right">
-                      {user.maternity}
-                    </span>
-                  </li>
-                )}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+export default graphql(USER_RECORD, {
+  options: ({ id }) => ({
+    variables: { id }
+  }),
+  name: 'userRecord'
+})(UserRecord);
