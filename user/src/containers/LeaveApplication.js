4baee07262e7c0f649c67fdce694c3cbaee46b2f
navinesh <navinesh.chand@gmail.com>
2017-10-03 12:50:32 +1200

    Remove redux prop

--- user/src/containers/LeaveApplication.js
+++ user/src/containers/LeaveApplication.js
@@ -12,8 +12,7 @@ type Props = {
   auth_info: Object,
   dispatch: Function,
   isAuthenticated: boolean,
-  message: string,
-  isFetching: boolean
+  message: string
 };
 
 class LeaveApplication extends Component<Props> {
@@ -29,7 +28,7 @@ class LeaveApplication extends Component<Props> {
   }
 
   render() {
-    const { dispatch, isAuthenticated, message, isFetching } = this.props;
+    const { dispatch, isAuthenticated, message } = this.props;
     let userID = this.props.auth_info.user_id
       ? this.props.auth_info.user_id
       : localStorage.getItem('user_id');
@@ -38,8 +37,8 @@ class LeaveApplication extends Component<Props> {
         {isAuthenticated ? (
           <Application
             id={userID}
-            isFetching={isFetching}
             message={message}
+            dispatch={dispatch}
             onLeaveApplicationClick={applicationDetails =>
               dispatch(fetchLeaveApplication(applicationDetails))}
           />
@@ -53,15 +52,13 @@ class LeaveApplication extends Component<Props> {
 
 const mapStateToProps = state => {
   const { userAuth, leaveApplication } = state;
-
   const { auth_info, isAuthenticated } = userAuth;
-  const { isFetching, message } = leaveApplication;
+  const { message } = leaveApplication;
 
   return {
     auth_info,
     isAuthenticated,
-    message,
-    isFetching
+    message
   };
 };
 
