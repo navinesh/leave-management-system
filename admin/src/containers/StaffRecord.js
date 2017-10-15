6d0d86bee8b429c644b61d577704c3294976aace
navinesh <navinesh.chand@gmail.com>
2017-10-16 07:40:49 +1200

    Add graphql query

--- admin/src/containers/StaffRecord.js
+++ admin/src/containers/StaffRecord.js
@@ -2,17 +2,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { graphql, gql, compose } from 'react-apollo';
 
 import { fetchLoginFromToken } from '../actions/AdminLogin';
-import { fetchStaffRecord } from '../actions/StaffRecord';
 import StaffRecordList from '../components/StaffRecord';
 import { submitModifyUserRecord } from '../actions/ModifyRecord';
 import { submitArchiveUser } from '../actions/ArchiveUser';
 
+const ACTIVE_USERS = gql`
+  {
+    findUsers(isArchived: "false") {
+      id
+      dbId
+      othernames
+      surname
+      email
+      annual
+      sick
+      christmas
+      bereavement
+      dateOfBirth
+      maternity
+      gender
+    }
+  }
+`;
+
 type Props = {
   isAuthenticated: boolean,
   auth_info: Object,
-  staff_record: Array<any>,
+  activeUsers: Object,
   dispatch: Function,
   isFetching: boolean,
   message: string,
@@ -23,7 +42,7 @@ type Props = {
 
 class StaffRecord extends Component<Props> {
   componentWillMount() {
-    const { dispatch, auth_info } = this.props;
+    const { auth_info, dispatch } = this.props;
     let admin_token = auth_info.admin_token
       ? auth_info.admin_token
       : localStorage.getItem('admin_token');
@@ -33,46 +52,50 @@ class StaffRecord extends Component<Props> {
     }
   }
 
-  componentDidMount() {
-    if (this.props.isAuthenticated) {
-      this.props.dispatch(fetchStaffRecord());
-    }
-  }
-
   render() {
     const {
       isAuthenticated,
-      staff_record,
+      activeUsers: { loading, error, refetch, findUsers: staff_record },
       dispatch,
       isFetching,
       message,
       isArchiveFetching,
-      archiveMessage,
-      isDataFetching
+      archiveMessage
     } = this.props;
+
+    if (loading) {
+      return (
+        <div className="text-center">
+          <div className="loader1" />
+        </div>
+      );
+    }
+
+    if (error) {
+      console.log(error);
+      return (
+        <div className="text-center">
+          <p>Something went wrong!</p>
+        </div>
+      );
+    }
 
     return (
       <div className="container">
         {isAuthenticated ? (
-          isDataFetching ? (
-            <div className="text-center">
-              <div className="loader1" />
-            </div>
-          ) : (
-            <StaffRecordList
-              staff_record={staff_record}
-              dispatch={dispatch}
-              isFetching={isFetching}
-              message={message}
-              isArchiveFetching={isArchiveFetching}
-              archiveMessage={archiveMessage}
-              onModifyUserRecordSubmit={modifyUserDetails =>
-                dispatch(submitModifyUserRecord(modifyUserDetails))}
-              onArchiveUserSubmit={archiveUser =>
-                dispatch(submitArchiveUser(archiveUser))}
-              fetchStaffRecord={fetchStaffRecord}
-            />
-          )
+          <StaffRecordList
+            staff_record={staff_record}
+            dispatch={dispatch}
+            isFetching={isFetching}
+            message={message}
+            isArchiveFetching={isArchiveFetching}
+            archiveMessage={archiveMessage}
+            onModifyUserRecordSubmit={modifyUserDetails =>
+              dispatch(submitModifyUserRecord(modifyUserDetails))}
+            onArchiveUserSubmit={archiveUser =>
+              dispatch(submitArchiveUser(archiveUser))}
+            refetch={refetch}
+          />
         ) : (
           <Redirect to="/login" />
         )}
@@ -82,23 +105,25 @@ class StaffRecord extends Component<Props> {
 }
 
 const mapStateToProps = state => {
-  const { adminAuth, staffRecord, modifyUser, archiveUser } = state;
+  const { adminAuth, modifyUser, archiveUser } = state;
 
   const { auth_info, isAuthenticated } = adminAuth;
-  const { staff_record, isFetching: isDataFetching } = staffRecord;
   const { isFetching, message } = modifyUser;
   const { isArchiveFetching, archiveMessage } = archiveUser;
 
   return {
     auth_info,
     isAuthenticated,
-    staff_record,
     isFetching,
     message,
     isArchiveFetching,
-    archiveMessage,
-    isDataFetching
+    archiveMessage
   };
 };
 
-export default connect(mapStateToProps)(StaffRecord);
+export default compose(
+  connect(mapStateToProps),
+  graphql(ACTIVE_USERS, {
+    name: 'activeUsers'
+  })
+)(StaffRecord);
