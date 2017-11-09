22e8af559a7a33f39947436daf376d5359d073ad
navinesh <navinesh.chand@gmail.com>
2017-11-10 11:04:34 +1300

    Add func to check token using graphql api

--- admin/src/containers/PublicHoliday.js
+++ admin/src/containers/PublicHoliday.js
@@ -2,27 +2,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { graphql, compose } from 'react-apollo';
 
-import { fetchLoginFromToken } from '../actions/AdminLogin';
+import {
+  requestAdminLoginFromToken,
+  receiveAdminLoginFromToken,
+  loginAdminErrorFromToken
+} from '../actions/AdminLogin';
 import PublicHolidays from '../components/PublicHoliday';
+
+const VERIFY_ADMIN_TOKEN = gql`
+  mutation verifyAdminToken($adminToken: String!) {
+    verifyAdminToken(adminToken: $adminToken) {
+      token
+      ok
+    }
+  }
+`;
 
 type Props = {
   isAuthenticated: boolean,
   auth_info: Object,
-  dispatch: Function
+  dispatch: Function,
+  verifyAdminToken: Function
 };
 
 class PublicHoliday extends Component<Props> {
   componentWillMount() {
-    const { dispatch, auth_info } = this.props;
+    const { auth_info } = this.props;
     let admin_token = auth_info.admin_token
       ? auth_info.admin_token
       : localStorage.getItem('admin_token');
 
     if (admin_token) {
-      dispatch(fetchLoginFromToken(admin_token));
+      this.verifyToken();
     }
   }
+
+  verifyToken = async () => {
+    const { dispatch, verifyAdminToken } = this.props;
+    const adminToken = localStorage.getItem('admin_token');
+
+    try {
+      dispatch(requestAdminLoginFromToken());
+      const response = await verifyAdminToken({
+        variables: { adminToken }
+      });
+      dispatch(
+        receiveAdminLoginFromToken(response.data.verifyAdminToken.token)
+      );
+    } catch (error) {
+      console.log(error);
+      localStorage.removeItem('admin_token');
+      dispatch(loginAdminErrorFromToken('Your session has expired!'));
+    }
+  };
 
   render() {
     const { isAuthenticated } = this.props;
@@ -45,4 +80,9 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(PublicHoliday);
+export default compose(
+  connect(mapStateToProps),
+  graphql(VERIFY_ADMIN_TOKEN, {
+    name: 'verifyAdminToken'
+  })
+)(PublicHoliday);
