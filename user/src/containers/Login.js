e9f8acd0a0e9e4cefdd6f5724222ca43d3cd9fea
navinesh <navinesh.chand@gmail.com>
2017-11-10 11:04:56 +1300

    Format

--- user/src/containers/Login.js
+++ user/src/containers/Login.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+
 import { fetchLoginFromToken } from '../actions/UserLogin';
 import LeaveCalendar from './LeaveCalendar';
 import UserLogin from './UserLogin';
