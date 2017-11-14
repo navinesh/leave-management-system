caa50f47bd04d6d5ccc7d1a3eba709fe72b45834
navinesh <navinesh.chand@gmail.com>
2017-11-15 10:01:13 +1300

    Remove redux action

--- admin/src/actions/UnArchiveUser.js
+++ admin/src/actions/UnArchiveUser.js
@@ -1,41 +1,19 @@
 // @flow
-import axios from 'axios';
-
 export const UNARCHIVE_USER_REQUEST = 'UNARCHIVE_USER_REQUEST';
 export const UNARCHIVE_USER_SUCCESS = 'UNARCHIVE_USER_SUCCESS';
 export const UNARCHIVE_USER_FAILURE = 'UNARCHIVE_USER_FAILURE';
 export const CLEAR_UNARCHIVE_MESSAGE = 'CLEAR_UNARCHIVE_MESSAGE';
 
-export const requestUnArchiveUser = (unArchiveUser: Object) => {
-  return { type: UNARCHIVE_USER_REQUEST, unArchiveUser };
+export const requestUnArchiveUser = () => {
+  return { type: UNARCHIVE_USER_REQUEST };
 };
 
-export const successUnArchiveUser = (data: Object) => {
-  return { type: UNARCHIVE_USER_SUCCESS, message: data.message };
+export const successUnArchiveUser = (data: string) => {
+  return { type: UNARCHIVE_USER_SUCCESS, message: data };
 };
 
-export const failureUnArchiveUser = (data: Object) => {
-  return { type: UNARCHIVE_USER_FAILURE, message: data.message };
+export const failureUnArchiveUser = (data: string) => {
+  return { type: UNARCHIVE_USER_FAILURE, message: data };
 };
 
 export const clearUnArchiveMessage = () => ({ type: CLEAR_UNARCHIVE_MESSAGE });
-
-export const submitUnArchiveUser = (unArchiveUser: Object) => async (
-  dispatch: Function
-) => {
-  try {
-    dispatch(requestUnArchiveUser(unArchiveUser));
-    const response = await axios.post('http://localhost:8080/unarchiveuser', {
-      user_id: unArchiveUser.id,
-      isArchived: unArchiveUser.isArchived
-    });
-
-    if (response.status !== 201) {
-      dispatch(failureUnArchiveUser(response.data));
-    } else {
-      dispatch(successUnArchiveUser(response.data));
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
