f176be4aff5110eba90730621aeddd580e255b15
navinesh <navinesh.chand@gmail.com>
2017-11-15 09:32:13 +1300

    Remove action creator

--- admin/src/actions/ArchiveUser.js
+++ admin/src/actions/ArchiveUser.js
@@ -1,42 +1,19 @@
 // @flow
-import axios from 'axios';
-
 export const ARCHIVE_USER_REQUEST = 'ARCHIVE_USER_REQUEST';
 export const ARCHIVE_USER_SUCCESS = 'ARCHIVE_USER_SUCCESS';
 export const ARCHIVE_USER_FAILURE = 'ARCHIVE_USER_FAILURE';
 export const CLEAR_ARCHIVE_MESSAGE = 'CLEAR_ARCHIVE_MESSAGE';
 
-export const requestArchiveUser = (archiveUser: Object) => {
-  return { type: ARCHIVE_USER_REQUEST, archiveUser };
+export const requestArchiveUser = () => {
+  return { type: ARCHIVE_USER_REQUEST };
 };
 
-export const successArchiveUser = (data: Object) => {
-  return { type: ARCHIVE_USER_SUCCESS, message: data.message };
+export const successArchiveUser = (data: string) => {
+  return { type: ARCHIVE_USER_SUCCESS, message: data };
 };
 
-export const failureArchiveUser = (data: Object) => {
-  return { type: ARCHIVE_USER_FAILURE, message: data.message };
+export const failureArchiveUser = (data: string) => {
+  return { type: ARCHIVE_USER_FAILURE, message: data };
 };
 
 export const clearArchiveMessage = () => ({ type: CLEAR_ARCHIVE_MESSAGE });
-
-export const submitArchiveUser = (archiveUser: Object) => async (
-  dispatch: Function
-) => {
-  try {
-    dispatch(requestArchiveUser(archiveUser));
-    const response = await axios.post('http://localhost:8080/archiveuser', {
-      user_id: archiveUser.id,
-      isArchived: archiveUser.isArchived,
-      archiveReason: archiveUser.archiveReason
-    });
-
-    if (response.status !== 201) {
-      dispatch(failureArchiveUser(response.data));
-    } else {
-      dispatch(successArchiveUser(response.data));
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
