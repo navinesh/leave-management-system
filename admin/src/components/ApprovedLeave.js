6a159e6f3d7abf159d4d8ad97ee1193a46bbe69b
navinesh <navinesh.chand@gmail.com>
2017-12-01 09:09:43 +1300

    Format

--- admin/src/components/ApprovedLeave.js
+++ admin/src/components/ApprovedLeave.js
@@ -463,10 +463,12 @@ export default class ApprovedLeaveList extends Component<Props, State> {
                               startDate={this.state.startDate}
                               endDate={this.state.endDate}
                               onDatesChange={({ startDate, endDate }) =>
-                                this.setState({ startDate, endDate })}
+                                this.setState({ startDate, endDate })
+                              }
                               focusedInput={this.state.focusedInput}
                               onFocusChange={focusedInput =>
-                                this.setState({ focusedInput })}
+                                this.setState({ focusedInput })
+                              }
                               isOutsideRange={() => false}
                               minimumNights={0}
                               showDefaultInputIcon
