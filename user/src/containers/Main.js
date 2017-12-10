34e71c61960613e7bbc179848ac327ab986c7611
navinesh <navinesh.chand@gmail.com>
2017-12-11 10:42:27 +1300

    Check token in function

--- user/src/containers/Main.js
+++ user/src/containers/Main.js
@@ -34,38 +34,37 @@ type Props = {
 };
 
 class Main extends Component<Props> {
-  componentDidMount() {
-    const { auth_info } = this.props;
-    let auth_token = auth_info.auth_token
-      ? auth_info.auth_token
-      : localStorage.getItem('auth_token');
-
-    if (auth_token) {
-      this.verifyToken();
-    }
+  componentWillMount() {
+    this.verifyToken();
+    setInterval(this.verifyToken, 600000);
   }
 
   verifyToken = async () => {
-    const { dispatch, verifyUserToken } = this.props;
-    const userToken = localStorage.getItem('auth_token');
+    const { auth_info, dispatch, verifyUserToken } = this.props;
 
-    try {
-      dispatch(requestUserLoginFromToken());
-      const response = await verifyUserToken({
-        variables: { userToken }
-      });
-      const auth_info = {
-        auth_token: response.data.verifyUserToken.token,
-        user_id: response.data.verifyUserToken.User.dbId,
-        id: response.data.verifyUserToken.User.id
-      };
-      dispatch(receiveUserLoginFromToken(auth_info));
-    } catch (error) {
-      console.log(error);
-      localStorage.removeItem('auth_token');
-      localStorage.removeItem('user_id');
-      localStorage.removeItem('id');
-      dispatch(loginUserErrorFromToken('Your session has expired!'));
+    const userToken = auth_info.auth_token
+      ? auth_info.auth_token
+      : localStorage.getItem('auth_token');
+
+    if (userToken) {
+      try {
+        dispatch(requestUserLoginFromToken());
+        const response = await verifyUserToken({
+          variables: { userToken }
+        });
+        const auth_info = {
+          auth_token: response.data.verifyUserToken.token,
+          user_id: response.data.verifyUserToken.User.dbId,
+          id: response.data.verifyUserToken.User.id
+        };
+        dispatch(receiveUserLoginFromToken(auth_info));
+      } catch (error) {
+        console.log(error);
+        localStorage.removeItem('auth_token');
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('id');
+        dispatch(loginUserErrorFromToken('Your session has expired!'));
+      }
     }
   };
 
@@ -93,7 +92,5 @@ const mapStateToProps = state => {
 
 export default compose(
   connect(mapStateToProps),
-  graphql(VERIFY_USER_TOKEN, {
-    name: 'verifyUserToken'
-  })
+  graphql(VERIFY_USER_TOKEN, { name: 'verifyUserToken' })
 )(Main);
