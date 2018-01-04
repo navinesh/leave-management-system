3caba103b0b6357bdc15961f5676511e7222c2f2
navinesh <navinesh.chand@gmail.com>
2018-01-05 12:42:33 +1300

    Remove redux actions

--- user/src/actions/UserLogout.js
+++ user/src/actions/UserLogout.js
@@ -1,15 +1,11 @@
 // @flow
-export const LOGOUT_USER_REQUEST = 'LOGOUT_USER_REQUEST';
-export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS';
+export const LOGOUT_USER = 'LOGOUT_USER';
 
-export const requestLogout = () => ({ type: LOGOUT_USER_REQUEST });
-
-export const receiveLogout = () => ({ type: LOGOUT_USER_SUCCESS });
+export const requestLogout = () => ({ type: LOGOUT_USER });
 
 export const logoutUser = () => (dispatch: Function) => {
-  dispatch(requestLogout());
-  dispatch(receiveLogout());
   localStorage.removeItem('auth_token');
   localStorage.removeItem('user_id');
   localStorage.removeItem('id');
+  dispatch(requestLogout());
 };
