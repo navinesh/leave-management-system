13717eea8445475c68ab7df9d56e3911eade17f7
navinesh <navinesh.chand@gmail.com>
2018-02-20 10:28:20 +1200

    Delete depreciated exports

--- user/flow-typed/modules.js
+++ user/flow-typed/modules.js
@@ -1,65 +1,53 @@
 // @flow
 declare module 'react-router-dom' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'react-apollo' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
-declare module 'apollo-client' {
-  declare var exports: any;
-}
-
-declare module 'apollo-link-http' {
-  declare var exports: any;
-}
-
-declare module 'apollo-cache-inmemory' {
-  declare var exports: any;
-}
-
-declare module 'graphql-tag' {
-  declare var exports: any;
+declare module 'apollo-boost' {
+  declare module.exports: any;
 }
 
 declare module 'redux' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'react-redux' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'redux-thunk' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 // remove in production
 declare module 'redux-logger' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'moment' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'moment-range' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'axios' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'react-dates' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'react-dates/lib/css/_datepicker.css' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'react-dates/initialize' {
-  declare var exports: any;
+  declare module.exports: any;
 }
