31faf1832269152d75b105cba5a322602b2a30f2
navinesh <navinesh.chand@gmail.com>
2018-02-26 13:37:44 +1200

    Use apollo-boost

--- admin/src/index.js
+++ admin/src/index.js
@@ -4,10 +4,8 @@ import { render } from 'react-dom';
 
 import { Provider } from 'react-redux';
 
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
@@ -31,10 +29,7 @@ import Error from './components/Error';
 import configureStore from './stores/ConfigureStore';
 const store = configureStore();
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
-  cache: new InMemoryCache()
-});
+const client = new ApolloClient({ uri: 'http://localhost:8080/graphql' });
 
 const PrivateRoute = ({ component, ...rest }) => (
   <Route
