390d2b82c2c01682f62233e18b04ba80c285b809
navinesh <navinesh.chand@gmail.com>
2018-05-01 14:34:50 +1200

    Use React Apollo Query component

--- admin/src/containers/ArchivedStaffRecord.js
+++ admin/src/containers/ArchivedStaffRecord.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { gql } from 'apollo-boost';
-import { graphql, compose } from 'react-apollo';
+import { graphql, compose, Query } from 'react-apollo';
 
 import {
   requestAdminLoginFromToken,
@@ -44,7 +44,6 @@ type Props = {
   isAuthenticated: boolean,
   auth_info: Object,
   verifyAdminToken: Function,
-  dispatch: Function,
   archivedUsers: Object
 };
 
@@ -78,37 +77,43 @@ class ArchivedStaffRecord extends Component<Props> {
   };
 
   render() {
-    const {
-      isAuthenticated,
-      archivedUsers: { loading, error, findUsers, refetch },
-      dispatch
-    } = this.props;
-
-    if (loading) {
-      return (
-        <div className="text-center">
-          <div className="loader1" />
-        </div>
-      );
-    }
-
-    if (error) {
-      console.log(error);
-      return (
-        <div className="text-center">
-          <p>Something went wrong!</p>
-        </div>
-      );
-    }
+    const { isAuthenticated } = this.props;
 
     return (
       <div className="container">
         {isAuthenticated ? (
-          <ArchivedStaffRecordList
-            archived_staff_record={findUsers}
-            dispatch={dispatch}
-            refetch={refetch}
-          />
+          <Query query={ARCHIVED_USERS}>
+            {({
+              loading,
+              error,
+              data: { findUsers: staff_record },
+              refetch
+            }) => {
+              if (loading) {
+                return (
+                  <div className="text-center">
+                    <div className="loader1" />
+                  </div>
+                );
+              }
+
+              if (error) {
+                console.log(error);
+                return (
+                  <div className="text-center">
+                    <p>Something went wrong!</p>
+                  </div>
+                );
+              }
+
+              return (
+                <ArchivedStaffRecordList
+                  archived_staff_record={staff_record}
+                  refetch={refetch}
+                />
+              );
+            }}
+          </Query>
         ) : (
           <Redirect to="/login" />
         )}
@@ -121,19 +126,13 @@ const mapStateToProps = state => {
   const { adminAuth } = state;
 
   const { auth_info, isAuthenticated } = adminAuth;
-  return {
-    auth_info,
-    isAuthenticated
-  };
+
+  return { auth_info, isAuthenticated };
 };
 
 export default compose(
   connect(mapStateToProps),
   graphql(VERIFY_ADMIN_TOKEN, {
     name: 'verifyAdminToken'
-  }),
-  graphql(ARCHIVED_USERS, {
-    name: 'archivedUsers',
-    options: { pollInterval: 60000 }
   })
 )(ArchivedStaffRecord);
