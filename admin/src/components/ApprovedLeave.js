5f215a05ee2d99dd98af46e0c837db0cac09a3bf
navinesh <navinesh.chand@gmail.com>
2018-06-28 11:37:27 +1200

    Delete unused imports

--- admin/src/components/ApprovedLeave.js
+++ admin/src/components/ApprovedLeave.js
@@ -1,9 +1,7 @@
 // @flow
 import React, { Component, Fragment } from 'react';
 
-import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
 import Moment from 'moment';
 import { extendMoment } from 'moment-range';
