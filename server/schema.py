1a08e3b6db79b34845c00adb918488396d2c92bd
navinesh <navinesh.chand@gmail.com>
2018-07-12 12:00:22 +1200

    Upgrade to v2.0

--- server/schema.py
+++ server/schema.py
@@ -70,28 +70,28 @@ class Query(graphene.ObjectType):
     find_user_updates = graphene.List(
         lambda: Userupdates, is_archived=graphene.String())
 
-    def resolve_find_user_updates(self, args, context, info):
+    def resolve_find_user_updates(self, info, is_archived):
         """Returns user updates"""
-        query = Userupdates.get_query(context)
-        is_archived = args.get('is_archived')
+        query = Userupdates.get_query(info)
+
         return query.join(UserModel).filter(
             UserModel.isArchived == is_archived)
 
     find_users = graphene.List(lambda: User, is_archived=graphene.String())
 
-    def resolve_find_users(self, args, context, info):
+    def resolve_find_users(self, info, is_archived):
         """Returns users"""
-        query = User.get_query(context)
-        is_archived = args.get('is_archived')
+        query = User.get_query(info)
+
         return query.filter(UserModel.isArchived == is_archived)
 
     find_leave_updates = graphene.List(
         lambda: Leaveupdates, is_archived=graphene.String())
 
-    def resolve_find_leave_updates(self, args, context, info):
+    def resolve_find_leave_updates(self, info, is_archived):
         """Returns leave updates"""
-        query = Leaveupdates.get_query(context)
-        is_archived = args.get('is_archived')
+        query = Leaveupdates.get_query(info)
+
         return query.join(LeaverecordModel).join(UserModel).filter(
             UserModel.isArchived == is_archived)
 
@@ -100,26 +100,25 @@ class Query(graphene.ObjectType):
         leave_status=graphene.String(),
         is_archived=graphene.String())
 
-    def resolve_find_leave_record(self, args, context, info):
+    def resolve_find_leave_record(self, info, leave_status, is_archived):
         """Returns leave records"""
-        query = Leaverecord.get_query(context)
-        leave_status = args.get('leave_status')
-        is_archived = args.get('is_archived')
+        query = Leaverecord.get_query(info)
+
         return query.filter(LeaverecordModel.leave_status ==
                             leave_status).join(UserModel).filter(
                                 UserModel.isArchived == is_archived)
 
     find_sicksheet_record = graphene.List(lambda: Leaverecord)
 
-    def resolve_find_sicksheet_record(self, args, context, info):
+    def resolve_find_sicksheet_record(self, info):
         """Returns sicksheet record"""
-        query = Leaverecord.get_query(context)
+        query = Leaverecord.get_query(info)
         return query.filter(LeaverecordModel.file_name.isnot(None))
 
 
 class AuthenticateUser(graphene.Mutation):
     """Authenticate user"""
-    class Input:
+    class Arguments:
         email = graphene.String()
         password = graphene.String()
 
@@ -127,11 +126,9 @@ class AuthenticateUser(graphene.Mutation):
     token = graphene.String()
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = User.get_query(context)
-        email = args.get('email')
-        password = args.get('password')
+    def mutate(self, info, email, password):
+        query = User.get_query(info)
+
         user = query.filter(UserModel.email == email).first()
 
         if not user or not user.verify_password(password):
@@ -146,42 +143,38 @@ class AuthenticateUser(graphene.Mutation):
 
 class VerifyUserToken(graphene.Mutation):
     """Verify user token"""
-    class Input:
+    class Arguments:
         userToken = graphene.String()
 
     User = graphene.Field(User)
     token = graphene.String()
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = User.get_query(context)
-        user_token = args.get('userToken')
+    def mutate(self, info, userToken):
+        query = User.get_query(info)
 
-        if not user_token or not UserModel.verify_auth_token(user_token):
+        if not userToken or not UserModel.verify_auth_token(userToken):
             raise Exception('Your session has expired!')
 
-        id = UserModel.verify_auth_token(user_token)
+        id = UserModel.verify_auth_token(userToken)
         user = query.filter(UserModel.id == id).first()
 
         ok = True
-        return VerifyUserToken(User=user, token=user_token, ok=ok)
+        return VerifyUserToken(User=user, token=userToken, ok=ok)
 
 
 class AuthenticateAdmin(graphene.Mutation):
     """Authenticate admin"""
-    class Input:
+    class Arguments:
         email = graphene.String()
         password = graphene.String()
 
     token = graphene.String()
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = Adminuser.get_query(context)
-        email = args.get('email')
-        password = args.get('password')
+    def mutate(self, info, email, password):
+        query = Adminuser.get_query(info)
+
         admin = query.filter(AdminuserModel.email == email).first()
 
         if not admin or not admin.verify_password(password):
@@ -196,45 +189,42 @@ class AuthenticateAdmin(graphene.Mutation):
 
 class VerifyAdminToken(graphene.Mutation):
     """Verify admin token"""
-    class Input:
+    class Arguments:
         adminToken = graphene.String()
 
     token = graphene.String()
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        admin_token = args.get('adminToken')
+    def mutate(self, info, adminToken):
 
-        if not admin_token or \
-                not AdminuserModel.verify_auth_token(admin_token):
+        if not adminToken or \
+                not AdminuserModel.verify_auth_token(adminToken):
             raise Exception('Your session has expired!')
 
         ok = True
-        return VerifyAdminToken(token=admin_token, ok=ok)
+        return VerifyAdminToken(token=adminToken, ok=ok)
 
 
 class ArchiveUser(graphene.Mutation):
     """Archive user"""
-    class Input:
+    class Arguments:
         id = graphene.String()
         archiveReason = graphene.String()
 
     User = graphene.Field(User)
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = User.get_query(context)
-        user_id = from_global_id(args.get('id'))[1]
-        archive_reason = args.get('archiveReason')
+    def mutate(self, info, id, archiveReason):
+        query = User.get_query(info)
+        user_id = from_global_id(id)[1]
+
         user = query.filter(UserModel.id == user_id).first()
 
         if user.isArchived is True:
             raise Exception('This user has an archived status!')
 
         user.isArchived = True
-        user.archiveReason = archive_reason
+        user.archiveReason = archiveReason
         db_session.add(user)
         db_session.commit()
         ok = True
@@ -243,16 +233,15 @@ class ArchiveUser(graphene.Mutation):
 
 class UnArchiveUser(graphene.Mutation):
     """Unarchive user"""
-    class Input:
+    class Arguments:
         id = graphene.String()
 
     User = graphene.Field(User)
     ok = graphene.Boolean()
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = User.get_query(context)
-        user_id = from_global_id(args.get('id'))[1]
+    def mutate(self, info, id):
+        query = User.get_query(info)
+        user_id = from_global_id(id)[1]
         user = query.filter(UserModel.id == user_id).first()
 
         if user.isArchived is False:
@@ -268,16 +257,15 @@ class UnArchiveUser(graphene.Mutation):
 
 class AddPublicholiday(graphene.Mutation):
     """Create public holiday"""
-    class Input:
+    class Arguments:
         holiday_date = graphene.String()
 
     ok = graphene.Boolean()
     publicHoliday = graphene.Field(Publicholiday)
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
+    def mutate(self, info, holiday_date):
         publicHoliday = PublicholidayModel(
-            holiday_date=args.get('holiday_date'))
+            holiday_date=holiday_date)
         db_session.add(publicHoliday)
         db_session.commit()
         ok = True
@@ -286,16 +274,15 @@ class AddPublicholiday(graphene.Mutation):
 
 class DeletePublicholiday(graphene.Mutation):
     """Delete public holiday"""
-    class Input:
+    class Arguments:
         id = graphene.String()
 
     ok = graphene.Boolean()
     publicHoliday = graphene.Field(Publicholiday)
 
-    @classmethod
-    def mutate(cls, _, args, context, info):
-        query = Publicholiday.get_query(context)
-        holiday_id = from_global_id(args.get('id'))[1]
+    def mutate(self, info, id):
+        query = Publicholiday.get_query(info)
+        holiday_id = from_global_id(id)[1]
         publicHoliday = query.filter(
             PublicholidayModel.id == holiday_id).first()
         db_session.delete(publicHoliday)
