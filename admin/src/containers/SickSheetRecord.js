3d2fbd01e438c7d495294dc647afffefc61e4817
navinesh <navinesh.chand@gmail.com>
2018-07-23 18:08:17 +1200

    Fix object

--- admin/src/containers/SickSheetRecord.js
+++ admin/src/containers/SickSheetRecord.js
@@ -54,7 +54,7 @@ class SickSheetRecord extends Component<Props> {
     const { auth_info, dispatch, verifyAdminToken } = this.props;
 
     const adminToken = auth_info.admin_token
-      ? auth_info.admin_token
+      ? auth_info
       : localStorage.getItem('admin_token');
 
     if (adminToken) {
