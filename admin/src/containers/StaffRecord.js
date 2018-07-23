ac845bf2510dc6021a5e8b786517123fb9b292e1
navinesh <navinesh.chand@gmail.com>
2018-07-23 18:08:13 +1200

    Fix object

--- admin/src/containers/StaffRecord.js
+++ admin/src/containers/StaffRecord.js
@@ -63,7 +63,7 @@ class StaffRecord extends Component<Props> {
     const { auth_info, dispatch, verifyAdminToken } = this.props;
 
     const adminToken = auth_info.admin_token
-      ? auth_info.admin_token
+      ? auth_info
       : localStorage.getItem('admin_token');
 
     if (adminToken) {
