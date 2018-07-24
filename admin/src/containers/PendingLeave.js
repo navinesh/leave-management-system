1f2ad243933d6b100996dc3347cc2cff277f3523
navinesh <navinesh.chand@gmail.com>
2018-07-24 14:52:58 +1200

    Fix object value

--- admin/src/containers/PendingLeave.js
+++ admin/src/containers/PendingLeave.js
@@ -80,7 +80,7 @@ class PendingLeave extends Component<Props> {
     const { auth_info, dispatch, verifyAdminToken } = this.props;
 
     const adminToken = auth_info.admin_token
-      ? auth_info.admin_token
+      ? auth_info
       : localStorage.getItem('admin_token');
 
     if (adminToken) {
