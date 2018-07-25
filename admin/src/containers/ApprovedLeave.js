875f8f76e56d18dee3147ee132f7c989b42d5366
navinesh <navinesh.chand@gmail.com>
2018-07-25 15:49:43 +1200

    Fix object value

--- admin/src/containers/ApprovedLeave.js
+++ admin/src/containers/ApprovedLeave.js
@@ -80,7 +80,7 @@ class ApprovedLeave extends Component<Props> {
     const { auth_info, dispatch, verifyAdminToken } = this.props;
 
     const adminToken = auth_info.admin_token
-      ? auth_info.admin_token
+      ? auth_info
       : localStorage.getItem('admin_token');
 
     if (adminToken) {
