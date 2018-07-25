805e6635c1a17e34ddae1267f6a62d89daa00089
navinesh <navinesh.chand@gmail.com>
2018-07-25 15:49:47 +1200

    Fix object value

--- admin/src/containers/LeaveReport.js
+++ admin/src/containers/LeaveReport.js
@@ -185,7 +185,7 @@ class LeaveReport extends Component<Props> {
     const { auth_info, dispatch, verifyAdminToken } = this.props;
 
     const adminToken = auth_info.admin_token
-      ? auth_info.admin_token
+      ? auth_info
       : localStorage.getItem('admin_token');
 
     if (adminToken) {
