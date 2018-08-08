b739d493488fe04ede22d66ea2e8fb6dcab82394
navinesh <navinesh.chand@gmail.com>
2018-08-08 12:00:44 +1200

    Remove admin user name on logout

--- admin/src/containers/CreateUser.js
+++ admin/src/containers/CreateUser.js
@@ -62,6 +62,7 @@ class CreateUser extends Component<Props> {
       } catch (error) {
         console.log(error);
         localStorage.removeItem('admin_token');
+        localStorage.removeItem('admin_user');
         dispatch(loginAdminErrorFromToken('Your session has expired!'));
       }
     }
