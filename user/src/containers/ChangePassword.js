9d891a61bfdd70802bc744a0d030cf66a3cd9333
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-02 10:20:12 +1200

    Use react hooks

--- user/src/containers/ChangePassword.js
+++ user/src/containers/ChangePassword.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
@@ -17,38 +17,32 @@ type Props = {
   isFetching: boolean
 };
 
-class UserChangePassword extends Component<Props> {
-  componentDidMount() {
-    this.props.dispatch(clearChangePasswordError());
-  }
+function UserChangePassword(props: Props) {
+  useEffect(function() {
+    return function() {
+      dispatch(clearChangePasswordError());
+    };
+  }, []);
 
-  render() {
-    const {
-      dispatch,
-      isAuthenticated,
-      message,
-      isFetching,
-      auth_info
-    } = this.props;
+  const { dispatch, isAuthenticated, message, isFetching, auth_info } = props;
 
-    return (
-      <Fragment>
-        {isAuthenticated ? (
-          <UserChange
-            dispatch={dispatch}
-            isFetching={isFetching}
-            message={message}
-            auth_info={auth_info}
-            onChangeClick={function(creds) {
-              return dispatch(changePassword(creds));
-            }}
-          />
-        ) : (
-          <Redirect to="/" />
-        )}
-      </Fragment>
-    );
-  }
+  return (
+    <>
+      {isAuthenticated ? (
+        <UserChange
+          dispatch={dispatch}
+          isFetching={isFetching}
+          message={message}
+          auth_info={auth_info}
+          onChangeClick={function(creds) {
+            return dispatch(changePassword(creds));
+          }}
+        />
+      ) : (
+        <Redirect to="/" />
+      )}
+    </>
+  );
 }
 
 function mapStateToProps(state) {
