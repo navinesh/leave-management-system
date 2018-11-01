75c087e7b98222ffc12f667fd2a0226a88e1e531
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-02 10:18:56 +1200

    Use react hooks

--- user/src/components/ChangePassword.js
+++ user/src/components/ChangePassword.js
@@ -1,8 +1,7 @@
 // @flow
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import '../spinners.css';
-import { clearChangePasswordError } from '../actions/ChangePassword';
 
 type Props = {
   dispatch: Function,
@@ -12,79 +11,46 @@ type Props = {
   auth_info: Object
 };
 
-type State = {
-  errorMessage: string,
-  currentPassword: string,
-  newPassword: string,
-  newPasswordConfirm: string
-};
+// type State = {
+//   errorMessage: string,
+//   currentPassword: string,
+//   newPassword: string,
+//   newPasswordConfirm: string
+// };
 
-export default class UserChange extends Component<Props, State> {
-  handleCurrentPasswordChange: Function;
-  handlePasswordChange: Function;
-  handlePasswordChangeConfirm: Function;
-  handleSubmit: Function;
+export default function UserChange(props: Props) {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  constructor() {
-    super();
-    this.state = {
-      errorMessage: '',
-      currentPassword: '',
-      newPassword: '',
-      newPasswordConfirm: ''
-    };
-
-    this.handleCurrentPasswordChange = this.handleCurrentPasswordChange.bind(
-      this
-    );
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handlePasswordChangeConfirm = this.handlePasswordChangeConfirm.bind(
-      this
-    );
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleCurrentPasswordChange({ target }: SyntheticInputEvent<>) {
+    setCurrentPassword(target.value);
   }
 
-  handleCurrentPasswordChange({ target }: SyntheticInputEvent<>) {
-    this.setState({ currentPassword: target.value });
+  function handlePasswordChange({ target }: SyntheticInputEvent<>) {
+    setNewPassword(target.value);
   }
 
-  handlePasswordChange({ target }: SyntheticInputEvent<>) {
-    this.setState({ newPassword: target.value });
+  function handlePasswordChangeConfirm({ target }: SyntheticInputEvent<>) {
+    setNewPasswordConfirm(target.value);
   }
 
-  handlePasswordChangeConfirm({ target }: SyntheticInputEvent<>) {
-    this.setState({ newPasswordConfirm: target.value });
-  }
-
-  handleSubmit(e: Event) {
+  function handleSubmit(e: Event) {
     e.preventDefault();
-    this.setState({ errorMessage: '' });
-    this.props.dispatch(clearChangePasswordError());
 
-    let auth_token = this.props.auth_info.auth_token;
+    let auth_token = props.auth_info.auth_token;
     if (!auth_token) {
       auth_token = localStorage.getItem('auth_token');
     }
 
-    const currentPassword = this.state.currentPassword
-      ? this.state.currentPassword.trim()
-      : null;
-    const newPassword = this.state.newPassword
-      ? this.state.newPassword.trim()
-      : null;
-    const newPasswordConfirm = this.state.newPasswordConfirm
-      ? this.state.newPasswordConfirm.trim()
-      : null;
-
     if (!currentPassword || !newPassword || !newPasswordConfirm) {
-      this.setState({
-        errorMessage: 'One or more required fields are missing!'
-      });
+      setErrorMessage('One or more required fields are missing!');
       return;
     }
 
     if (newPassword !== newPasswordConfirm) {
-      this.setState({ errorMessage: 'Your new passwords do not match!' });
+      setErrorMessage('Your new passwords do not match!');
       return;
     }
 
@@ -93,17 +59,11 @@ export default class UserChange extends Component<Props, State> {
         '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
       )
     ) {
-      this.setState({
-        errorMessage: 'Password does not meet complexity requirements!'
-      });
+      setErrorMessage('Password does not meet complexity requirements!');
       return;
     }
 
-    this.setState({
-      currentPassword: '',
-      newPassword: '',
-      newPasswordConfirm: ''
-    });
+    setErrorMessage('');
 
     const creds = {
       currentPassword: currentPassword,
@@ -111,75 +71,71 @@ export default class UserChange extends Component<Props, State> {
       auth_token: auth_token
     };
 
-    this.props.onChangeClick(creds);
+    props.onChangeClick(creds);
   }
 
-  render() {
-    return (
-      <div className="col-md-3 ml-auto mr-auto">
-        <div className="card card-body shadow p-3 mb-5 bg-white rounded">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="currentPassword">Current password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Current password"
-                id="currentPassword"
-                value={this.state.currentPassword}
-                onChange={this.handleCurrentPasswordChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="newPassword">New password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="New password"
-                id="newPassword"
-                value={this.state.newPassword}
-                onChange={this.handlePasswordChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="newPasswordConfirm">Confirm new password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Confirm new password"
-                id="newPasswordConfirm"
-                value={this.state.newPasswordConfirm}
-                onChange={this.handlePasswordChangeConfirm}
-              />
-              <small className="text-muted">
-                Password must meet complexity requirements:
-                <ul>
-                  <li>English uppercase characters (A through Z)</li>
-                  <li>English lowercase characters (a through z)</li>
-                  <li>Numerals (0 through 9)</li>
-                  <li>Non-alphabetic characters (such as !, $, #, %)</li>
-                  <li>More than 8 characters</li>
-                </ul>
-              </small>
-            </div>
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary col">
-                Update password
-              </button>
-            </div>
-          </form>
-          <div className="text-primary text-center">
-            {this.props.isFetching ? (
-              <div className="loader" />
-            ) : (
-              this.props.message
-            )}
+  const { isFetching, message } = props;
+
+  return (
+    <div className="col-md-3 ml-auto mr-auto">
+      <div className="card card-body shadow p-3 mb-5 bg-white rounded">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="currentPassword">Current password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Current password"
+              id="currentPassword"
+              value={currentPassword}
+              onChange={handleCurrentPasswordChange}
+            />
           </div>
-          <div className="text-danger text-center">
-            <div>{this.state.errorMessage}</div>
+          <div className="form-group">
+            <label htmlFor="newPassword">New password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="New password"
+              id="newPassword"
+              value={newPassword}
+              onChange={handlePasswordChange}
+            />
           </div>
+          <div className="form-group">
+            <label htmlFor="newPasswordConfirm">Confirm new password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm new password"
+              id="newPasswordConfirm"
+              value={newPasswordConfirm}
+              onChange={handlePasswordChangeConfirm}
+            />
+            <small className="text-muted">
+              Password must meet complexity requirements:
+              <ul>
+                <li>English uppercase characters (A through Z)</li>
+                <li>English lowercase characters (a through z)</li>
+                <li>Numerals (0 through 9)</li>
+                <li>Non-alphabetic characters (such as !, $, #, %)</li>
+                <li>More than 8 characters</li>
+              </ul>
+            </small>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary col">
+              Update password
+            </button>
+          </div>
+        </form>
+        <div className="text-primary text-center">
+          {isFetching ? <div className="loader" /> : message}
+        </div>
+        <div className="text-danger text-center">
+          <div>{errorMessage}</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
