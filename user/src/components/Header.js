04ad76a449e8ad79df4318cae0d770eeaefb70e4
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-02 10:19:01 +1200

    Use react hooks

--- user/src/components/Header.js
+++ user/src/components/Header.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 type Props = {
@@ -8,61 +8,49 @@ type Props = {
   logoutUser: Function
 };
 
-export default class Navs extends Component<Props> {
-  userLogout: Function;
-
-  constructor() {
-    super();
-    this.userLogout = this.userLogout.bind(this);
+export default function Navs(props: Props) {
+  function userLogout(e: Event) {
+    props.dispatch(props.logoutUser());
   }
 
-  userLogout(e: Event) {
-    this.props.dispatch(this.props.logoutUser());
-  }
-
-  render() {
-    return (
-      <nav className="navbar fixed-top navbar-expand-lg">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Leave Management System
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          {this.props.isAuthenticated && (
-            <div className="justify-content-end">
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <div className="navbar-nav">
-                  <Link className="nav-item nav-link" to="/leaveapplication">
-                    Apply for leave
-                  </Link>
-                  <Link className="nav-item nav-link" to="/leavecalendar">
-                    Leave calendar
-                  </Link>
-                  <button
-                    onClick={this.userLogout}
-                    className="btn btn-primary ml-2"
-                  >
-                    Sign out
-                  </button>
-                </div>
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Leave Management System
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        {props.isAuthenticated && (
+          <div className="justify-content-end">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <div className="navbar-nav">
+                <Link className="nav-item nav-link" to="/leaveapplication">
+                  Apply for leave
+                </Link>
+                <Link className="nav-item nav-link" to="/leavecalendar">
+                  Leave calendar
+                </Link>
+                <button onClick={userLogout} className="btn btn-primary ml-2">
+                  Sign out
+                </button>
               </div>
             </div>
-          )}
-        </div>
-      </nav>
-    );
-  }
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
