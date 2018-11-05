eb94544ba4511b320347e31d33d3a894c472b38b
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-05 15:36:01 +1300

    Fix refactor

--- user/src/containers/LeaveApplication.js
+++ user/src/containers/LeaveApplication.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { graphql, compose } from 'react-apollo';
 
@@ -27,7 +28,7 @@ const VERIFY_USER_TOKEN = gql`
 type Props = {
   auth_info: Object,
   dispatch: Function,
-
+  isAuthenticated: boolean,
   message: string,
   verifyUserToken: Function
 };
@@ -69,28 +70,35 @@ function LeaveApplication(props: Props) {
     }
   }
 
-  const { dispatch, auth_info, message } = props;
+  const { dispatch, isAuthenticated, auth_info, message } = props;
   let id = auth_info.id ? auth_info.id : localStorage.getItem('id');
 
   return (
-    <Application
-      id={id}
-      message={message}
-      dispatch={dispatch}
-      onLeaveApplicationClick={function(applicationDetails) {
-        return dispatch(fetchLeaveApplication(applicationDetails));
-      }}
-    />
+    <>
+      {isAuthenticated ? (
+        <Application
+          id={id}
+          message={message}
+          dispatch={dispatch}
+          onLeaveApplicationClick={function(applicationDetails) {
+            return dispatch(fetchLeaveApplication(applicationDetails));
+          }}
+        />
+      ) : (
+        <Redirect to="/" />
+      )}
+    </>
   );
 }
 
 function mapStateToProps(state) {
   const { userAuth, leaveApplication } = state;
-  const { auth_info } = userAuth;
+  const { auth_info, isAuthenticated } = userAuth;
   const { message } = leaveApplication;
 
   return {
     auth_info,
+    isAuthenticated,
     message
   };
 }
