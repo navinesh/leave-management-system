9228e1171e6fdb744d845a16a18fee2b7359104d
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-23 11:41:51 +1300

    Use functions

--- user/src/actions/ChangePassword.js
+++ user/src/actions/ChangePassword.js
@@ -6,42 +6,44 @@ export const PASSWORD_CHANGE_SUCCESS = 'PASSWORD_CHANGE_SUCCESS';
 export const PASSWORD_CHANGE_ERROR = 'PASSWORD_CHANGE_ERROR';
 export const CLEAR_CHANGE_PASSWORD_ERROR = 'CLEAR_CHANGE_PASSWORD_ERROR';
 
-export const requestPasswordChange = (creds: Object) => {
+export function requestPasswordChange(creds: Object) {
   return { type: REQUEST_PASSWORD_CHANGE, creds };
-};
+}
 
-export const passwordChangeError = (data: Object) => {
+export function passwordChangeError(data: Object) {
   return { type: PASSWORD_CHANGE_ERROR, message: data.message };
-};
+}
 
-export const passwordChangeSuccess = (data: Object) => {
+export function passwordChangeSuccess(data: Object) {
   return { type: PASSWORD_CHANGE_SUCCESS, message: data.message };
-};
+}
 
-export const clearChangePasswordError = () => {
+export function clearChangePasswordError() {
   return { type: CLEAR_CHANGE_PASSWORD_ERROR };
-};
+}
 
-export const changePassword = (creds: Object) => async (dispatch: Function) => {
-  try {
-    dispatch(requestPasswordChange(creds));
-    const response = await axios({
-      method: 'post',
-      url: 'http://localhost:8080/change-password',
-      auth: { username: creds.auth_token },
-      data: {
-        oldPassword: creds.currentPassword,
-        newPassword: creds.newPassword
+export function changePassword(creds: Object) {
+  return async function(dispatch: Function) {
+    try {
+      dispatch(requestPasswordChange(creds));
+      const response = await axios({
+        method: 'post',
+        url: 'http://localhost:8080/change-password',
+        auth: { username: creds.auth_token },
+        data: {
+          oldPassword: creds.currentPassword,
+          newPassword: creds.newPassword
+        }
+      });
+
+      if (response.status !== 201) {
+        dispatch(passwordChangeError(response.data));
+      } else {
+        dispatch(passwordChangeSuccess(response.data));
       }
-    });
-
-    if (response.status !== 201) {
-      dispatch(passwordChangeError(response.data));
-    } else {
-      dispatch(passwordChangeSuccess(response.data));
+    } catch (error) {
+      localStorage.removeItem('auth_token');
+      dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
     }
-  } catch (error) {
-    localStorage.removeItem('auth_token');
-    dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
-  }
-};
+  };
+}
