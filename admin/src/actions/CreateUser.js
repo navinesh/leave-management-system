b66a48e85dc4be89184a793176526c6cc8ba012e
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-26 10:45:28 +1300

    Use functions

--- admin/src/actions/CreateUser.js
+++ admin/src/actions/CreateUser.js
@@ -6,63 +6,63 @@ export const NEW_USER_RECORD_SUCCESS = 'NEW_USER_RECORD_SUCCESS';
 export const NEW_USER_RECORD_FAILURE = 'NEW_USER_RECORD_FAILURE';
 export const CLEAR_NEW_USER_RECORD = 'CLEAR_NEW_USER_RECORD';
 
-export const requestNewUserRecord = (newUserDetails: Object) => {
+export function requestNewUserRecord(newUserDetails: Object) {
   return {
     type: NEW_USER_RECORD_REQUEST,
     newUserDetails
   };
-};
+}
 
-export const successNewUserRecord = (data: Object) => {
+export function successNewUserRecord(data: Object) {
   return {
     type: NEW_USER_RECORD_SUCCESS,
     message: data.message
   };
-};
+}
 
-export const failureNewUserRecord = (data: Object) => {
+export function failureNewUserRecord(data: Object) {
   return {
     type: NEW_USER_RECORD_FAILURE,
     message: data.message
   };
-};
+}
 
-export const clearNewUserRecordMessage = () => {
+export function clearNewUserRecordMessage() {
   return {
     type: CLEAR_NEW_USER_RECORD
   };
-};
+}
 
-export const submitNewUserRecord = (newUserDetails: Object) => async (
-  dispatch: Function
-) => {
-  try {
-    dispatch(requestNewUserRecord(newUserDetails));
-    const response = await axios.post('http://localhost:8080/adduser', {
-      surname: newUserDetails.surname,
-      othernames: newUserDetails.othernames,
-      email: newUserDetails.staffEmail,
-      designation: newUserDetails.designation,
-      annual: newUserDetails.annualDays,
-      sick: newUserDetails.sickDays,
-      bereavement: newUserDetails.bereavementDays,
-      family_care: newUserDetails.familyCareDays,
-      christmas: newUserDetails.christmasDays,
-      date_of_birth: newUserDetails.dateOfBirth,
-      maternity: newUserDetails.maternityDays,
-      paternity: newUserDetails.paternityDays,
-      gender: newUserDetails.gender,
-      employee_number: newUserDetails.employeeNumber,
-      employee_start_date: newUserDetails.employeeStartDate,
-      admin_user: newUserDetails.adminUser
-    });
+export function submitNewUserRecord(newUserDetails: Object) {
+  return async function(dispatch: Function) {
+    try {
+      dispatch(requestNewUserRecord(newUserDetails));
+      const response = await axios.post('http://localhost:8080/adduser', {
+        surname: newUserDetails.surname,
+        othernames: newUserDetails.othernames,
+        email: newUserDetails.staffEmail,
+        designation: newUserDetails.designation,
+        annual: newUserDetails.annualDays,
+        sick: newUserDetails.sickDays,
+        bereavement: newUserDetails.bereavementDays,
+        family_care: newUserDetails.familyCareDays,
+        christmas: newUserDetails.christmasDays,
+        date_of_birth: newUserDetails.dateOfBirth,
+        maternity: newUserDetails.maternityDays,
+        paternity: newUserDetails.paternityDays,
+        gender: newUserDetails.gender,
+        employee_number: newUserDetails.employeeNumber,
+        employee_start_date: newUserDetails.employeeStartDate,
+        admin_user: newUserDetails.adminUser
+      });
 
-    if (response.status !== 201) {
-      dispatch(failureNewUserRecord(response.data));
-    } else {
-      dispatch(successNewUserRecord(response.data));
+      if (response.status !== 201) {
+        dispatch(failureNewUserRecord(response.data));
+      } else {
+        dispatch(successNewUserRecord(response.data));
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
-};
+  };
+}
