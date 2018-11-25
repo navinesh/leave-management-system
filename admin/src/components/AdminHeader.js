f70816e047c78a6e7459d3adf6f11404dbd7d6ce
Navinesh Chand <NEC@MunroLeysLaw.local>
2018-11-26 10:46:01 +1300

    Use functions

--- admin/src/components/AdminHeader.js
+++ admin/src/components/AdminHeader.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 type Props = {
@@ -7,74 +7,59 @@ type Props = {
   logoutAdmin: Function
 };
 
-export default class Header extends Component<Props> {
-  adminLogout: Function;
-
-  constructor() {
-    super();
-    this.adminLogout = this.adminLogout.bind(this);
+export default function Header(props: Props) {
+  function adminLogout(e: Event) {
+    props.dispatch(props.logoutAdmin());
   }
 
-  adminLogout(e: Event) {
-    this.props.dispatch(this.props.logoutAdmin());
-  }
-
-  render() {
-    return (
-      <nav className="navbar fixed-top navbar-expand-lg">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Leave Management System
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="justify-content-end">
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/approvedleave">
-                  Approved
-                </Link>
-                <Link className="nav-item nav-link" to="/staffrecord">
-                  Staff
-                </Link>
-                <Link className="nav-item nav-link" to="/archivedstaffrecord">
-                  Archived
-                </Link>
-                <Link className="nav-item nav-link" to="/newrecord">
-                  New
-                </Link>
-                <Link className="nav-item nav-link" to="/leavereport">
-                  Reports
-                </Link>
-                <Link className="nav-item nav-link" to="/sicksheetrecord">
-                  Sick sheet
-                </Link>
-                <Link className="nav-item nav-link" to="/publicholiday">
-                  Public Holidays
-                </Link>
-                <button
-                  onClick={this.adminLogout}
-                  className="btn btn-primary ml-1"
-                >
-                  Sign out
-                </button>
-              </div>
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Leave Management System
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="justify-content-end">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link" to="/approvedleave">
+                Approved
+              </Link>
+              <Link className="nav-item nav-link" to="/staffrecord">
+                Staff
+              </Link>
+              <Link className="nav-item nav-link" to="/archivedstaffrecord">
+                Archived
+              </Link>
+              <Link className="nav-item nav-link" to="/createuser">
+                Create user
+              </Link>
+              <Link className="nav-item nav-link" to="/leavereport">
+                Reports
+              </Link>
+              <Link className="nav-item nav-link" to="/sicksheetrecord">
+                Sick sheet
+              </Link>
+              <Link className="nav-item nav-link" to="/publicholiday">
+                Public Holidays
+              </Link>
+              <button onClick={adminLogout} className="btn btn-primary ml-1">
+                Sign out
+              </button>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
