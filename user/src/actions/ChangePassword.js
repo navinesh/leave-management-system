9609bf52e78ae8e6b99451d5c146f68833e9eb55
navinesh <navinesh.chand@gmail.com>
2018-11-29 16:10:31 +1300

    Revert "Delete file"
    This reverts commit 7c836351d2fe6f09cc76b28e3926703bb5521f21.

---
+++ user/src/actions/ChangePassword.js
@@ -0,0 +1,49 @@
+// @flow
+import axios from 'axios';
+
+export const REQUEST_PASSWORD_CHANGE = 'REQUEST_PASSWORD_CHANGE';
+export const PASSWORD_CHANGE_SUCCESS = 'PASSWORD_CHANGE_SUCCESS';
+export const PASSWORD_CHANGE_ERROR = 'PASSWORD_CHANGE_ERROR';
+export const CLEAR_CHANGE_PASSWORD_ERROR = 'CLEAR_CHANGE_PASSWORD_ERROR';
+
+export function requestPasswordChange(creds: Object) {
+  return { type: REQUEST_PASSWORD_CHANGE, creds };
+}
+
+export function passwordChangeError(data: Object) {
+  return { type: PASSWORD_CHANGE_ERROR, message: data.message };
+}
+
+export function passwordChangeSuccess(data: Object) {
+  return { type: PASSWORD_CHANGE_SUCCESS, message: data.message };
+}
+
+export function clearChangePasswordError() {
+  return { type: CLEAR_CHANGE_PASSWORD_ERROR };
+}
+
+export function changePassword(creds: Object) {
+  return async function(dispatch: Function) {
+    try {
+      dispatch(requestPasswordChange(creds));
+      const response = await axios({
+        method: 'post',
+        url: 'http://localhost:8080/change-password',
+        auth: { username: creds.auth_token },
+        data: {
+          oldPassword: creds.currentPassword,
+          newPassword: creds.newPassword
+        }
+      });
+
+      if (response.status !== 201) {
+        dispatch(passwordChangeError(response.data));
+      } else {
+        dispatch(passwordChangeSuccess(response.data));
+      }
+    } catch (error) {
+      localStorage.removeItem('auth_token');
+      dispatch({ type: 'LOGIN_FAILURE_FROM_TOKEN' });
+    }
+  };
+}
