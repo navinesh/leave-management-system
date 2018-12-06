224b6896d007f0568855156f11f4b9086b391e05
navinesh <navinesh.chand@gmail.com>
2018-12-06 17:37:15 +1300

    Add flow type

--- admin/flow-typed/modules.js
+++ admin/flow-typed/modules.js
@@ -3,19 +3,31 @@ declare module 'react-router-dom' {
   declare module.exports: any;
 }
 
-declare module 'redux' {
-  declare module.exports: any;
-}
-
-declare module 'react-redux' {
-  declare module.exports: any;
-}
-
 declare module 'react-apollo' {
   declare module.exports: any;
 }
 
-declare module 'apollo-boost' {
+declare module 'apollo-client' {
+  declare module.exports: any;
+}
+
+declare module 'apollo-cache-inmemory' {
+  declare module.exports: any;
+}
+
+declare module 'apollo-link' {
+  declare module.exports: any;
+}
+
+declare module 'apollo-link-http' {
+  declare module.exports: any;
+}
+
+declare module 'apollo-link-error' {
+  declare module.exports: any;
+}
+
+declare module 'graphql-tag' {
   declare module.exports: any;
 }
 
