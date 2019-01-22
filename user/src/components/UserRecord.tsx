5bbb47ac620560cd0afda5d761cbf3506d194791
navinesh <navinesh.chand@gmail.com>
2019-01-22 15:48:00 +1200

    First commit to TypeScript file

---
+++ user/src/components/UserRecord.tsx
@@ -0,0 +1,277 @@
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+import NoData from '../img/undraw_no_data_qbuo.svg';
+
+const USER_RECORD = gql`
+  query($id: ID!) {
+    user(id: $id) {
+      leaverecord {
+        edges {
+          node {
+            id
+            leaveName
+            leaveDays
+            startDate
+            endDate
+            leaveReason
+            leaveStatus
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface UserLeaveDataProps {
+  user_record: UserRecord;
+}
+
+interface UserRecord {
+  leaverecord: LeaveRecord;
+}
+
+interface LeaveRecord {
+  edges: Array<Edge>;
+}
+
+interface Edge {
+  node: UserLeaveData;
+}
+
+interface UserLeaveData {
+  leaveStatus: string;
+  id: string;
+  leaveName: string;
+  leaveDays: number;
+  startDate: Date;
+  endDate: Date;
+  leaveReason: string;
+}
+
+function ApprovedRecordList(props: UserLeaveDataProps): JSX.Element {
+  const approvedList = props.user_record.leaverecord.edges
+    .filter(data => data.node.leaveStatus === 'approved')
+    .map(record => (
+      <tr key={record.node.id}>
+        <td>{record.node.leaveName}</td>
+        <td>{record.node.leaveDays}</td>
+        <td>{record.node.startDate}</td>
+        <td>{record.node.endDate}</td>
+        <td>{record.node.leaveReason}</td>
+      </tr>
+    ));
+
+  if (approvedList.length > 0) {
+    return (
+      <table className="table table-bordered table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th>Leave type</th>
+            <th>Leave days</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Reason</th>
+          </tr>
+        </thead>
+        <tbody>{approvedList}</tbody>
+      </table>
+    );
+  } else {
+    return (
+      <div data-align="center">
+        <img src={NoData} alt="No data" />
+      </div>
+    );
+  }
+}
+
+function PendingRecordList(props: UserLeaveDataProps): JSX.Element {
+  const pendingList = props.user_record.leaverecord.edges
+    .filter(data => data.node.leaveStatus === 'pending')
+    .map(record => (
+      <tr key={record.node.id}>
+        <td>{record.node.leaveName}</td>
+        <td>{record.node.leaveDays}</td>
+        <td>{record.node.startDate}</td>
+        <td>{record.node.endDate}</td>
+        <td>{record.node.leaveReason}</td>
+      </tr>
+    ));
+
+  if (pendingList.length > 0) {
+    return (
+      <table className="table table-bordered table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th>Leave type</th>
+            <th>Leave days</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Reason</th>
+          </tr>
+        </thead>
+        <tbody>{pendingList}</tbody>
+      </table>
+    );
+  } else {
+    return (
+      <div data-align="center">
+        <img src={NoData} alt="No data" />
+      </div>
+    );
+  }
+}
+
+function ArchivedRecordList(props: UserLeaveDataProps): JSX.Element {
+  const pendingList = props.user_record.leaverecord.edges
+    .filter(data => data.node.leaveStatus === 'archived')
+    .map(record => (
+      <tr key={record.node.id}>
+        <td>{record.node.leaveName}</td>
+        <td>{record.node.leaveDays}</td>
+        <td>{record.node.startDate}</td>
+        <td>{record.node.endDate}</td>
+        <td>{record.node.leaveReason}</td>
+      </tr>
+    ));
+
+  if (pendingList.length > 0) {
+    return (
+      <table className="table table-bordered table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th>Leave type</th>
+            <th>Leave days</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Reason</th>
+          </tr>
+        </thead>
+        <tbody>{pendingList}</tbody>
+      </table>
+    );
+  } else {
+    return (
+      <div data-align="center">
+        <img src={NoData} alt="No data" />
+      </div>
+    );
+  }
+}
+
+interface TabsProps {
+  data: Array<TabData>;
+}
+
+interface TabData {
+  label: string;
+  content: any;
+}
+
+function Tabs(props: TabsProps): JSX.Element {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  function selectTabIndex(index: number): void {
+    setActiveIndex(index);
+  }
+
+  function renderHeading() {
+    return <div className="container h6">Leave status</div>;
+  }
+
+  function renderTabs(): JSX.Element[] {
+    return props.data.map((tab, index) => {
+      const isActive = activeIndex === index;
+      return (
+        <div className="nav-link btn" key={index}>
+          <div
+            className={
+              isActive
+                ? 'border border-right-0 border-left-0 border-top-0 border-secondary'
+                : 'text-secondary'
+            }
+            onClick={() => selectTabIndex(index)}
+          >
+            {tab.label}
+          </div>
+        </div>
+      );
+    });
+  }
+
+  function renderPanel(): JSX.Element {
+    return (
+      <div className="container pt-2">{props.data[activeIndex].content}</div>
+    );
+  }
+
+  return (
+    <>
+      {renderHeading()}
+      <div className="container nav">{renderTabs()}</div>
+      {renderPanel()}
+    </>
+  );
+}
+
+interface Props {
+  id: string;
+}
+
+export default function UserRecord(props: Props): JSX.Element {
+  return (
+    <Query
+      query={USER_RECORD}
+      variables={{ id: props.id }}
+      pollInterval={60000}
+    >
+      {({ loading, error, data }) => {
+        if (loading) {
+          return (
+            <div
+              className="container text-center"
+              style={{ paddingTop: '80px' }}
+            >
+              <div className="col-md-8 ml-auto mr-auto">
+                <div className="loader" />
+              </div>
+            </div>
+          );
+        }
+
+        if (error) {
+          console.log(error.message);
+          return (
+            <div
+              className="container text-center"
+              style={{ paddingTop: '100px' }}
+            >
+              <div className="col-md-8 ml-auto mr-auto">
+                <p>Something went wrong!</p>
+              </div>
+            </div>
+          );
+        }
+
+        const tabData = [
+          {
+            label: 'Approved',
+            content: <ApprovedRecordList user_record={data.user} />
+          },
+          {
+            label: 'Pending',
+            content: <PendingRecordList user_record={data.user} />
+          },
+          {
+            label: 'Archived',
+            content: <ArchivedRecordList user_record={data.user} />
+          }
+        ];
+
+        return <Tabs data={tabData} />;
+      }}
+    </Query>
+  );
+}
