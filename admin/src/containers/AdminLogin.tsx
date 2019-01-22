4e8a69cb23f5f7678d23bc8c9e33436f83092c4e
navinesh <navinesh.chand@gmail.com>
2019-01-23 08:33:19 +1200

    First commit to TypeScript

---
+++ admin/src/containers/AdminLogin.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+
+import Login from '../components/AdminLogin';
+
+const IS_AUTHENTICATED = gql`
+  query isAdminAuthenticated {
+    isAuthenticated @client
+    sessionError @client
+  }
+`;
+
+export default function AdminLogin(): JSX.Element {
+  return (
+    <Query query={IS_AUTHENTICATED}>
+      {({ data }) => {
+        return !data.isAuthenticated ? (
+          <Login sessionError={data.sessionError} />
+        ) : (
+          <Redirect to="/" />
+        );
+      }}
+    </Query>
+  );
+}
