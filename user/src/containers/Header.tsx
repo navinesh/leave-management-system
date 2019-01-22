85fc323ae793a2ffc7029f0876b94ec3c743a594
navinesh <navinesh.chand@gmail.com>
2019-01-22 15:47:52 +1200

    First commit to TypeScript file

---
+++ user/src/containers/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+import Navs from '../components/Header';
+
+const IS_AUTHENTICATED = gql`
+  query IsAuthenticated {
+    isAuthenticated @client
+  }
+`;
+
+export default function Header(): JSX.Element {
+  return (
+    <Query query={IS_AUTHENTICATED}>
+      {({ data }) => {
+        return <Navs isAuthenticated={data.isAuthenticated} />;
+      }}
+    </Query>
+  );
+}
