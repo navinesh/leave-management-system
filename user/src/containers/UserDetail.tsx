06a316a13e6fc7974b8b87505e9cc82e66471cc5
navinesh <navinesh.chand@gmail.com>
2019-01-22 15:47:23 +1200

    First commit to TypeScript file

---
+++ user/src/containers/UserDetail.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+import UserDetail from '../components/UserDetail';
+
+const GET_ID = gql`
+  query Id {
+    id @client
+  }
+`;
+
+export default function UserDetails(): JSX.Element {
+  return (
+    <Query query={GET_ID}>
+      {({ data }) => {
+        let id = data.id ? data.id : localStorage.getItem('id');
+        return <UserDetail id={id} />;
+      }}
+    </Query>
+  );
+}
