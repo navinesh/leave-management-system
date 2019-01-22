cfc902559b257e7fb4916185275c35c651df4aa6
navinesh <navinesh.chand@gmail.com>
2019-01-22 15:47:56 +1200

    First commit to TypeScript file

---
+++ user/src/containers/ChangePassword.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+
+import UserChange from '../components/ChangePassword';
+
+const IS_AUTHENTICATED = gql`
+  query IsAuthenticated {
+    isAuthenticated @client
+    auth_token @client
+  }
+`;
+
+export default function UserChangePassword(): JSX.Element {
+  return (
+    <Query query={IS_AUTHENTICATED}>
+      {({ data }) => {
+        return data.isAuthenticated ? (
+          <UserChange auth_token={data.auth_token} />
+        ) : (
+          <Redirect to="/" />
+        );
+      }}
+    </Query>
+  );
+}
