3b7b10457afe96731d36880ee43dfba9c4a9870f
navinesh <navinesh.chand@gmail.com>
2019-01-23 08:33:29 +1200

    First commit to TypeScript

---
+++ admin/src/containers/AdminResetPassword.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+
+import AdminResetPassword from '../components/AdminResetPassword';
+
+const IS_AUTHENTICATED = gql`
+  query isAdminAuthenticated {
+    isAuthenticated @client
+  }
+`;
+
+export default function ResetPassword(): JSX.Element {
+  return (
+    <div className="container">
+      <Query query={IS_AUTHENTICATED}>
+        {({ data }) => {
+          return !data.isAuthenticated ? (
+            <AdminResetPassword />
+          ) : (
+            <Redirect to="/" />
+          );
+        }}
+      </Query>
+    </div>
+  );
+}
