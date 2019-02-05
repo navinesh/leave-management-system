e0e3caad3b9eca4b87a84ab73b7b08e06289857b
navinesh <navinesh.chand@gmail.com>
2019-02-06 10:28:32 +1200

    First commit

---
+++ user/src/containers/UseInterval.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+
+export default function useInterval(callback: any, delay: any): void {
+  const savedCallback = useRef<any>('');
+  // Remember the latest callback.
+  useEffect(
+    function(): void {
+      savedCallback.current = callback;
+    },
+    [callback]
+  );
+  // Set up the interval.
+  useEffect(
+    function() {
+      function tick(): void {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    },
+    [delay]
+  );
+}
