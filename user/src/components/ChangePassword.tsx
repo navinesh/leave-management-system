00ce37977d32deb48955b134df07b6f2d7b79a9f
navinesh <navinesh.chand@gmail.com>
2019-04-09 13:52:32 +1200

    Add bootstrap spinner

--- user/src/components/ChangePassword.tsx
+++ user/src/components/ChangePassword.tsx
@@ -156,7 +156,13 @@ export default function UserChange(props: Props): JSX.Element {
           </div>
         </form>
         <div className="text-primary text-center">
-          {loading ? <div className="loader" /> : serverMessage}
+          {loading ? (
+            <div className="spinner-border text-primary" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          ) : (
+            serverMessage
+          )}
         </div>
         <div className="text-danger text-center">
           <div>{errorMessage}</div>
