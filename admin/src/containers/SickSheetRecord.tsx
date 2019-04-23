13e580bcd1434680dff1f131a31a4ce7f1d657f3
navinesh <navinesh.chand@gmail.com>
2019-04-24 09:29:58 +1200

    Add useEffect missing dependency

--- admin/src/containers/SickSheetRecord.tsx
+++ admin/src/containers/SickSheetRecord.tsx
@@ -47,9 +47,13 @@ type Props = {
 };
 
 function MainView(props: Props): JSX.Element {
-  useEffect(function(): void {
-    props.verifyAdminToken();
-  }, []);
+  const { verifyAdminToken } = props;
+  useEffect(
+    function(): void {
+      verifyAdminToken();
+    },
+    [verifyAdminToken]
+  );
 
   return (
     <div className="container">
