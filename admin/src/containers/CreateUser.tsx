82b1885220bb6145429cbbaf961dd34b13f206c3
navinesh <navinesh.chand@gmail.com>
2019-04-24 09:29:36 +1200

    Add useEffect missing dependency

--- admin/src/containers/CreateUser.tsx
+++ admin/src/containers/CreateUser.tsx
@@ -30,9 +30,13 @@ type Props = {
 };
 
 function MainView(props: Props): JSX.Element {
-  useEffect(function(): void {
-    props.verifyAdminToken();
-  }, []);
+  const { verifyAdminToken } = props;
+  useEffect(
+    function(): void {
+      verifyAdminToken();
+    },
+    [verifyAdminToken]
+  );
 
   return <CreateUserForm />;
 }
