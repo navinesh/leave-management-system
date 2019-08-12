dce2d0bb5b7fc10163423222d76f919bf72b46e6
navinesh <navinesh.chand@gmail.com>
2019-08-13 11:15:47 +1200

    Use Apollo Client Hooks

--- user/src/components/Calendar.tsx
+++ user/src/components/Calendar.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 import Error from '../img/error.gif';
+import NoData from '../img/undraw_no_data_qbuo.svg';
 
 const moment = require('moment');
 
@@ -41,97 +42,90 @@ interface Leave {
 }
 
 export default function Calendar(): JSX.Element {
-  return (
-    <Query query={LEAVE_RECORD} pollInterval={60000}>
-      {({ loading, error, data }: any) => {
-        if (loading) {
-          return (
-            <div
-              className="container text-center"
-              style={{ paddingTop: '100px' }}
-            >
-              <div className="col-md-8 ml-auto mr-auto">
-                <div className="spinner-border text-primary" role="status">
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            </div>
-          );
-        }
+  const { loading, error, data } = useQuery(LEAVE_RECORD, {
+    pollInterval: 60000
+  });
 
-        if (error) {
-          console.log(error.message);
-          return (
-            <div className="col mx-auto">
-              <div className="text-center">
-                <p style={{ fontSize: '42px' }}>Oops!</p>
-                <p style={{ fontSize: '24px' }}>Something went wrong!</p>
-                <img src={Error} alt="Error" />
-              </div>
-            </div>
-          );
-        }
-
-        const leaveRecords = data.findLeaveRecord
-          .filter((record: Leave) => {
-            // get current date and format it
-            let dateToday = moment();
-
-            let todayDate = dateToday.format('DD/MM/YYYY');
-
-            // get end date and format it
-            let end_Date = moment(record.endDate, 'DD/MM/YYYY').format(
-              'DD/MM/YYYY'
-            );
-
-            // check if current date and end date is same
-            let isCurrentDate = todayDate === end_Date ? true : false;
-
-            // get end date and format it
-            let eDate = moment(record.endDate, 'DD/MM/YYYY').format(
-              'MM/DD/YYYY'
-            );
-
-            let endDate = moment(new Date(eDate));
-
-            // check if end date is same as or falls after current date
-            let isEndDate = endDate.isSameOrAfter(dateToday);
-
-            // return true for current and future dates
-            return isCurrentDate || isEndDate ? true : false;
-          })
-          .sort((a: UserProps, b: UserProps) => {
-            return a.user.othernames.localeCompare(b.user.othernames);
-          })
-          .map((data: Leave) => (
-            <tr key={data.id}>
-              <td>
-                {data.user.othernames} {data.user.surname}
-              </td>
-              <td>{data.leaveName}</td>
-              <td>{data.startDate}</td>
-              <td>{data.endDate}</td>
-              <td>{data.leaveDays}</td>
-            </tr>
-          ));
-
-        return (
-          <div className="table-responsive">
-            <table className="table table-bordered table-hover">
-              <thead className="thead-light">
-                <tr>
-                  <th>Name</th>
-                  <th>Leave type</th>
-                  <th>Start date</th>
-                  <th>End date</th>
-                  <th>Leave days</th>
-                </tr>
-              </thead>
-              <tbody>{leaveRecords}</tbody>
-            </table>
+  if (loading) {
+    return (
+      <div className="container text-center" style={{ paddingTop: '100px' }}>
+        <div className="col-md-8 ml-auto mr-auto">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
-        );
-      }}
-    </Query>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    console.log(error.message);
+    return (
+      <div className="col mx-auto">
+        <div className="text-center">
+          <p style={{ fontSize: '42px' }}>Oops!</p>
+          <p style={{ fontSize: '24px' }}>Something went wrong!</p>
+          <img src={Error} alt="Error" />
+        </div>
+      </div>
+    );
+  }
+
+  const leaveRecords = data.findLeaveRecord
+    .filter((record: Leave) => {
+      // get current date and format it
+      let dateToday = moment();
+
+      let todayDate = dateToday.format('DD/MM/YYYY');
+
+      // get end date and format it
+      let end_Date = moment(record.endDate, 'DD/MM/YYYY').format('DD/MM/YYYY');
+
+      // check if current date and end date is same
+      let isCurrentDate = todayDate === end_Date ? true : false;
+
+      // get end date and format it
+      let eDate = moment(record.endDate, 'DD/MM/YYYY').format('MM/DD/YYYY');
+
+      let endDate = moment(new Date(eDate));
+
+      // check if end date is same as or falls after current date
+      let isEndDate = endDate.isSameOrAfter(dateToday);
+
+      // return true for current and future dates
+      return isCurrentDate || isEndDate ? true : false;
+    })
+    .sort((a: UserProps, b: UserProps) => {
+      return a.user.othernames.localeCompare(b.user.othernames);
+    })
+    .map((data: Leave) => (
+      <tr key={data.id}>
+        <td>
+          {data.user.othernames} {data.user.surname}
+        </td>
+        <td>{data.leaveName}</td>
+        <td>{data.startDate}</td>
+        <td>{data.endDate}</td>
+        <td>{data.leaveDays}</td>
+      </tr>
+    ));
+
+  return leaveRecords.length > 0 ? (
+    <div className="table-responsive">
+      <table className="table table-bordered table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th>Name</th>
+            <th>Leave type</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Leave days</th>
+          </tr>
+        </thead>
+        <tbody>{leaveRecords}</tbody>
+      </table>
+    </div>
+  ) : (
+    <img src={NoData} alt="No data" width="60%" height="60%" />
   );
 }
