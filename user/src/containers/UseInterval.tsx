7d3310dee039cf827af6ee731c359ad2a19da614
navinesh <navinesh.chand@gmail.com>
2019-08-13 11:19:09 +1200

    Format

--- user/src/containers/UseInterval.tsx
+++ user/src/containers/UseInterval.tsx
@@ -3,23 +3,17 @@ import { useEffect, useRef } from 'react';
 export default function useInterval(callback: any, delay: any): void {
   const savedCallback = useRef<any>('');
   // Remember the latest callback.
-  useEffect(
-    function(): void {
-      savedCallback.current = callback;
-    },
-    [callback]
-  );
+  useEffect((): void => {
+    savedCallback.current = callback;
+  }, [callback]);
   // Set up the interval.
-  useEffect(
-    function() {
-      function tick(): void {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    },
-    [delay]
-  );
+  useEffect(() => {
+    function tick(): void {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
