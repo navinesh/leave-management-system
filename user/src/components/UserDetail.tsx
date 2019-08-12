5a6f67dc0b20da6cb2960f58a1f3c4e0ee0bb326
navinesh <navinesh.chand@gmail.com>
2019-08-13 11:17:09 +1200

    Use Apollo Client Hooks

--- user/src/components/UserDetail.tsx
+++ user/src/components/UserDetail.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 const USER_DETAIL = gql`
   query($id: ID!) {
@@ -25,113 +25,102 @@ interface Props {
 }
 
 export default function UserDetail(props: Props): JSX.Element {
-  return (
-    <Query
-      query={USER_DETAIL}
-      variables={{ id: props.id }}
-      pollInterval={60000}
-    >
-      {({ loading, error, data }: any) => {
-        if (loading) {
-          return (
-            <div
-              className="container text-center"
-              style={{ paddingTop: '80px' }}
-            >
-              <div className="col-md-8 ml-auto mr-auto">
-                <div className="spinner-border text-primary" role="status">
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            </div>
-          );
-        }
+  const { loading, error, data } = useQuery(USER_DETAIL, {
+    variables: { id: props.id },
+    pollInterval: 60000
+  });
 
-        if (error) {
-          console.log(error.message);
-          return (
-            <div
-              className="container text-center"
-              style={{ paddingTop: '100px' }}
-            >
-              <div className="col-md-8 ml-auto mr-auto">
-                <p>Something went wrong!</p>
-              </div>
-            </div>
-          );
-        }
-
-        return (
-          <div style={{ paddingBottom: '30px' }}>
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8">
-                  <p style={{ fontSize: '30px' }}>
-                    {data.user.othernames} {data.user.surname}
-                  </p>
-                  <p>
-                    <Link to="/changepassword" className="btn btn-primary">
-                      Change password
-                    </Link>
-                  </p>
-                </div>
-                <div className="col-md-4">
-                  <ul className="list-group">
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                      Annual
-                      <span className="badge badge-primary badge-pill">
-                        {data.user.annual}
-                      </span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                      Sick
-                      <span className="badge badge-primary badge-pill">
-                        {data.user.sick}
-                      </span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                      Bereavement
-                      <span className="badge badge-primary badge-pill">
-                        {data.user.bereavement}
-                      </span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                      Family care
-                      <span className="badge badge-primary badge-pill">
-                        {data.user.familyCare}
-                      </span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                      Christmas
-                      <span className="badge badge-primary badge-pill">
-                        {data.user.christmas}
-                      </span>
-                    </li>
-                    {data.user.gender.toLowerCase() === 'female' &&
-                      data.user.maternity > 0 && (
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                          Maternity
-                          <span className="badge badge-primary badge-pill">
-                            {data.user.maternity}
-                          </span>
-                        </li>
-                      )}
-                    {data.user.gender.toLowerCase() === 'male' &&
-                      data.user.paternity > 0 && (
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                          Paternity
-                          <span className="badge badge-primary badge-pill">
-                            {data.user.paternity}
-                          </span>
-                        </li>
-                      )}
-                  </ul>
-                </div>
-              </div>
-            </div>
+  if (loading) {
+    return (
+      <div className="container text-center" style={{ paddingTop: '80px' }}>
+        <div className="col-md-8 ml-auto mr-auto">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
-        );
-      }}
-    </Query>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    console.log(error.message);
+    return (
+      <div className="container text-center" style={{ paddingTop: '100px' }}>
+        <div className="col-md-8 ml-auto mr-auto">
+          <p>Something went wrong!</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ paddingBottom: '30px' }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <p style={{ fontSize: '30px' }}>
+              {data.user.othernames} {data.user.surname}
+            </p>
+            <p>
+              <Link to="/changepassword" className="btn btn-primary">
+                Change password
+              </Link>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <ul className="list-group">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Annual
+                <span className="badge badge-primary badge-pill">
+                  {data.user.annual}
+                </span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Sick
+                <span className="badge badge-primary badge-pill">
+                  {data.user.sick}
+                </span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Bereavement
+                <span className="badge badge-primary badge-pill">
+                  {data.user.bereavement}
+                </span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Family care
+                <span className="badge badge-primary badge-pill">
+                  {data.user.familyCare}
+                </span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                Christmas
+                <span className="badge badge-primary badge-pill">
+                  {data.user.christmas}
+                </span>
+              </li>
+              {data.user.gender.toLowerCase() === 'female' &&
+                data.user.maternity > 0 && (
+                  <li className="list-group-item d-flex justify-content-between align-items-center">
+                    Maternity
+                    <span className="badge badge-primary badge-pill">
+                      {data.user.maternity}
+                    </span>
+                  </li>
+                )}
+              {data.user.gender.toLowerCase() === 'male' &&
+                data.user.paternity > 0 && (
+                  <li className="list-group-item d-flex justify-content-between align-items-center">
+                    Paternity
+                    <span className="badge badge-primary badge-pill">
+                      {data.user.paternity}
+                    </span>
+                  </li>
+                )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
