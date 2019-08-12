7af0061549c3cb4186b0c432c64260b5c1a6b6f5
navinesh <navinesh.chand@gmail.com>
2019-08-13 11:19:19 +1200

    Use Apollo Client Hooks

--- user/src/containers/UserRecord.tsx
+++ user/src/containers/UserRecord.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 import UserRecord from '../components/UserRecord';
 
@@ -11,12 +11,9 @@ const GET_ID = gql`
 `;
 
 export default function UserRecords(): JSX.Element {
-  return (
-    <Query query={GET_ID}>
-      {({ data }: any) => {
-        let id = data.id ? data.id : localStorage.getItem('id');
-        return <UserRecord id={id} />;
-      }}
-    </Query>
-  );
+  const { data } = useQuery(GET_ID);
+
+  let id = data.id ? data.id : localStorage.getItem('id');
+
+  return <UserRecord id={id} />;
 }
