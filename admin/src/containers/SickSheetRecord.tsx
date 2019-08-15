d01e3913af00fa1de69b16d89f9111fbe6146eea
navinesh <navinesh.chand@gmail.com>
2019-08-16 11:06:28 +1200

    Use Apollo Client Hooks

--- admin/src/containers/SickSheetRecord.tsx
+++ admin/src/containers/SickSheetRecord.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import gql from 'graphql-tag';
-import { Query, Mutation, ApolloConsumer } from 'react-apollo';
+import { useQuery, useMutation, useApolloClient } from '@apollo/react-hooks';
 import { Redirect } from 'react-router-dom';
 
 import { TokenSuccess, TokenFailure } from './TokenComponents';
@@ -42,85 +42,65 @@ const SICK_RECORD = gql`
   }
 `;
 
-type Props = {
-  verifyAdminToken: Function;
-};
-
-function MainView(props: Props): JSX.Element {
-  const { verifyAdminToken } = props;
-  useEffect(
-    function(): void {
-      verifyAdminToken();
-    },
-    [verifyAdminToken]
-  );
-
-  return (
-    <div className="container">
-      <Query query={SICK_RECORD} pollInterval={60000}>
-        {({
-          loading,
-          error,
-          data: { findSicksheetRecord: sickSheet_items }
-        }: any) => {
-          if (loading) {
-            return (
-              <div className="text-center" style={{ marginTop: '80px' }}>
-                <div className="spinner-border text-primary" role="status">
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            );
-          }
-
-          if (error) {
-            console.log(error);
-            return (
-              <div className="text-center">
-                <p>Something went wrong!</p>
-              </div>
-            );
-          }
-
-          return <SickSheetList sickSheet_items={sickSheet_items} />;
-        }}
-      </Query>
-    </div>
-  );
-}
-
 export default function SickSheetRecord(): JSX.Element {
-  return (
-    <Query query={IS_AUTHENTICATED}>
-      {({ data }: any) => {
-        let adminToken = data.admin_token
-          ? data.admin_token
-          : localStorage.getItem('admin_token');
+  const client = useApolloClient();
 
-        return data.isAuthenticated ? (
-          <ApolloConsumer>
-            {client => (
-              <Mutation
-                mutation={VERIFY_ADMIN_TOKEN}
-                variables={{ adminToken: adminToken }}
-                onCompleted={(data: any) => {
-                  if (data.verifyAdminToken) {
-                    TokenSuccess(data, client);
-                  } else {
-                    TokenFailure(client);
-                  }
-                }}
-              >
-                {(verifyAdminToken: any) => {
-                  return <MainView verifyAdminToken={verifyAdminToken} />;
-                }}
-              </Mutation>
-            )}
-          </ApolloConsumer>
-        ) : (
-          <Redirect to="/login" />
-        );
-      }}
-    </Query>
+  const { data } = useQuery(IS_AUTHENTICATED);
+  let adminToken = data.admin_token
+    ? data.admin_token
+    : localStorage.getItem('admin_token');
+
+  const {
+    loading,
+    error,
+    data: { findSicksheetRecord: sickSheet_items }
+  }: any = useQuery(SICK_RECORD, {
+    pollInterval: 60000
+  });
+
+  const [verifyAdminToken] = useMutation(VERIFY_ADMIN_TOKEN, {
+    variables: { adminToken: adminToken },
+    onCompleted(data) {
+      if (data.verifyAdminToken) {
+        TokenSuccess(data, client);
+      } else {
+        TokenFailure(client);
+      }
+    }
+  });
+
+  useEffect((): void => {
+    verifyAdminToken();
+  }, [verifyAdminToken]);
+
+  if (loading) {
+    return (
+      <div className="container">
+        <div className="text-center" style={{ marginTop: '80px' }}>
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    console.log(error);
+    return (
+      <div className="container">
+        <div className="text-center">
+          <p>Something went wrong!</p>
+        </div>
+      </div>
+    );
+  }
+
+  return data.isAuthenticated ? (
+    <div className="container">
+      <SickSheetList sickSheet_items={sickSheet_items} />
+    </div>
+  ) : (
+    <Redirect to="/login" />
   );
 }
